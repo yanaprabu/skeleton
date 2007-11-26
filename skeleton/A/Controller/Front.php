6e0faee0d0ea287ca5c1d677d95f0c31246be1ee
arborint <arborint@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2007-11-26 17:41:41 +0000

    is_a() to instanceof

--- skeleton/A/Controller/Front.php
+++ skeleton/A/Controller/Front.php
@@ -3,32 +3,32 @@ if (! class_exists('A_Locator')) include 'A/Locator.php';
 if (! class_exists('A_DL')) include 'A/DL.php';
 
 class A_Controller_Front {
-   protected $mapper;
-   protected $error_action;
-   protected $prefilters;
+   protected $_mapper;
+   protected $_error_action;
+   protected $_prefilters;
 
     public function __construct($mapper, $error_action, $prefilters=array()) {
-    	$this->mapper = $mapper;
-    	$this->error_action = $error_action;
-    	$this->prefilters = $prefilters;
+    	$this->_mapper = $mapper;
+    	$this->_error_action = $error_action;
+    	$this->_prefilters = $prefilters;
     }
 
     public function addPreMethod($method, $action) {
-    	$this->prefilters[$method] = $action;
+    	$this->_prefilters[$method] = $action;
     }
 
     public function addPreFilter($name, $prefilter) {
-   		$this->prefilters[$name] = $prefilter;
+   		$this->_prefilters[$name] = $prefilter;
     }
 
     public function run($locator) {
-        $action = $this->mapper->doMapping($locator);
-		$error_action = $this->error_action;
+        $action = $this->_mapper->doMapping($locator);
+		$error_action = $this->_error_action;
         while ($action) {
-			$class  = $this->mapper->buildClass($action->class);
-	        $method = $this->mapper->buildMethod($action->method);
+			$class  = $this->_mapper->buildClass($action->class);
+	        $method = $this->_mapper->buildMethod($action->method);
 			if ($action->dir == '') {
-				$dir = $this->mapper->getPath();
+				$dir = $this->_mapper->getPath();
 			} else {
 	    	    $dir = $action->dir;
 			}
@@ -38,12 +38,12 @@ class A_Controller_Front {
 				$class = str_replace('-', '_', $class);
 		        $controller = new $class($locator);
 	
-				if ($this->prefilters) {
-					foreach (array_keys($this->prefilters) as $name) {
-						if (is_object($this->prefilters[$name])) {
-							if (! is_a($this->prefilters[$name], 'A_DL')) {
+				if ($this->_prefilters) {
+					foreach (array_keys($this->_prefilters) as $name) {
+						if (is_object($this->_prefilters[$name])) {
+							if (! ($this->_prefilters[$name] instanceof A_DL)) {
 								// pass controller to DI object to modify
-								$change_action = $this->prefilters[$name]->run($controller);
+								$change_action = $this->_prefilters[$name]->run($controller);
 							} elseif (method_exists($controller, $name)) {
 								// pre-execute method if it exists 
 								$change_action = $controller->{$name}($locator);
@@ -53,22 +53,22 @@ class A_Controller_Front {
 							if ($change_action) {
 								if (is_object($change_action)) {
 									$action = $change_action;
-								} elseif (is_object($this->prefilters[$name])) {
-									$action = $this->prefilters[$name];
+								} elseif (is_object($this->_prefilters[$name])) {
+									$action = $this->_prefilters[$name];
 								} else {
-									$action = $this->error_action;
+									$action = $this->_error_action;
 								}
 								continue 2;
 							}
-						} elseif (is_string($this->prefilters[$name]) && function_exists($this->prefilters[$name])) {
-							$func = $this->prefilters[$name];
+						} elseif (is_string($this->_prefilters[$name]) && function_exists($this->_prefilters[$name])) {
+							$func = $this->_prefilters[$name];
 							$func($controller);
 						}
 					}
 				}
 				
 				if (! method_exists($controller, $method)) {
-					$method = $this->mapper->default_method;
+					$method = $this->_mapper->default_method;
 				}
 				if (method_exists($controller, $method)) {
 					$action = $controller->{$method}($locator);
