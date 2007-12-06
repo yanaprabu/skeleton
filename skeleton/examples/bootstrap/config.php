842733c67f1ad896c68db4f7f5d9ad07759426fa
arborint <arborint@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2007-12-06 18:20:29 +0000

    add bootstrap class and example

---
+++ skeleton/examples/bootstrap/config.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL);
+ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . dirname(__FILE__) . '/../../');
+
+function dump($var, $name='') {
+	echo $name . '<pre>' . print_r($var, 1) . '</pre>';
+}
+?>
---
+++ skeleton/examples/bootstrap/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once('config.php');
+require_once('A/Bootstrap.php');
+
+$App = new A_Bootstrap();
+$App->run();
