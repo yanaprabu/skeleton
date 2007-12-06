bb9b6875f0095e89c47d94cdd2d27a063687308e
arborint <arborint@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2007-12-06 07:33:53 +0000

    renamed directory template to templates

--- skeleton/examples/response/example.php
+++ skeleton/examples/response/example.php
@@ -4,19 +4,19 @@ include '../../A/Template.php';
 
 $response = new A_Http_Response();
 
-$template = new A_Template_Strreplace('template/layout.html');
+$template = new A_Template_Strreplace('templates/layout.html');
 $template->set('title', 'Response Example');
 $template->set('content', 'This is the content for the response example. ');
 $response->setRenderer($template);
 
 $response1 = new A_Http_ResponseChild('block1');
-$template1 = new A_Template_Strreplace('template/block1.html');
+$template1 = new A_Template_Strreplace('templates/block1.html');
 $template1->set('title', 'Block One');
 $template1->set('content', 'This is the content for block one. ');
 $response1->setRenderer($template1);
 
 $response2 = new A_Http_ResponseChild('block2');
-$template2 = new A_Template_Strreplace('template/block2.html');
+$template2 = new A_Template_Strreplace('templates/block2.html');
 $template2->set('title', 'Block Two');
 $template2->set('content', 'This is the content for block two. ');
 $response2->setRenderer($template2);
