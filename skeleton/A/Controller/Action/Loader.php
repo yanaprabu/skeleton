58421240716fecb5c5dce1ae461f01bc147f03d3
arborint <arborint@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2007-12-06 09:03:46 +0000

    add new Action_Loader for model/view loading in Action

---
+++ skeleton/A/Controller/Action/Loader.php
@@ -0,0 +1,67 @@
+<?php
+
+class A_Controller_Action_Loader {
+	protected $locator;
+	protected $paths;
+	protected $dirs;
+	protected $action;
+	protected $suffix;
+	protected $scope_path;
+
+	public function __construct($locator, $paths, $dirs, $action) {
+		$this->locator = $locator;
+		$this->paths = $paths;
+		$this->dirs = $dirs;
+		$this->action = $action;
+		$this->suffix = array('model'=>'Model', 'view'=>'View', );
+	}
+	 
+	public function setScope($module=null) {
+		if (! isset($this->paths[$module])) {
+			$module = 'module';     // the default setting e.g., "/app/module/models"
+		}
+		$this->scope_path = $this->paths[$module];
+		return $this;
+	}
+
+	public function __call($type, $params) {
+		// is this a defined type of subdirectory
+		if (isset($this->dirs[$type])) {
+			// get class name parameter or use action name
+			if (isset($params[0])) {
+				$class = $params[0];
+			} else {
+				$class = $this->action;
+			}
+			if (isset($this->suffix[$type])) {
+				$length = strlen($this->suffix[$type]);
+				// if a suffix is defined and the end of the action name does not contain it -- append it
+				if ($length && (substr($class, -$length) != $this->suffix[$type])) {
+					$class .= $this->suffix[$type];
+				}
+			}
+			// load class if necessary and return instance
+			return $this->locator->get($class, $class, $this->scope_path . $this->dirs[$type]);
+		}
+	}
+
+/*
+	public function model($class=null) {
+		if ($class === null) {
+			$class = $this->action;
+		}
+		$name = $class . $this->suffix['model'];
+echo "A_Controller_Loader: model: class=$class, name=$name<br/>";
+		return $this->locator->get($name, $name, $this->scope_path . 'models/');
+	}
+*/
+	 
+	public function view($class=null) {
+		if ($class === null) {
+			$class = $this->action;
+		}
+		$name = $class . $this->suffix['model'];
+		return $this->locator->get($name, $name, $this->scope_path . 'views/');
+	}
+	 
+}
