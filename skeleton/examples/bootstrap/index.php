d048b8006a5aad1e28a7a58ede36d9eb8e6e7f0a
jcartwri <jcartwri@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2007-12-08 22:13:59 +0000

    Deleted A_Bootstrap to be replaced with A_Application

--- skeleton/examples/bootstrap/index.php
+++ skeleton/examples/bootstrap/index.php
@@ -1,6 +1,19 @@
 <?php
 require_once('config.php');
-require_once('A/Bootstrap.php');
+require_once('A/Application.php');
 
-$App = new A_Bootstrap();
-$App->run();
+class foo 
+{
+	public $bar = 'woohoo';
+}
+
+$App = new A_Application();
+
+//add a random component that will be passed along into locator
+$App->set('foo', new foo);
+
+//set path to application
+$App->setPath(dirname($_SERVER['SCRIPT_FILENAME']));
+
+//output content directly to screen
+echo $App->run();
