8990048b5826dde5226d56967e34b75d094012b5
arborint <arborint@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2007-12-29 04:53:02 +0000

    add support for sql objects to query()

--- skeleton/A/Db/Mysqli.php
+++ skeleton/A/Db/Mysqli.php
@@ -1,128 +1,132 @@
 <?php
 
-class A_Db_Mysqli {protected $dsn = null;protected $link = null;protected $sequenceext = '_seq';protected $sequencestart = 1;
-
-public function __construct($dsn=null) {
-	if ($dsn) {
-		$this->connect($dsn);
-	}
-}
+class A_Db_Mysqli {	protected $dsn = null;	protected $link = null;	protected $sequenceext = '_seq';	protected $sequencestart = 1;
 	
-public function connect ($dsn=null) {
-	$result = false;
-	if ($dsn) {
-		$this->dsn = $dsn;
-	}
-	if ($this->link == null) {
-		$this->link = mysqli_connect($this->dsn['hostspec'], $this->dsn['username'], $this->dsn['password']);
-		if ($this->link) {
-			if (isset($this->dsn['database'])) {
-				$result = mysqli_select_db($this->dsn['database'], $this->link);
-			} else {
-				$result = true;
-			}
+	public function __construct($dsn=null) {
+		if ($dsn) {
+			$this->connect($dsn);
 		}
 	}
-	return $result;
-}
-	
-public function disconnect() {
-	if ($db->link) {
-		mysqli_disconnect($db->link);
-	} 
-}
-	
-public function query ($sql) {
-	mysql_select_db($this->dsn['database'], $this->link);
-	if (strpos(strtolower($sql), 'select') === 0) {
-		$obj = new A_Db_Mysqli_Recordset(mysqli_query($sql));
-	} else {
-		$obj = new A_Db_Mysqli_Result(mysqli_query($sql));
+		
+	public function connect ($dsn=null) {
+		$result = false;
+		if ($dsn) {
+			$this->dsn = $dsn;
+		}
+		if ($this->link == null) {
+			$this->link = mysqli_connect($this->dsn['hostspec'], $this->dsn['username'], $this->dsn['password']);
+			if ($this->link) {
+				if (isset($this->dsn['database'])) {
+					$result = mysqli_select_db($this->dsn['database'], $this->link);
+				} else {
+					$result = true;
+				}
+			}
+		}
+		return $result;
 	}
-	$obj->errno = mysqli_errno($this->link);
-	$obj->errmsg = mysqli_error($this->link);
-	return $obj;
-}
-	
-public function limitQuery ($sql, $from, $count) {
-	return($this->query($sql . " LIMIT $from,$count"));
-}
-	
-public function lastId() {
-	if ($this->link) {
-		return(mysqli_insert_id($this->link));
-	} else {
-		return 0;
+		
+	public function disconnect() {
+		if ($db->link) {
+			mysqli_disconnect($db->link);
+		} 
 	}
-}
-	
-public function nextId ($sequence) {
-    if ($sequence) {
-	    $result = mysqli_query("UPDATE $sequence{$this->sequenceext} SET id=LAST_INSERT_ID(id+1)");
-    	if ($result) {
-	        $id = mysqli_insert_id($this->link);
-	        if ($id > 0) {
-	            return $id;
-	        } else {
-			    $result = mysqli_query("INSERT $sequence{$this->sequenceext} SET id=1");
+		
+	public function query ($sql) {
+		if (is_object($sql) && method_exists($sql, 'execute')) {
+			// convert object to string by executing SQL builder object
+			$sql = $sql->execute($this);   // pass $this to provide db specific escape() method
+		}
+		mysql_select_db($this->dsn['database'], $this->link);
+		if (strpos(strtolower($sql), 'select') === 0) {
+			$obj = new A_Db_Mysqli_Recordset(mysqli_query($sql));
+		} else {
+			$obj = new A_Db_Mysqli_Result(mysqli_query($sql));
+		}
+		$obj->errno = mysqli_errno($this->link);
+		$obj->errmsg = mysqli_error($this->link);
+		return $obj;
+	}
+		
+	public function limitQuery ($sql, $from, $count) {
+		return($this->query($sql . " LIMIT $from,$count"));
+	}
+		
+	public function lastId() {
+		if ($this->link) {
+			return(mysqli_insert_id($this->link));
+		} else {
+			return 0;
+		}
+	}
+		
+	public function nextId ($sequence) {
+	    if ($sequence) {
+		    $result = mysqli_query("UPDATE $sequence{$this->sequenceext} SET id=LAST_INSERT_ID(id+1)");
+	    	if ($result) {
 		        $id = mysqli_insert_id($this->link);
 		        if ($id > 0) {
 		            return $id;
+		        } else {
+				    $result = mysqli_query("INSERT $sequence{$this->sequenceext} SET id=1");
+			        $id = mysqli_insert_id($this->link);
+			        if ($id > 0) {
+			            return $id;
+			        }
 		        }
-	        }
-		} elseif (mysqli_errno() == 1146) {		// table does not exist
-			if ($this->createSequence($sequence)) {
-				return $this->sequencestart;
+			} elseif (mysqli_errno() == 1146) {		// table does not exist
+				if ($this->createSequence($sequence)) {
+					return $this->sequencestart;
+				}
 			}
-		}
-    }
-    return 0;
-}
+	    }
+	    return 0;
+	}
+		
+	public function createSequence ($sequence) {
+	    $result = 0;
+	    if ($sequence) {
+		    $result = mysqli_query($this->link, "CREATE TABLE $sequence{$this->sequenceext} (id int(10) unsigned NOT NULL auto_increment, PRIMARY KEY(id)) TYPE=MyISAM AUTO_INCREMENT={$this->sequencestart}");
+	    }
+	    return($result);
+	}
+		
+	public function escape($value) {
+		return mysqli_real_escape_string($this->link, $value);
+	}
 	
-public function createSequence ($sequence) {
-    $result = 0;
-    if ($sequence) {
-	    $result = mysqli_query($this->link, "CREATE TABLE $sequence{$this->sequenceext} (id int(10) unsigned NOT NULL auto_increment, PRIMARY KEY(id)) TYPE=MyISAM AUTO_INCREMENT={$this->sequencestart}");
-    }
-    return($result);
-}
-	
-public function escape($value) {
-	return mysqli_real_escape_string($this->link, $value);
-}
-
-public function isError() {
-	return mysqli_errno($this->link);
-}
-	
-public function getMessage() {
-	return mysqli_error($this->link);
-}
+	public function isError() {
+		return mysqli_errno($this->link);
+	}
+		
+	public function getMessage() {
+		return mysqli_error($this->link);
+	}
 	
 } // end DAO class
 
 
-class A_Db_Mysqli_Result {protected $result;public $errno;public $errmsg;
-
-public function __construct($result=null) {
-	$this->result = $result;
-}
+class A_Db_Mysqli_Result {	protected $result;	public $errno;	public $errmsg;
 	
-public function numRows() {
-	if ($this->result) {
-		return mysql_affected_rows($this->result);
-	} else {
-		return 0;
+	public function __construct($result=null) {
+		$this->result = $result;
 	}
-}
-	
-public function isError() {
-	return $this->errno;
-}
-	
-public function getMessage() {
-	return $this->errmsg;
-}
+		
+	public function numRows() {
+		if ($this->result) {
+			return mysql_affected_rows($this->result);
+		} else {
+			return 0;
+		}
+	}
+		
+	public function isError() {
+		return $this->errno;
+	}
+		
+	public function getMessage() {
+		return $this->errmsg;
+	}
 
 }
 
--- skeleton/A/Db/SqLite.php
+++ skeleton/A/Db/SqLite.php
@@ -1,85 +1,89 @@
 <?php
 
-class A_Db_Sqlite {protected $dsn = null;protected $link = null;protected $sequenceext = '_seq';protected $sequencestart = 1;
-
-public function __construct($dsn=null) {
-	if ($dsn) {
-		$this->connect($dsn);
+class A_Db_Sqlite {	protected $dsn = null;	protected $link = null;	protected $sequenceext = '_seq';	protected $sequencestart = 1;
+	
+	public function __construct($dsn=null) {
+		if ($dsn) {
+			$this->connect($dsn);
+		}
 	}
-}
-	
-public function connect ($dsn, $options=null) {
-	if ($this->link == null) {
-		$this->link = @sqlite_open($dsn['filename'], $dsn['mode']);
-	} 
-}
-	
-public function disconnect() {
-	if ($db->link) {
-		sqlite_close($db->link);
-	} 
-}
-	
-public function query ($sql) {
-	mysql_select_db($this->dsn['database'], $this->link);
-	if (strpos(strtolower($sql), 'select') === 0) {
-		$obj = new A_Db_Sqlite_Recordset(sqlite_query($sql));
-	} else {
-		$obj = new A_Db_Sqlite_Result(sqlite_query($sql));
+		
+	public function connect ($dsn, $options=null) {
+		if ($this->link == null) {
+			$this->link = @sqlite_open($dsn['filename'], $dsn['mode']);
+		} 
 	}
-	$obj->errno = sqlite_last_error($this->link);
-	$obj->errmsg = sqlite_error_string($obj->errno);
-	return $obj;
-}
-	
-public function limitQuery ($sql, $from, $count) {
-	return($this->query($sql . " LIMIT $from,$count"));
-}
-	
-public function lastId() {
-	if ($this->link) {
-		return(sqlite_last_insert_rowid($this->link));
-	} else {
-		return 0;
+		
+	public function disconnect() {
+		if ($db->link) {
+			sqlite_close($db->link);
+		} 
 	}
-}
+		
+	public function query ($sql) {
+		if (is_object($sql) && method_exists($sql, 'execute')) {
+			// convert object to string by executing SQL builder object
+			$sql = $sql->execute($this);   // pass $this to provide db specific escape() method
+		}
+		mysql_select_db($this->dsn['database'], $this->link);
+		if (strpos(strtolower($sql), 'select') === 0) {
+			$obj = new A_Db_Sqlite_Recordset(sqlite_query($sql));
+		} else {
+			$obj = new A_Db_Sqlite_Result(sqlite_query($sql));
+		}
+		$obj->errno = sqlite_last_error($this->link);
+		$obj->errmsg = sqlite_error_string($obj->errno);
+		return $obj;
+	}
+		
+	public function limitQuery ($sql, $from, $count) {
+		return($this->query($sql . " LIMIT $from,$count"));
+	}
+		
+	public function lastId() {
+		if ($this->link) {
+			return(sqlite_last_insert_rowid($this->link));
+		} else {
+			return 0;
+		}
+	}
+		
+	public function nextId ($sequence) {
+	    return 0;
+	}
+		
+	public function createSequence ($sequence) {
+	    return 0;
+	}
+		
+	public function escape($value) {
+		return sqlite_escape_string($value);
+	}
 	
-public function nextId ($sequence) {
-    return 0;
-}
+	public function isError() {
+		return sqlite_last_error($this->link);
+	}
+		
+	public function getMessage() {
+		return sqlite_error_string(sqlite_last_error($this->link));
+	}
 	
-public function createSequence ($sequence) {
-    return 0;
-}
-	
-public function escape($value) {
-	return sqlite_escape_string($value);
 }
 
-public function isError() {
-	return sqlite_last_error($this->link);
-}
-	
-public function getMessage() {
-	return sqlite_error_string(sqlite_last_error($this->link));
-}
-	
-}
 
-
-class A_Db_Sqlite_Result {protected $result;public $errno;public $errmsg;
-
-public function __construct($result=null) {
-	$this->result = $result;
-}
+class A_Db_Sqlite_Result {	protected $result;	public $errno;	public $errmsg;
 	
-public function isError() {
-	return $this->errno;
-}
-	
-public function getMessage() {
-	return $this->errmsg;
-}
+	public function __construct($result=null) {
+		$this->result = $result;
+	}
+		
+	public function isError() {
+		return $this->errno;
+	}
+		
+	public function getMessage() {
+		return $this->errmsg;
+	}
 	
 }
 
@@ -88,36 +92,36 @@ public function getMessage() {
 
 class A_Db_Sqlite_Recordset extends A_Db_Sqlite_Result {
 
-public function __construct($result=null) {
-	$this->result = $result;
-}
-
-public function fetchRow () {
-	if ($this->result) {
-		return(sqlite_fetch_array($this->result, SQLITE_ASSOC));
+	public function __construct($result=null) {
+		$this->result = $result;
 	}
-}
 	
-public function fetchObject () {
-	if ($this->result) {
-		return(sqlite_fetch_object($this->result));
+	public function fetchRow () {
+		if ($this->result) {
+			return(sqlite_fetch_array($this->result, SQLITE_ASSOC));
+		}
 	}
-}
-	
-public function numRows() {
-	if ($this->result) {
-		return(sqlite_num_rows($this->result));
-	} else {
-		return 0;
+		
+	public function fetchObject () {
+		if ($this->result) {
+			return(sqlite_fetch_object($this->result));
+		}
 	}
-}
-	
-public function numCols() {
-	if ($this->result) {
-		return(sqlite_num_cols($this->result));
-	} else {
-		return 0;
+		
+	public function numRows() {
+		if ($this->result) {
+			return(sqlite_num_rows($this->result));
+		} else {
+			return 0;
+		}
 	}
-}
+		
+	public function numCols() {
+		if ($this->result) {
+			return(sqlite_num_cols($this->result));
+		} else {
+			return 0;
+		}
+	}
 	
 }
