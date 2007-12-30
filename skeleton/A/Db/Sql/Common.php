fd0363c508f4fc9f1854cc71389399d1686bd004
jcartwri <jcartwri@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2007-12-30 05:07:05 +0000

    Updated A_Db_Sql_Common::quoteValue() to not quote numerical values

--- skeleton/A/Db/Sql/Common.php
+++ skeleton/A/Db/Sql/Common.php
@@ -2,13 +2,9 @@
 
 class A_Db_Sql_Common {
 	protected $nameQuote = '`';
-	
-	public function __construct() {
-	}
-	
+
 	public function escape($value) {
-		$value = addslashes($value);
-		return $value;		// at least do something. Will depend on db
+		return addslashes($value); //at least do something. Will depend on db
 	}
 	
 	public function equation($field, $op, $value) {
@@ -16,16 +12,15 @@ class A_Db_Sql_Common {
 	}
 
 	public function quoteValue($value) {
-		if (preg_match('/^[A-Z\_]*\(/', $value) == 0) {	// not a function
-			$value = "'$value'";
-		}
-		return $value;
+		$value = trim($value, '\''); //incase the user already quoted the value
+		if (preg_match('/^[A-Z\_]*\(/i', $value) || ctype_digit($value)) { //detect if the value is a function or digits
+			return $value;
+		}		
+		return '\''. $value .'\'';
 	}
 	
 	public function quoteName($name) {
 		$name = str_ireplace(' AS ', $this->nameQuote .' AS '. $this->nameQuote, $name); //table aliases need backticks between AS
 		return $this->nameQuote . trim($name, $this->nameQuote) . $this->nameQuote;
 	}
-	
 }
-
