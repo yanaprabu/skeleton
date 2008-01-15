4d6e39650c0bc672b7862c4f594842fb7f6578a6
arborint <arborint@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-01-15 09:49:52 +0000

    add setResponse() example

--- skeleton/examples/actioncontroller/app/controllers/example.php
+++ skeleton/examples/actioncontroller/app/controllers/example.php
@@ -15,7 +15,8 @@ class example extends A_Controller_Action {
 	<h2>Front Controller: Page - Default Action</h2>
 	<ol>
 		<li><a href="?controller=example">Default controller, no action specified.</a></li>
-		<li><a href="?controller=example&action=foo">Default controller, specific action specified.</a></li>
+		<li><a href="?controller=example&action=foo">Default controller, specific action, this->autoRender().</a></li>
+		<li><a href="?controller=example&action=bar">Default controller, specific action, this->load->setResponse()->template().</a></li>
 		<li><a href="?module=module1&controller=example">Module and controller, no action specified.</a></li>
 		<li><a href="?module=module1&controller=example&action=bar">Module and controller, specific action specified.</a></li>
 	</ol>
@@ -37,28 +38,11 @@ class example extends A_Controller_Action {
 	}
 
 	function foo($locator) {
-/*
-		$content = '
-<html>
-<body>
-	<h2>Front Controller: Page - Specific Action</h2>
-	<ol>
-		<li><a href="?controller=example">Default controller, no action specified.</a></li>
-		<li><a href="?controller=example&action=foo">Default controller, specific action specified.</a></li>
-		<li><a href="?module=module1&controller=example">Module and controller, no action specified.</a></li>
-		<li><a href="?module=module1&controller=example&action=bar">Module and controller, specific action specified.</a></li>
-	</ol>
-	<br/>
-	<p><a href="../">Return to Examples</a></p>
-';
-		$content .= '<br/>Action Object:<pre>' . print_r($this, 1) . '</pre>';
-		$content .= '
-</body>
-</html>
-';
-		$this->response->setContent($content);
-*/
 		$this->autoRender();
+	}
+
+	function bar($locator) {
+		$this->load()->setResponse()->template();
 	}
 
 }
