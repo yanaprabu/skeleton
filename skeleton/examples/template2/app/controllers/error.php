3c77ad8196390e479371dbf84cc4d510fd29397b
mattiman <mattiman@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-01-26 21:13:57 +0000

    Added an example with a "complex" template

---
+++ skeleton/examples/template2/app/controllers/error.php
@@ -0,0 +1,20 @@
+<?php
+
+class error {
+
+	function run($locator) {
+		$content = '
+<html>
+<body>
+	<h2>Front Controller: Error</h2>
+	<a href="?controller=example">Return</a>
+</body>
+</html>
+';
+		$response = $locator->get('Response');
+		$response->setContent($content);
+	}
+
+}
+
+?>
