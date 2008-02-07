ab91afe89f222a4791dd0c77a8134ed2f87c276e
arborint <arborint@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-02-07 19:16:58 +0000

    Update to A_Html_Form to add fluent interface to create entire form.

--- skeleton/A/Html/Form.php
+++ skeleton/A/Html/Form.php
@@ -2,16 +2,64 @@
 include_once 'A/Html/Tag.php';
 
 class A_Html_Form {
-
+	protected $_attr = array(
+					'action' => '',
+					'method' => 'post',
+					); 
+	protected $_elements = array(); 
+	protected $_helpers = array(
+					); 
+					
 	/*
 	 * name=string, value=string or renderer
 	 */
-	public function render($attr, $content=null) {
-		if (! isset($attr['method'])) {
-			$attr['method'] = 'post';
+	public function render($attr=array(), $content=null) {
+		$attr = isset($this) ? array_merge($this->_attr, $attr) : $attr;
+#		if (! isset($attr['method'])) {
+#			$attr['method'] = 'post';
+#		}
+
+		$content = '';
+		foreach ($this->_elements as $param) {
+			$class = 'A_Html_Form_' . ucfirst($param['type']);
+			if (! class_exists($class)) {
+				include str_replace('_', '/', $class) . '.php';
+			}
+			if (class_exists($class)) {
+				unset($param['type']);
+				$element = new $class();
+				$content .= $element->render($param);
+			}
 		}
+
 		A_Html_Tag::setDefaults($attr, array('method'=>'post', 'action'=>'', ));
 		return A_Html_Tag::render('form', $attr, $content);
+	}
+
+	// Set the method. Is there a setter for the action?
+	public function setAction($action='') {
+		$this->attr['action'] = $action;
+	}
+                             // Optional method to set the Model
+	public function setMethod($method='post') {
+		$this->attr['action'] = $action;
+	}
+                             // Optional method to set the Model
+	public function setModel($model) {
+		$this->model = $model;
+	}
+	
+	public function __call($type, $args) {
+		if(is_array($args[0])) {
+			$params = $args;
+		} else {
+			$params['name'] = $args[0];
+			$params['label'] = $args[1];
+		}
+		if (isset($params['name']) && $params['name']) {
+			$params['type'] = $type;
+			$this->_elements[$params['name']] = $params;
+		}
 	}
 
 }
