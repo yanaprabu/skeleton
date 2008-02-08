cb9e88d03d8daf50bae795f953fae1274e039e3c
arborint <arborint@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-02-08 23:37:11 +0000

    updates for A_Html_Form to add partial() and fieldset support, plus clarified label support

--- skeleton/A/Html/Form.php
+++ skeleton/A/Html/Form.php
@@ -15,24 +15,55 @@ class A_Html_Form {
 	 */
 	public function render($attr=array(), $content=null) {
 		if (isset($this)) {
-			$attr = array_merge($this->_attr, $attr);
-
-			$content = '';
-			foreach ($this->_elements as $param) {
-				$class = 'A_Html_Form_' . ucfirst($param['type']);
+/*
+			foreach ($this->_elements as $params) {
+				$class = 'A_Html_Form_' . ucfirst($params['type']);
 				if (! class_exists($class)) {
 					include str_replace('_', '/', $class) . '.php';
 				}
 				if (class_exists($class)) {
-					unset($param['type']);
+					if (isset($params['content'])) {
+						$str = $params['content'];
+						unset($params['content']);
+					} else {
+						$str = null;
+					}
+					unset($params['type']);
 					$element = new $class();
-					$content .= $element->render($param);
+					$content .= $element->render($params, $str);
 				}
 			}
+*/
+			$content = $this->partial($attr);
 		}
 		
 		A_Html_Tag::setDefaults($attr, array('method'=>'post', 'action'=>'', ));
 		return A_Html_Tag::render('form', $attr, $content);
+	}
+
+	public function partial($attr=array()) {
+		$attr = array_merge($this->_attr, $attr);
+
+		$content = '';
+		foreach ($this->_elements as $params) {
+			$class = 'A_Html_Form_' . ucfirst($params['type']);
+			if (! class_exists($class)) {
+				include str_replace('_', '/', $class) . '.php';
+			}
+			if (class_exists($class)) {
+				if (isset($params['content'])) {
+					$str = $params['content'];
+					unset($params['content']);
+				} else {
+					$str = null;
+				}
+				unset($params['type']);
+				$element = new $class();
+				$content .= $element->render($params, $str);
+			}
+		}
+		
+		return $content;
 	}
 
 	// Set the method. Is there a setter for the action?
@@ -50,17 +81,33 @@ class A_Html_Form {
 		$this->model = $model;
 		return $this;
 	}
+
+	public function reset() {
+		$this->_elements = array(); 
+		return $this;
+	}
 	
 	public function __call($type, $args) {
 		if(is_array($args[0])) {
 			$params = $args[0];
 		} else {
 			$params['name'] = $args[0];
-			$params['label'] = $args[1];
+			if (isset($args[1])) {
+				// fieldset is the exception that does not get a label
+				if ($type == 'fieldset') {
+					$params['content'] = $args[1];
+				} else {
+					$params['label'] = $args[1];
+				}
+			}
+		}
+		if (isset($params['label'])) {
+			$this->_elements[] = array('type'=>'label', 'for'=>$params['name'], 'content'=>$params['label']);
+			unset($params['label']);
 		}
 		if (isset($params['name']) && $params['name']) {
 			$params['type'] = $type;
-			$this->_elements[$params['name']] = $params;
+			$this->_elements[] = $params;
 		}
 		return $this;
 	}
