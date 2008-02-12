caf02d668042deb8d59617f356c0dc78560e6a28
arborint <arborint@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-02-12 04:27:29 +0000

    Added ability to expire session vars. Added support for no namespace just $_SESSION['foo'].

--- skeleton/A/Session.php
+++ skeleton/A/Session.php
@@ -1,12 +1,28 @@
 <?php
 
 class A_Session {
+	protected $_data = array();
+	protected $a_namespace = 'A_Session';
 	protected $namespace;
 	protected $regenerate;
+	protected $isstarted = false;
 	
-	public function __construct($namespace='A_Session', $regenerate=false) {
+	public function __construct($namespace=null, $regenerate=false) {
 		$this->namespace = $namespace;
 		$this->regenerate = $regenerate;
+		$this->_init();
+	}
+	
+	private function _init() {
+		if (session_id() != '') {
+			if ($this->namespace) {
+				$this->_data =& $_SESSION[$this->namespace];
+			} else {
+				$this->_data =& $_SESSION;
+			}
+			$this->isstarted = true;	// already started
+			$this->doExpiration();
+		}
 	}
 	
 	public function setHandler($handler) 
@@ -33,32 +49,58 @@ class A_Session {
 				session_cache_limiter('must-revalidate');
 			}
 			session_start();
+			$this->_init();
 			if ($this->regenerate) {
 				session_regenerate_id();
 			}
 		}
 	}
 	
-	public function get($name) {
+	public function get($name, $default=null) {
 		$this->start();
-		return (isset($_SESSION[$this->namespace][$name]) ? $_SESSION[$this->namespace][$name] : null);
+		return isset($this->_data[$name]) ? $this->_data[$name] : $default;
 	}
 	
-	public function set($name, $value) {
+	public function set($name, $value, $count=0) {
 		if ($name) {
-			$this->start();
-			$_SESSION[$this->namespace][$name] = $value;
+			$this->start($this->namespace);
+			$this->_data[$name] = $value;
+			if ($count > 0) {
+				$this->expire($name, $count);
+			}
 		}
 		return $this;
 	}
 	
 	public function has($name) {
 		$this->start();
-		return isset($_SESSION[$this->namespace][$name]);
+		return isset($this->_data[$name]);
+	}
+	
+	public function expire($name, $count=0) {
+		$this->start();
+		$_SESSION[$this->a_namespace]['expire'][$name] = $count;
+	}
+	
+	protected function doExpiration() {
+		if (isset($_SESSION[$this->a_namespace]['expire'])) {
+			foreach ($_SESSION[$this->a_namespace]['expire'] as $name => $value) {
+				if ($value > 1) {
+					--$_SESSION[$this->a_namespace]['expire'][$name];		// decrement counter if > 1
+				} else {
+					unset($this->_data[$name]);								// remove session var
+					unset($_SESSION[$this->a_namespace]['expire'][$name]);	// remove counter
+				}
+			}
+		}
 	}
 	
 	public function close() {
 		session_write_close();
+	}
+	
+	public function destroy() {
+		session_destroy();
 	}
 	
 }
