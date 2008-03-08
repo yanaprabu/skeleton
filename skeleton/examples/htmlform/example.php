5126f11a0ce67e119d66516c8a5032f5e646b46d
arborint <arborint@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-03-08 21:53:21 +0000

    Changed to show wrapper functionality

--- skeleton/examples/htmlform/example.php
+++ skeleton/examples/htmlform/example.php
@@ -4,6 +4,7 @@ include_once 'A/Html/Form.php';
 
 echo '<br/>Object usage:';
 $form = new A_Html_Form();
+$form->setWrapper('A_Html_Div', array('class'=>'fieldclass', 'style'=>'border:1px solid red;'));
 $form->text('username', 'Enter Username:');
 $form->password('passwd', 'Enter Password:');
 $form->submit('submit', 'login');
@@ -22,11 +23,15 @@ echo A_Html_Form::render(array('action'=>'foo', 'class'=>'bar'),
 	);
 
 echo '<br/>Fieldset usage:';
-$fields = new A_Html_Form();
-$fields->text('username', 'Enter Username:');
-$fields->password('passwd', 'Enter Password:');
+$fields1 = new A_Html_Form();
+$fields1->text('username', 'Enter Username:');
+$fields1->password('passwd', 'Enter Password:');
+$fields2 = new A_Html_Form();
+$fields2->text('email', 'Enter Email:');
+$fields2->password('phone', 'Enter Phone:');
 $form = new A_Html_Form();
-$form->fieldset('set1', $fields->partial());
+$form->fieldset('set1', $fields1->partial());
+$form->fieldset('set2', $fields2->partial());
 $form->submit('submit', 'login');
 echo $form->render();
 	
