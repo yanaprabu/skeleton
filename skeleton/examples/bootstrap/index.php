c3ab7ab09aae186d4b9fcfeef24d6dd72bbb8663
matthijsenator <matthijsenator@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-04-15 20:07:21 +0000

    Updated menu file in examples

--- skeleton/examples/bootstrap/index.php
+++ skeleton/examples/bootstrap/index.php
@@ -1,6 +1,11 @@
 <?php
+ini_set('error_reporting', E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+ini_set('log_errors', 'Off');
+
 require_once('config.php');
 require_once('A/Application.php');
+
 
 class foo 
 {
@@ -16,4 +21,4 @@ $App->set('foo', new foo);
 $App->setPath(dirname($_SERVER['SCRIPT_FILENAME']));
 
 //output content directly to screen
-echo $App->run();
+echo $App->run();dump($App);
