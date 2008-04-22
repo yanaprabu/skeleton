7d47781b369ee0e829e8551655995ddb866c4e0f
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-04-22 01:36:36 +0000

    Added a 'print'=>1 attribute so you can print the value beside the hidden input. This is sometimes needed in forms because hidden is obviously the only form tag that does not display the value in some way.

--- skeleton/A/Html/Form/Hidden.php
+++ skeleton/A/Html/Form/Hidden.php
@@ -4,11 +4,11 @@ include_once 'A/Html/Tag.php';
 class A_Html_Form_Hidden {
 
 	/*
-	 * name=string, value=string
+	 * name=string, value=string, print=boolean(0|1)
 	 */
 	public function render($attr) {
 		$attr['type'] = 'hidden';
-		return A_Html_Tag::render('input', $attr);
+		return A_Html_Tag::render('input', $attr) . (isset($attr['print']) ? $attr['value'] : '');
 	}
 
 }
