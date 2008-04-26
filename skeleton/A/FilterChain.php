be2d1bb69c78e7283b8b937ce3e5cd75e8d76564
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-04-26 23:44:37 +0000

    Added the ability for FilterChain to load Filters and Validators to load Rules. FilterChain can also accept the names of functions like 'strtolower'.

--- skeleton/A/FilterChain.php
+++ skeleton/A/FilterChain.php
@@ -1,24 +1,46 @@
 <?php
 
 class A_FilterChain {
-protected $chain = array();
+	protected $chain = array();
+	protected $dir = 'A_Filter_';
 	
 	public function addFilter ($filter) {
-		if (is_array($filter)) {
-			$this->chain = array_merge($this->chain, $filter);
-		} else {
-			$this->chain[] = $filter;
+		if (is_string($filter)) {
+			$filter = func_get_args();
 		}
+		$this->chain[] = $filter;
 	}
 		
-	public function run ($value, $filter=null) {
+	public function setDir($dir) {
+		$this->dir = strreplace('/', '_', rtrim($dir, '_')) . '_';
+	}
+		
+	public function run($value, $filter=null) {
 		if ($filter) {
 			$this->chain = $filter;
 		}
-		foreach ($this->chain as $filter) {
+		foreach ($this->chain as $key => $filter) {
+			// non-objects are added as arrays
+			if (is_array($filter)) {
+				$name = array_shift($filter);
+				if (function_exists($name)) {
+					$value = call_user_func_array($name, $value);
+					continue;
+				} else {
+					// can use built-in filters and $this->dir will be used
+					if(strstr($name, '_') === false) {
+						$name = $this->dir . ucfirst($name);
+					}
+					include_once str_replace('_', '/', $name) . '.php';
+					$ref = new ReflectionClass($name);
+					$filter = $ref->newInstanceArgs($filter);
+					$this->chain[$key] = $filter;
+					unset($ref);
+				}
+			}
 			$value = $filter->run($value);
 		}
-		return ($value);
+		return $value;
 	}
 	
 }
--- skeleton/A/Validator.php
+++ skeleton/A/Validator.php
@@ -3,13 +3,13 @@
 class A_Validator {
 	protected $chain = array();
 	protected $errorMsg = array();
+	protected $dir = 'A_Rule_';
 	
-	public function addRule(&$rule) {
-		if (is_array($rule)) {
-			$this->chain = array_merge($this->chain, $rule);
-		} else {
-			$this->chain[] = $rule;
+	public function addRule($rule) {
+		if (is_string($rule)) {
+			$rule = func_get_args();
 		}
+		$this->chain[] = $rule;
 	}
 		
 	public function validate ($container, $rule=null) {
@@ -18,6 +18,19 @@ class A_Validator {
 		}
 		$this->errorMsg = array();
 		foreach ($this->chain as $key => $rule) {
+			// class names with params are added as arrays
+			if (is_array($rule)) {
+				$name = array_shift($rule);
+				// can use built-in rules and $this->dir will be used
+				if(strstr($name, '_') === false) {
+					$name = $this->dir . ucfirst($name);
+				}
+				include_once str_replace('_', '/', $name) . '.php';
+				$ref = new ReflectionClass($name);
+				$rule = $ref->newInstanceArgs($rule);
+				$this->chain[$key] = $rule;
+				unset($ref);
+			}
 			if (! $this->chain[$key]->isValid($container)) {
 				$this->errorMsg[] = $this->chain[$key]->getErrorMsg();
 			}
