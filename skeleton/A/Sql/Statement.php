a71f6001854761a5184e2863940ca51a7da82083
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-05-08 07:01:25 +0000

    Updated version with check in condition() for when 1st parameter is logic and 2nd parameter is an array

--- skeleton/A/Sql/Statement.php
+++ skeleton/A/Sql/Statement.php
@@ -5,6 +5,27 @@ class A_Sql_Statement {
 	protected $escapeListeners = array();	
 	
 	protected function condition(&$list, $arg1, $arg2=null, $arg3=null) {
+		if ($arg1) {	
+			if (($arg3 === null) && ! is_array($arg2)) {
+				$logic = 'AND';		// if 3rd null and arg2 not an array then it is ('field', 'value')
+			} else {
+				$logic = $arg1;		// if 3rd arg is set then it is logic
+				$arg1 = $arg2;		// move args down
+				$arg2 = $arg3;
+			}
+				
+			include_once('A/Sql/Expression.php');
+			$expression = new A_Sql_Expression($arg1, $arg2);
+			$this->escapeListeners[] = $expression; 		
+			if ($list) {
+				$list[] = $logic;
+	        }
+			$list[] = $expression;
+		}    
+        return $this;
+    }
+/*
+    protected function condition(&$list, $arg1, $arg2=null, $arg3=null) {
 		if (!$arg1) return $this; //no need to proceed if arguments are empty
 		$arguments = array_filter(array($arg1, $arg2, $arg3));
 		$numArguments = count($arguments);
@@ -22,6 +43,7 @@ class A_Sql_Statement {
 		$list[] = $expression;
         return $this;
     }	
+*/
 		
 	public function setDb($db) {
 		$this->db = $db;
