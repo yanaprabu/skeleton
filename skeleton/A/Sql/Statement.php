babce6877be14f7131205d3dd0db1f47141e32e6
jcartdev <jcartdev@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-05-08 05:31:58 +0000

    - A_Sql_Statement condition method refactored

--- skeleton/A/Sql/Statement.php
+++ skeleton/A/Sql/Statement.php
@@ -6,25 +6,17 @@ class A_Sql_Statement {
 	
 	protected function condition(&$list, $arg1, $arg2=null, $arg3=null) {
 		if (!$arg1) return $this; //no need to proceed if arguments are empty
-		if ($arg3 === null) {
-			if ($arg2 === null) {
-				$logic = 'AND';
-			} else {
-				$logic = $arg1;
-				$arg1 = $arg2;
-				$arg2 = null;
-			}	
-		} else {
-			$logic = $arg1;	// if 3rd arg is set then it is logic
-			$arg1 = $arg2;	// move args down
-			$arg2 = $arg3;
+		$arguments = array_filter(array($arg1, $arg2, $arg3));
+		$numArguments = count($arguments);
+		if ($numArguments < 2) {
+			array_unshift($arguments, 'AND');
 		}
-				
+
 		include_once('A/Sql/Expression.php');
-		$expression = new A_Sql_Expression($arg1, $arg2);
+		$expression = new A_Sql_Expression($arguments[1], isset($arguments[2]) ? $arguments[2] : null);
 		$this->escapeListeners[] = $expression; 		
 		if ($list) {
-			$list[] = $logic;
+			$list[] = $arguments[0];
 		}
 		$list[] = $expression;
         return $this;
