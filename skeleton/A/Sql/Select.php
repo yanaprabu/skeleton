80e81c839394831f923218f4620f9d6b9c709384
jcartdev <jcartdev@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-05-29 01:36:50 +0000

    Refactored A_Sql_Select and cleaned up render() method

--- skeleton/A/Sql/Select.php
+++ skeleton/A/Sql/Select.php
@@ -2,102 +2,101 @@
 require_once 'A/Sql/Statement.php';
 
 class A_Sql_Select extends A_Sql_Statement {
-	protected $table;
-	protected $columns;
-	protected $where;
-	protected $joins = array();
-	protected $having;
-	protected $groupby;			
-	protected $orderby;
-	
+	protected $render = array();
+	protected $pieces = array(
+		'tables' 	=> null,
+		'columns' 	=> null,
+		'joins' 	=> null,
+		'where' 	=> null,
+		'having' 	=> null,
+		'groupby' 	=> null,
+		'orderby' 	=> null,
+	);
+
 	public function columns() {
-		include_once('A/Sql/Columns.php');
-		$this->columns = new A_Sql_Columns(func_get_args());
+		require_once 'A/Sql/Columns.php';		
+		$this->pieces['columns'] = new A_Sql_Columns(func_get_args());
 		return $this;
 	}
-
-	public function from($table) {
-		include_once('A/Sql/Table.php');
-		$this->table = new A_Sql_Table($table);
-		return $this;
-	}
-
-	public function join($table1, $column1, $table2, $column2) {
-		include_once('A/Sql/Join.php');
-		$this->joins[$table2] = new A_Sql_Join($table1, $column1, $table2, $column2);
-		return $this;
-	}
-
-	public function where($arg1, $arg2=null, $arg3=null) {
-		if (!$this->where) {
-			include_once('A/Sql/Where.php');		
-			$this->where = new A_Sql_Where();
+	
+	public function getColumns() {
+		if (!$this->pieces['columns']) {
+			return array();
 		}
-		$this->where->addExpression($arg1, $arg2, $arg3);
+		return $this->pieces['columns']->getColumns();
+	}	
+	
+	public function from() {
+		require_once 'A/Sql/Table.php';	
+		$this->pieces['tables'] = new A_Sql_Table(func_get_args());
+		return $this;
+	}
+	
+	public function where($argument1, $argument2=null, $argument3=null) {
+		if (!$this->pieces['where']) {
+			require_once 'A/Sql/Where.php';		
+			$this->pieces['where'] = new A_Sql_Where();
+		}
+		$this->pieces['where']->addExpression($argument1, $argument2, $argument3);
 		return $this;		
 	}
 
 	public function orWhere($data, $value=null) {
-		if (!$this->where) {
-			include_once('A/Sql/Where.php');
-			$this->where = new A_Sql_Where();
+		if (!$this->pieces['where']) {
+			require_once 'A/Sql/Where.php';		
+			$this->pieces['where'] = new A_Sql_Where();
 		}
-		$this->where->addExpression('OR', $data, $value);
+		$this->pieces['where']->addExpression('OR', $data, $value);
 		return $this;		
 	}
 
-	public function having($arg1, $arg2=null, $arg3=null) {
-		if (!$this->having) {
-			include_once('A/Sql/Having.php');
-			$this->having = new A_Sql_Having();
+	public function having($argument1, $argument2=null, $argument3=null) {
+		if (!$this->pieces['having']) {
+			require_once 'A/Sql/Having.php';
+			$this->pieces['having'] = new A_Sql_Having();
 		}
-		$this->having->addExpression($arg1, $arg2, $arg3);
-		return $this;			
+		$this->pieces['having']->addExpression($argument1, $argument2, $argument3);
+		return $this;		
 	}
 
 	public function orHaving($data, $value=null) {
-		if (!$this->having) {
-			include_once('A/Sql/Having.php');
-			$this->having = new A_Sql_Having();
+		if (!($this->pieces['having'] instanceof A_Sql_Having)) {
+			require_once 'A/Sql/Having.php';
+			$this->pieces['having'] = new A_Sql_Having();
 		}
-		$this->having->addExpression('OR', $data, $value);
+		$this->pieces['having']->addExpression('OR', $data, $value);
 		return $this;		
-	}
+	}	
 
 	public function groupBy($columns) {
-		include_once('A/Sql/Groupby.php');
-		$this->groupby = new A_Sql_Groupby($columns);	
+		require_once 'A/Sql/Groupby.php';
+		$this->pieces['grouby'] = new A_Sql_Groupby($columns);	
 		return $this;
 	}
 
 	public function orderBy($columns) {
-		include_once('A/Sql/Orderby.php');
-		$this->orderby = new A_Sql_Orderby($columns);	
+		require_once 'A/Sql/Orderby.php';
+		$this->pieces['orderby'] = new A_Sql_Orderby($columns);	
 		return $this;
-	}
-
-	public function render() {
-		if (!$this->table) return;
-		
-		$joins = '';
-		if (count($this->joins)) {
-			foreach ($this->joins as $join) {
-				$joins .= $join->render();
-			}
-		}
-
-		$table = $this->table->render();
-		$columns = $this->columns ? $this->columns->render() : '*';
-		$where   = $this->where ? ' '. $this->where->setDb($this->db)->render() : '';
-		$having  = $this->having ? ' '. $this->having->setDb($this->db)->render() : '';
-		$orderby = $this->orderby ? ' '. $this->orderby->render() : '';
-		$groupby = $this->groupby ? ' '. $this->groupby->render() : '';
-		
-		return "SELECT $columns FROM $table$joins$having$where$orderby$groupby";
 	}
 
 	public function __toString() {
 		return $this->render();
 	}
 
+	public function render() {
+		foreach ($this->pieces as $name => $piece) {
+			$output = null;
+			if (method_exists($piece, 'render')) {
+				if (method_exists($piece, 'setDb')) {
+					$piece->setDb($this->getDb());
+				}
+				$output = $piece->render();
+			}
+			$this->replace['['.$name.']'] = strlen($output) ? ' '. $output : $output; //add spacing
+		}
+		
+		$sql = "SELECT[columns] FROM [tables][joins][having][where][orderby][groupby]";
+		return str_replace(array_keys($this->replace), array_values($this->replace), $sql);
+	}
 }
