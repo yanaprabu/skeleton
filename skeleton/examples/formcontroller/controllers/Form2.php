7796c3971badc0ebff15f0664354d0159ac5bd07
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-05-31 20:05:54 +0000

    Changed position of HTML form code in example.

--- skeleton/examples/formcontroller/controllers/Form2.php
+++ skeleton/examples/formcontroller/controllers/Form2.php
@@ -41,16 +41,6 @@ class Form2 {
 		
 		$model->run($locator);
 		
-		// create HTML form generator
-		$form = new A_Html_Form();
-		$form->setModel($model)
-				->setWrapper('A_Html_Div', array('class'=>'fieldclass', 'style'=>'border:1px solid red;'))
-				->select(array('name'=>'field1', 'values'=>array(5,10,15), 'labels'=>array('five','ten','fifteen'), 'id'=>'field1', 'label'=>'Field 1 (Numbers only in range 1-10)', 'after'=>$field1->getErrorMsg()))
-				->text(array('name'=>'field2', 'id'=>'field2', 'label'=>'Field 2 (Must match Field 1)', 'after'=>$field2->getErrorMsg()))
-				->text(array('name'=>'field3', 'id'=>'field3', 'label'=>'Field 3 (Letters only min length 5)', 'after'=>$field3->getErrorMsg()))
-				->text(array('name'=>'field4', 'id'=>'field4', 'label'=>'Field 4 (Convert letters to uppercase)', 'after'=>$field4->getErrorMsg()))
-				->submit('submit', 'Submit');
-
 		if ($model->isValid()) {
 			echo 'DONE<br/><br/><a href="../">Return to Examples</a>';
 		} else {
@@ -61,6 +51,15 @@ class Form2 {
 				$model->set('field4', 'init');
 			}
 
+			// create HTML form generator
+			$form = new A_Html_Form();
+			$form->setModel($model)
+					->setWrapper('A_Html_Div', array('class'=>'fieldclass', 'style'=>'border:1px solid red;'))
+					->select(array('name'=>'field1', 'values'=>array(5,10,15), 'labels'=>array('five','ten','fifteen'), 'id'=>'field1', 'label'=>'Field 1 (Numbers only in range 1-10)', 'after'=>$field1->getErrorMsg()))
+					->text(array('name'=>'field2', 'id'=>'field2', 'label'=>'Field 2 (Must match Field 1)', 'after'=>$field2->getErrorMsg()))
+					->text(array('name'=>'field3', 'id'=>'field3', 'label'=>'Field 3 (Letters only min length 5)', 'after'=>$field3->getErrorMsg()))
+					->text(array('name'=>'field4', 'id'=>'field4', 'label'=>'Field 4 (Convert letters to uppercase)', 'after'=>$field4->getErrorMsg()))
+					->submit('submit', 'Submit');
 			echo $form->render();
 #			include 'templates/example_form2.php';
 		}
