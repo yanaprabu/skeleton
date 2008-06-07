3f3ec94a404c6501d90c486ed7c50502f6dd14b6
m.p.abeelen <m.p.abeelen@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-06-07 12:13:19 +0000

    Moved specs dir

---
+++ specs/Db/ActiverecordSpec.php
@@ -0,0 +1,21 @@
+<?php
+
+class DescribeActiverecord extends PHPSpec_Context
+{
+
+    public function itShouldSetTable()
+    {
+        $this->pending();
+    }
+
+    public function itShouldSetPrimaryKey()
+    {
+        $this->pending();
+    }
+
+    public function itShouldRequireDbConnention()
+    {
+        $this->pending();
+    }
+
+}
