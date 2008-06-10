d2f0f3ca81c4d44709fc6067353fac18d6f51a18
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-06-10 08:09:58 +0000

    Update formcontroller and validator examples because of changes in the Validator and Model classes getErrorMsg()

--- examples/formcontroller/controllers/Form3.php
+++ examples/formcontroller/controllers/Form3.php
@@ -23,10 +23,10 @@ class Form3 extends A_Controller_Action {
 			$form = new A_Html_Form();
 			$form->setModel($model)
 					->setWrapper('A_Html_Div', array('class'=>'fieldclass', 'style'=>'border:1px solid red;'))
-					->select(array('name'=>'field1', 'values'=>array(5,10,15), 'labels'=>array('five','ten','fifteen'), 'id'=>'field1', 'label'=>'Field 1 (Numbers only in range 1-10)', 'after'=>$model->getField('field1')->getErrorMsg()))
-					->text(array('name'=>'field2', 'id'=>'field2', 'label'=>'Field 2 (Must match Field 1)', 'after'=>$model->getField('field1')->getErrorMsg()))
-					->text(array('name'=>'field3', 'id'=>'field3', 'label'=>'Field 3 (Letters only min length 5)', 'after'=>$model->getField('field3')->getErrorMsg()))
-					->text(array('name'=>'field4', 'id'=>'field4', 'label'=>'Field 4 (Convert letters to uppercase)', 'after'=>$model->getField('field4')->getErrorMsg()))
+					->select(array('name'=>'field1', 'values'=>array(5,10,15), 'labels'=>array('five','ten','fifteen'), 'id'=>'field1', 'label'=>'Field 1 (Numbers only in range 1-10)', 'after'=>$model->getField('field1')->getErrorMsg(', ')))
+					->text(array('name'=>'field2', 'id'=>'field2', 'label'=>'Field 2 (Must match Field 1)', 'after'=>$model->getField('field2')->getErrorMsg(', ')))
+					->text(array('name'=>'field3', 'id'=>'field3', 'label'=>'Field 3 (Letters only min length 5)', 'after'=>$model->getField('field3')->getErrorMsg(', ')))
+					->text(array('name'=>'field4', 'id'=>'field4', 'label'=>'Field 4 (Convert letters to uppercase)', 'after'=>$model->getField('field4')->getErrorMsg(', ')))
 					->submit('submit', 'Submit');
 			echo $form->render();
 		}
--- examples/rule/example_validator.php
+++ examples/rule/example_validator.php
@@ -2,30 +2,44 @@
 error_reporting(E_ALL);
 require_once('config.php');
 require_once('A/Http/Request.php');
-require_once('A/Rule.php');
+require_once('A/Validator.php');
+require_once('A/Rule/Alpha.php');
+require_once('A/Rule/Length.php');
 require_once('A/Rule/Match.php');
+require_once('A/Rule/Numeric.php');
 
 $request = new A_Http_Request();
 
-$rule = new A_Rule_Match('one', 'two', 'Fields do not match');
+$validator = new A_Validator();
+$alpha = new A_Rule_Alpha('one', 'One is not alpha');
+$length = new A_Rule_Length('two', 5, null, 'Two must be 5 characters long');
+$match = new A_Rule_Match('one', 'two', 'Fields do not match');
+$numeric = new A_Rule_Numeric('two', 'Two is not numeric');
 
-if ($rule->isValid($request)) {
+$validator->addRule($alpha);
+$validator->addRule($match);
+#$validator->addRule($length);
+$validator->addRule('A_Rule_Length', 'two', 5, null, 'Two must be 5 characters long');
+
+$validator->validate($request);
+
+if ($validator->isValid($request)) {
 	$errmsg = 'OK';
 } else {
-	$errmsg = $rule->getErrorMsg();
+	$errmsg = print_r($validator->getErrorMsg(), 1);
 }
 
 ?>
 <html>
 <head>
-<title>Rule Example</title>
+<title>Validator Example</title>
 </head>
 <body>
-<h2>Rule Example</h2>
-The rule will match if the two values are equal.<br/>
+<h2>Validator Example</h2>
+One must be Alpha. Two must be Numeric. The two values must match.<br/>
 <form action="" method="post">
-Status: <span style="color:red"><?php echo $errmsg; ?></span><br/>
-<input type="text" name="one" value="<?php echo isset($_POST['one']) ? $_POST['one'] : null; ?>"/><br/>
-<input type="text" name="two" value="<?php echo isset($_POST['two']) ? $_POST['two'] : null; ?>"/><br/>
+Status: <span style="color:red"><pre><?php echo $errmsg; ?></pre></span><br/>
+One: <input type="text" name="one" value="<?php echo isset($_POST['one']) ? $_POST['one'] : null; ?>"/><br/>
+Two: <input type="text" name="two" value="<?php echo isset($_POST['two']) ? $_POST['two'] : null; ?>"/><br/>
 <input type="submit"/><br/>
 </body>
