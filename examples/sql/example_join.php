298766fc37031acf5a70b0b627cd098618349c98
jcartdev <jcartdev@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-06-19 08:38:45 +0000

    - Updated sql example_join.php

--- examples/sql/example_join.php
+++ examples/sql/example_join.php
@@ -8,7 +8,8 @@ $select = new A_Sql_Select();
 echo $select
 	->columns()
 	->from('tablefoo')
-	->leftjoin('foo', 'bar')->on('foo1.column1', 'column2')->on('column3', 'column4')
+	->join('foo0', 'bar0', 'LEFT')
+	->leftjoin('foo', 'bar')->on('foo1.column1', 'column2')->on('OR', 'column3', 'column4')
 	->innerjoin('foo2', 'bar2')->on('column5', 'column6')
 	->innerjoin('foo3', 'bar3')->on(array('column7' => 'column8', 'column9' => 'column10'))
 	->where('foo', 'bar')
