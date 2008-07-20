cdf5b6fd51fac57dede6ef4e40289260d406380a
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-07-20 05:53:18 +0000

    removed file_exists check from autoload()

--- A/functions/a_autoload.php
+++ A/functions/a_autoload.php
@@ -2,9 +2,7 @@
 
 function a_autoload($class) {
 	$file = str_replace(array('_','-'), array('/','_'), $class) . '.php';
-	if (file_exists($file)) {
-		include($file);
-	}
+	include($file);
 }
 
 spl_autoload_register('a_autoload');
