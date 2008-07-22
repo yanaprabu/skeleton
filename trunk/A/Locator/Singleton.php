ef737ada91e4e6bab2cd826ff746622d6ddab340
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-07-22 20:29:28 +0000

    Add class to allow static Singleton access to Locator

---
+++ trunk/A/Locator/Singleton.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * A_Locator_Singleton
+ * 
+ * Allows static Singleton access to an A_Locator (or other) obejct
+ *
+ * @package A_Locator
+ */
+
+class A_Locator_Singleton {
+	private static $_instance;
+       
+	function setInstance($instance) {
+		$this->_instance = $instance;
+	}
+
+	function getInstance() {
+		if (! isset($this->_instance)) {
+			include_once "A/Locator.php";
+			$this->_instance = new A_Locator();
+		}
+		return $this->_instance;
+	}
+
+}
