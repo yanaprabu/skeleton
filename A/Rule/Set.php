53047c38f67c87a04a2af97526bc2125d2f79e7d
Luke.Visinoni <Luke.Visinoni@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-08-09 02:34:16 +0000

    Somehow I managed to forget to give A_Rule_Set::isValid() a return value. This fixes that problem. Doh!

--- A/Rule/Set.php
+++ A/Rule/Set.php
@@ -2,61 +2,62 @@
 class A_Rule_Set implements A_Rule_Interface {
 
     protected $chain = array();
-	protected $excludes = array();		// array of names not to be validated
-	protected $errorMsg = array();
-	protected $dir = 'A_Rule_';
+    protected $excludes = array();		// array of names not to be validated
+    protected $errorMsg = array();
+    protected $dir = 'A_Rule_';
     
-	public function addRule(A_Rule_Interface $rule) {
+    public function addRule(A_Rule_Interface $rule) {
     
-		if (is_string($rule)) {
-			$rule = func_get_args();
-		}
-		$this->chain[] = $rule;
-        return $this;
-	
+	if (is_string($rule)) {
+	    $rule = func_get_args();
+	}
+	$this->chain[] = $rule;
+	return $this;
+    
     }
 
-	public function exclude($names=array()) {
-	
-    	if (is_string($names)) {
-			$names = array($names);
-		}
-		$this->excludes = $names;
-        return $this;
-	
+    public function exclude($names=array()) {
+    
+	if (is_string($names)) {
+	    $names = array($names);
+	}
+	$this->excludes = $names;
+	return $this;
+    
     }
     
     public function isValid($container) {
     
-		$this->errorMsg = array();
-		foreach ($this->chain as $key => $rule) {
-			if ($this->excludes && (in_array($rule->getName(), $this->excludes))) {
-				continue;
-			}
-			// class names with params are added as arrays
-			if (is_array($rule)) {
-				$name = array_shift($rule);
-				// can use built-in rules and $this->dir will be used
-				if(strstr($name, '_') === false) {
-					$name = $this->dir . ucfirst($name);
-				}
-				include_once str_replace('_', '/', $name) . '.php';
-				$ref = new ReflectionClass($name);
-				$rule = $ref->newInstanceArgs($rule);
-				$this->chain[$key] = $rule;
-				unset($ref);
-			}
-			if (! $this->chain[$key]->isValid($container)) {
-				$this->errorMsg[$this->chain[$key]->getName()][get_class($this->chain[$key])] = $this->chain[$key]->getErrorMsg();
-			}
+	$this->errorMsg = array();
+	foreach ($this->chain as $key => $rule) {
+	    if ($this->excludes && (in_array($rule->getName(), $this->excludes))) {
+		    continue;
+	    }
+	    // class names with params are added as arrays
+	    if (is_array($rule)) {
+		$name = array_shift($rule);
+		// can use built-in rules and $this->dir will be used
+		if(strstr($name, '_') === false) {
+		    $name = $this->dir . ucfirst($name);
 		}
+		include_once str_replace('_', '/', $name) . '.php';
+		$ref = new ReflectionClass($name);
+		$rule = $ref->newInstanceArgs($rule);
+		$this->chain[$key] = $rule;
+		unset($ref);
+	    }
+	    if (! $this->chain[$key]->isValid($container)) {
+		$this->errorMsg[$this->chain[$key]->getName()][get_class($this->chain[$key])] = $this->chain[$key]->getErrorMsg();
+	    }
+	}
+	return empty($this->errorMsg);
     
     }
-	
-	public function getErrorMsg($separator=null) {
     
-		return $separator === null ? $this->errorMsg : implode($separator, $this->errorMsg);
-	
+    public function getErrorMsg($separator=null) {
+    
+	return $separator === null ? $this->errorMsg : implode($separator, $this->errorMsg);
+    
     }
 
 }
