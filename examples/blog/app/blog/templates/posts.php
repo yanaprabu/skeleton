36251dcb8eb61ec494b2aeb2fd2f8e2c620cd15f
m.p.abeelen <m.p.abeelen@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-09-04 07:12:05 +0000

    Updates to blog example

--- examples/blog/app/blog/templates/posts.php
+++ examples/blog/app/blog/templates/posts.php
@@ -1,7 +1,7 @@
 
-	<h3>This is a list of all posts</h3>
-	<p>Todo: add paging here :) Sorting, filtering?</p>
-	<ul>
+<h3>This is a list of all posts</h3>
+
+<ul>
 	<?php foreach($content as $article){
 		echo '<li>';
 		echo '<h4><a href="'. $article['permalink'] . '">' . $article['title'] . '</a></h4>';
@@ -10,4 +10,4 @@
 		echo '<p>' .  $article['content'] . '</p>';
 		echo '</li>';
 	} ?>
-	</ul>
+</ul>
