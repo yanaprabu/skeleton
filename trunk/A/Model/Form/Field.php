f46632747be24dc5eface72ff6cf67fb5d7efaf5
m.p.abeelen@gmail.com <m.p.abeelen@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2008-12-15 07:33:35 +0000

    Changed A_Model_Request to A_Model_Form

---
+++ trunk/A/Model/Form/Field.php
@@ -0,0 +1,60 @@
+<?php
+include_once 'A/Model/Field.php';
+
+class A_Model_Form_Field extends A_Model_Field {
+	// from Input Controller
+	public $renderer = null;
+	// from Form Controller
+	public $type = '';
+	public $addtype = '';
+	
+	public function setType($type, $addtype='') {
+		$this->type = $type;
+		$this->addtype = $addtype;
+		return $this;
+	}
+	
+	public function setRenderer($renderer) {
+		$this->renderer = $renderer;
+		return $this;
+	}
+	
+	public function render() {
+		// if no value and addtype set then use it
+		if ($this->addtype && ($this->value == '')) {
+			$savetype = $this->type;
+			$this->type = $this->addtype;
+		} else {
+			$savetype = null;
+		}
+		if (isset($this->type['renderer'])) {
+			if (! isset($this->renderer)){
+				$this->renderer = $this->type['renderer'];
+				unset($this->type['renderer']);
+			}
+		}
+		// string is name of class with underscores in loadable convention
+		if (is_string($this->renderer)){
+			// load locator if not loaded
+			include_once 'A/Locator.php';
+			if (A_Locator::loadClass($this->renderer)) {
+				// instantiate render passing the array of field
+				$this->renderer = new $this->renderer();
+			}
+		}
+		if (isset($this->renderer) && method_exists($this->renderer, 'render')) {
+			// set name and value in array passed to renderer
+			$this->type['name'] = $this->name;
+			$this->type['value'] = $this->value;
+			return $this->renderer->render($this->type);
+		}
+		
+		// restore type
+		if ($savetype) {
+			$this->type = $savetype;
+		}
+		
+		return $this->value;
+	}
+
+}
