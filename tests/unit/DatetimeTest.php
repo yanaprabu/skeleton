5a8c550f4318d62ceacdaad815ef4c9983d0ec73
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-01-20 18:29:58 +0000

    Renamed A_DateTime_Period to A_DateTime_Range

--- tests/unit/DatetimeTest.php
+++ tests/unit/DatetimeTest.php
@@ -2,7 +2,7 @@
 require_once('A/Datetime.php');
 require_once('A/DateTime/Interval.php');
 
-Mock::Generate ('A_DateTime_Period');
+Mock::Generate ('A_DateTime_Range');
 Mock::Generate ('A_DateTime_Duration');
 
 class DatetimeTest extends UnitTestCase {
@@ -105,30 +105,30 @@ class DatetimeTest extends UnitTestCase {
 		
 	}
 
-	function testIsWithinPeriodReturnsTrue()	{
+	function testIsWithinRangeReturnsTrue()	{
 		$date1 = new A_DateTime();
 		$date1->parseDate ('2008/12/22');
 		$date2 = new A_DateTime();
 		$date2->parseDate ('2008/12/21');
 		$date3 = new A_DateTime();
 		$date3->parseDate ('2008/12/23');
-		$period = new MockA_DateTime_Period();
-		$period->setReturnValue ('getTimestampBefore', $date2->getTimestamp());
-		$period->setReturnValue ('getTimestampAfter', $date3->getTimestamp());
-		$this->assertTrue ($date1->isWithin ($period));
+		$range = new MockA_DateTime_Range();
+		$range->setReturnValue ('getTimestampBefore', $date2->getTimestamp());
+		$range->setReturnValue ('getTimestampAfter', $date3->getTimestamp());
+		$this->assertTrue ($date1->isWithin ($range));
 	}
 
-	function testIsNotWithinPeriodReturnsFalse()	{
+	function testIsNotWithinRangeReturnsFalse()	{
 		$date1 = new A_DateTime();
 		$date1->parseDate ('2008/12/20');
 		$date2 = new A_DateTime();
 		$date2->parseDate ('2008/12/21');
 		$date3 = new A_DateTime();
 		$date3->parseDate ('2008/12/23');
-		$period = new MockA_DateTime_Period();
-		$period->setReturnValue ('getTimestampBefore', $date2->getTimestamp());
-		$period->setReturnValue ('getTimestampAfter', $date3->getTimestamp());
-		$this->assertFalse ($date1->isWithin ($period));
+		$range = new MockA_DateTime_Range();
+		$range->setReturnValue ('getTimestampBefore', $date2->getTimestamp());
+		$range->setReturnValue ('getTimestampAfter', $date3->getTimestamp());
+		$this->assertFalse ($date1->isWithin ($range));
 
 	
 }
