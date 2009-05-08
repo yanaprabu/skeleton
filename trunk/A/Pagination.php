1b1897ac1355b1ba61fec113000bccd45ed7aead
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-08 08:27:25 +0000

    A_Pagination changes in method names. Added comment blocks. Updated tests.

--- trunk/A/Pagination.php
+++ trunk/A/Pagination.php
@@ -1,51 +1,99 @@
 <?php
+/**
+ * A_Pagination
+ *
+ * Component to paginate items from a datasource
+ *
+ * @author Cory Kaufman, Christopher Thompson
+ * @package A_Pagination
+ * @version @package_version@
+ */
 
 class A_Pagination	{
 
 	protected $datasource;
 	protected $pageSize = 10;
 	protected $currentPage = 1;
-
+	protected $numPages = false;
+	
+	/**
+	 * @param 
+	 * @type 
+	 */
 	public function __construct(A_Pagination_Datasource_Interface $datasource, $pageSize=10, $currentPage=1)	{
 		$this->datasource = $datasource;
 		$this->pageSize = $pageSize;
 		$this->currentPage = $currentPage;
 	}
 
-	public function setCurrentPage($currentPage)	{
-		$this->currentPage = $this->isValid($currentPage) ? $currentPage : $this->currentPage;
+	/**
+	 * @param integer - number of last page
+	 * @type 
+	 */
+	public function setCurrentPage($page)	{
+		if (($page >= $this->getFirstPage()) && ($page <= $this->getLastPage())) {
+			$this->currentPage = $page;
+		}
 	}
 
+	/**
+	 * @param 
+	 * @type integer - number of current page
+	 */
 	public function getCurrentPage()	{
 		return $this->currentPage;
 	}
 
-	// should this just be called items() because we don't use get*() on most methods
+	/**
+	 * @param 
+	 * @type array - of items
+	 */
 	public function getItems()	{
-		return $this->datasource->getItems($this->getOffset($this->currentPage), $this->pageSize);
+		return $this->datasource->getItems($this->getFirstItem($this->currentPage), $this->pageSize);
 	}
 
-	// should this just be called numItems() because we don't use get*() on most methods
+	/**
+	 * @param 
+	 * @type integer - number of total items
+	 */
 	public function getNumItems()	{
-		// should we cache this value because it may be expensive for datasource to get this value?
-		return $this->datasource->getNumItems();
+		if ($this->numPages === false) {
+			$this->numPages = $this->datasource->getNumItems();
+		}
+		return $this->numPages;
 	}
 
+	/**
+	 * @param 
+	 * @type integer - number of last page
+	 */
 	public function getFirstPage()	{
 		return 1;
 	}
 
+	/**
+	 * @param 
+	 * @type integer - number of last page
+	 */
 	public function getLastPage()	{
 		// do we cache this value and only recalculate when getItems()/getNumItems called
 		return ceil($this->getNumItems() / $this->pageSize);
 	}
 
+	/**
+	 * @param 
+	 * @type integer - position of first item on current page
+	 */
 	public function getFirstItem()	{
-		return $this->getOffset($this->currentPage);
+		return (($this->currentPage - 1) * $this->pageSize) + 1;
 	}
 
+	/**
+	 * @param 
+	 * @type integer - position of last item on current page
+	 */
 	public function getLastItem()	{
-		$lastItem = $this->getOffset($this->currentPage) + $this->pageSize - 1;
+		$lastItem = $this->getFirstItem() + $this->pageSize - 1;
 		$numItems = $this->getNumItems();
 		if ($lastItem > $numItems) {
 			$lastItem = $numItems;
@@ -53,16 +101,21 @@ class A_Pagination	{
 		return $lastItem;
 	}
 
-	public function isValid($page)	{
-		return ($page >= $this->firstPage() && $page <= $this->lastPage()) ? true : false;
+	/**
+	 * @param 
+	 * @type boolean - true if page in range of 1..last page
+	 */
+	public function isPage($page)	{
+		$page += $this->currentPage;
+		return ($page >= $this->firstPage()) && ($page <= $this->lastPage());
 	}
 
-	protected function getOffset($page)	{
-		// Should this be put in firstItem($page=0)? If 0 then use $this->currentPage.
-		// Also the isValid check could be skip for the currentPage.
-		if ($this->isValid($page)) {
-			return (($page - 1) * $this->pageSize) + 1;
-		}
+	/**
+	 * @param 
+	 * @type boolean - true if number of pages > 1
+	 */
+	public function hasPages()	{
+		return $this->getNumItems() >= $this->pageSize;
 	}
 
 }
