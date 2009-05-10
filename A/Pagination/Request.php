d6e7739f62bc164ad26411fd71353ee2c4384a18
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-10 21:51:16 +0000

    Updated A_Pagination_Request, added A_Pagination_Url

--- A/Pagination/Request.php
+++ A/Pagination/Request.php
@@ -2,17 +2,29 @@
 
 class A_Pagination_Request	{
 
+	protected $adapter;
+	protected $pager;
+
 	public function __construct ($adapter, $pageSize = 10, $currentPage = 1)	{
+		$this->adapter = $adapter;
 		$this->pager = new A_Pagination ($adapter, $pageSize, $currentPage);
+		$this->url = new A_Pagination_Url();
 		$this->process(); // would we ever need to not call $process?
 	}
 
 	public function process()	{
 		$this->pager->setcurrentPage (intval ($this->get ('page'), $this->pager->getFirstPage()));
-		if ($this->get ('page_size')) $this->pager->setPageSize (intval ($this->get ('page_size')));
-		// Set up adapter? url helper? view helper?
+		$this->url->set ('page', $this->get ('page'));
+		if ($size = $this->get ('page_size'))	{
+			$this->pager->setPageSize (intval ($size));
+			$this->url->set ('page_size', $size);
+		}
+		if ($orderBy = $this->get ('order_by'))	{
+			list ($field, $dir) = explode ('_', $orderBy);
+			$this->adapter->setOrderBy ($field, ($dir == 'asc' ? false : true));
+			$this->url->set ('order_by', $field . '_' . $dir);
+		}
 	}
-
 
 	public function get ($key, $default = '')	{
 		return (isset ($_GET[$key]) ? $_GET[$key] : $default);
