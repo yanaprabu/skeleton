96e4adee9991ed4ab1c3e76b28148a027a002e54
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-15 07:33:42 +0000

    Slimmed down A_Pagination_Request

--- trunk/A/Pagination/Request.php
+++ trunk/A/Pagination/Request.php
@@ -6,34 +6,21 @@ class A_Pagination_Request	{
 	protected $pager;
 
 	public function __construct ($adapter, $pageSize, $currentPage)	{
-		$this->pager = new A_Pagination ($adapter, $pageSize, $currentPage);
-		$this->adapter = func_get_arg(0);
-		$this->url = new A_Pagination_Url();
-		$this->process(); // would we ever need to not call $process?
+		$this->pager = new A_Pagination($adapter, $pageSize, $currentPage);
+		$this->process();
 	}
 
 	public function process()	{
-		$this->pager->setcurrentPage (intval ($this->get ('page'), $this->pager->getFirstPage()));
-		$this->url->set ('page', $this->get ('page'));
-		if ($pageSize = $this->get ('page_size'))	{
-			$this->pager->setPageSize (intval ($pageSize));
-			$this->set ('page_size', $pageSize);
-		}
-		if ($orderBy = $this->get ('order_by'))	{
-			list ($field, $dir) = explode ('_', $orderBy);
-			$this->adapter->setOrderBy ($field, ($dir == 'asc' ? false : true));
-			$this->set ('order_by', $field . '_' . $dir);
+		$this->pager->setcurrentPage(intval($this->get('page'), $this->pager->getFirstPage()));
+		if ($pageSize = $this->get('page_size')) $this->pager->setPageSize(intval ($pageSize));
+		if ($orderBy = $this->get('order_by'))	{
+			list($field, $dir) = explode('_', $orderBy);
+			$this->pager->setOrderBy ($field, ($dir == 'asc' ? false : true));
 		}
 	}
 
 	public function get ($param, $default = '')	{
-		return (isset ($_GET[$this->pager->getParamName ($key)]) ? $_GET[$this->pager->getParamName ($key)] : $default);
+		return (isset ($_GET[$this->pager->getParamName($key)]) ? $_GET[$this->pager->getParamName($key)] : $default);
 	}
-
-	public function set ($param, $value)	{
-		$this->url->set ($this->pager->getParamName ($param), $value);
-		$this->pager->setParam ($this->pager->getParamName ($param), $value);
-	}
-
 
 }
