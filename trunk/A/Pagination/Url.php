aef6fd73abe49cafd09ae347109d3ec2312195d1
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-15 05:14:01 +0000

    Added setBase() and setProtocol() to A_Pagination_Url

--- trunk/A/Pagination/Url.php
+++ trunk/A/Pagination/Url.php
@@ -2,19 +2,30 @@
 
 class A_Pagination_Url	{
 
-	protected $base = '';
+	protected $base;
+	protected $protocol;
 	protected $state = array();
 
-	public function __construct ($base)	{
+	public function __construct ($base, $protocol = 'http')	{
 		$this->base = $base;
+		$this->protocol = $protocol;
 	}
 
 	public function set ($key, $value)	{
 		$this->state[$key] = $value;
 	}
 
-	public function build ($params = array())	{
-		return $this->base . '?' . http_build_query (array_merge ($params, $this->state));
+	public function setBase ($base)	{
+		$this->base = $base;
+	}
+
+	public function setProtocol ($protocol)	{
+		$this->protocol = $protocol;
+	}
+
+	public function build ($page, $params = array())	{
+		$params = array_merge ($params, $this->state);
+		return $this->protocol . '://' . $this->base . '/' . $page . (count ($params) > 0 ? '?' . http_build_query ($params) : '');
 	}
 
 }
