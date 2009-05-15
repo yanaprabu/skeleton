5120278c5036e168ad64c88464b267897b331e2a
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-15 05:13:28 +0000

    Added setBase() and setProtocol() to A_Pagination_Url

--- tests/unit/Pagination/UrlTest.php
+++ tests/unit/Pagination/UrlTest.php
@@ -5,24 +5,34 @@ include ('A/Pagination/Url.php');
 class Pagination_UrlTest extends UnitTestCase	{
 
 	public function setUp()	{
-		$this->url = new A_Pagination_Url ('page.php');
+		$this->url = new A_Pagination_Url ('forums.devnetwork.net', 'http');
 	}
 
 	public function testSingleSetParameter()	{
 		$this->url->set('order', 'title');
-		$this->assertEqual ($this->url->build(), 'page.php?order=title');
+		$this->assertEqual ($this->url->build ('page.php'), 'http://forums.devnetwork.net/page.php?order=title');
 	}
 
 	public function testDoubleSetParameter()	{
 		$this->url->set ('order', 'title');
 		$this->url->set ('pageSize', '5');
-		$this->assertEqual ($this->url->build(), 'page.php?order=title&pageSize=5');
+		$this->assertEqual ($this->url->build ('page.php'), 'http://forums.devnetwork.net/page.php?order=title&pageSize=5');
 	}
 
 	public function testDoubleSetParameterWithBuildParameter()	{
 		$this->url->set ('order', 'title');
 		$this->url->set ('pageSize', '5');
-		$this->assertEqual ($this->url->build (array ('page' => 2)), 'page.php?page=2&order=title&pageSize=5');
+		$this->assertEqual ($this->url->build ('page.php', array ('page' => 2)), 'http://forums.devnetwork.net/page.php?page=2&order=title&pageSize=5');
+	}
+
+	public function testSetBase()	{
+		$this->url->setBase ('www.devnetwork.net');
+		$this->assertEqual ($this->url->build ('page.php'), 'http://www.devnetwork.net/page.php');
+	}
+
+	public function testSetProtocol()	{
+		$this->url->setProtocol ('https');
+		$this->assertEqual ($this->url->build ('page.php'), 'https://forums.devnetwork.net/page.php');
 	}
 
 }
