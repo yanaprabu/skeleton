503c778d597d6818f9c2e19bcf595684a986c942
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-15 04:08:06 +0000

    Added A_Pagination_Url and test.

--- trunk/A/Pagination/Url.php
+++ trunk/A/Pagination/Url.php
@@ -2,8 +2,19 @@
 
 class A_Pagination_Url	{
 
+	protected $base = '';
+	protected $state = array();
+
+	public function __construct ($base)	{
+		$this->base = $base;
+	}
+
 	public function set ($key, $value)	{
-		$this->array[$key] = $value;
+		$this->state[$key] = $value;
+	}
+
+	public function build ($params = array())	{
+		return $this->base . '?' . http_build_query (array_merge ($params, $this->state));
 	}
 
 }
