869413de3929ac8301cf1c358e2be7ae4ceb571e
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-15 07:18:42 +0000

    Removed crazy constructor on A_Pagination_Request

--- trunk/A/Pagination/Request.php
+++ trunk/A/Pagination/Request.php
@@ -1,25 +1,13 @@
 <?php
 
-$adapter = new A_Pagination_Adapter_Array (...);
-$pager = new A_Pagination_Request ($adapter, 5);
-$view = new A_Pagination_View_Standard();
-
-
 class A_Pagination_Request	{
 
 	protected $adapter;
 	protected $pager;
 
-	public function __construct()	{
-		if (func_get_arg(0) instanceof A_Pagination)	{
-			$this->pager = func_get_arg(0);
-			$this->adapter = $this->pager->getDataSource();
-		} elseif (func_get_arg(0) instanceof A_Pagination_Adapter_Interface)	{
-			$pageSize = func_get_arg(1);
-			$currentPage = func_get_arg(2);
-			$this->pager = new A_Pagination ($adapter, $pageSize, $currentPage);
-			$this->adapter = func_get_arg(0);
-		}
+	public function __construct ($adapter, $pageSize, $currentPage)	{
+		$this->pager = new A_Pagination ($adapter, $pageSize, $currentPage);
+		$this->adapter = func_get_arg(0);
 		$this->url = new A_Pagination_Url();
 		$this->process(); // would we ever need to not call $process?
 	}
