2c20fc72ae8cf877bebee73cfc2bb752ddcf78d9
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-16 21:15:27 +0000

    Added methods to view helpers

--- A/Pagination/View/Link.php
+++ A/Pagination/View/Link.php
@@ -2,4 +2,48 @@
 
 class A_Pagination_View_Link	{
 
+	public function ($pager)	{
+		$this->pager = $pager;
+	}
+
+	public function first()	{
+		$html = '<a href="{url}">{text}</a>';
+		$html = str_replace ('{url}', $this->pager->url ('page.php', array ('page' => $pager->getFirstPage())));
+		$html = str_replace ('{text}', 'First');
+		return $html;
+	}
+
+	public function previous ($offset = 1)	{
+		$html = '<a href="{url}">{text}</a>';
+		$html = str_replace ('{url}', $this->pager->url ('page.php', array ('page' => $pager->getCurrentPage() - $offset)));
+		$html = str_replace ('{text}', 'Previous');
+		return $html;
+	}
+
+	public function next ($offset = 1)	{
+		$html = '<a href="{url}">{text}</a>';
+		$html = str_replace ('{url}', $this->pager->url ('page.php', array ('page' => $pager->getCurrentPage() + $offset)));
+		$html = str_replace ('{text}', 'Next');
+		return $html;
+	}
+
+	public function last()	{
+		$html = '<a href="{url}">{text}</a>';
+		$html = str_replace ('{url}', $this->pager->url ('page.php', array ('page' => $pager->getLastPage())));
+		$html = str_replace ('{text}', 'Last');
+		return $html;
+	}
+
+	public function separator ($separator)	{
+
+	}
+
+	public function addClass ($class)	{
+
+	}
+
+	public function render()	{
+
+	}
+
 }
