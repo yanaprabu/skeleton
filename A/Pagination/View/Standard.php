1be3d92e2f2a30a825c4fcc1acd84bfed23f588a
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-16 17:37:02 +0000

    Added some methods for helpers on A_Pagination_View_Standard

--- A/Pagination/View/Standard.php
+++ A/Pagination/View/Standard.php
@@ -2,7 +2,45 @@
 
 class A_Pagination_View_Standard	{
 
+	protected $helpers = array();
+
 	public function __construct ($pager)	{
+		$this->pager = $pager;
+		$this->helpers['url'] = new A_Pagination_Url();
+	}
+
+	public function setHelper ($name, $helper)	{
+		$this->helpers[$name] = $helper;
+	}
+
+	public function order()	{
+		if ($this->helpers['order'] !== null)	{
+			return $this->helpers['order'];
+		} else	{
+			$this->helpers['order'] = new A_Pagination_View_Order ($this);
+			return $this->helpers['order'];
+		}
+	}
+
+	public function link()	{
+		if ($this->helpers['link'] !== null)	{
+			return $this->helpers['link'];
+		} else	{
+			$this->helpers['link'] = new A_Pagination_View_Link ($this);
+			return $this->helpers['link'];
+		}
+	}
+
+	public function url()	{
+		if ($this->helpers['url'] !== null)	{
+			return $this->helpers['url'];
+		} else	{
+			$this->helpers['url'] = new A_Pagination_View_Link ($this);
+			return $this->helpers['url'];
+		}
+	}
+
+	public function render()	{
 
 	}
 
