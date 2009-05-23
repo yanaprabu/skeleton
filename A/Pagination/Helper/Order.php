fb20d7fb996d09861f7e4f780c14998ddd4925c7
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-23 07:59:32 +0000

    A_Pagination_Helper_Order and example/example_request_order

--- A/Pagination/Helper/Order.php
+++ A/Pagination/Helper/Order.php
@@ -10,21 +10,35 @@
  */
 
 class A_Pagination_Helper_Order	{
-
-	public function __construct ($pager, $url)	{
-
+	protected $pager;
+	protected $url;
+	protected $columns;
+	protected $separator = '</th><th>';
+	
+	public function __construct ($pager, $url=false, $columns=array()) {
+		$this->pager = $pager;
+		$this->url = $url ? $url : new A_Pagination_Helper_Url();
+		$this->columns = $columns;
 	}
 
-	public function links()	{
-
+	public function setColumns($columns) {
+		$this->columns = $columns;
 	}
 
-	public function link ($key, $title)	{
-
+	public function setSeparator($separator) {
+		$this->separator = $separator;
 	}
 
-	public function url ($key)	{
-
+	public function render($columns=array()) {
+		if ($columns) {
+			$this->columns = $columns;
+		}
+		$link = new A_Pagination_Helper_Link($this->pager, $this->url);
+		$links = array();
+		foreach ($this->columns as $column => $label) {
+			$links[] = $column ? $link->order($column, $label) : $label;
+		}
+		return implode($this->separator, $links);
 	}
 
 }
