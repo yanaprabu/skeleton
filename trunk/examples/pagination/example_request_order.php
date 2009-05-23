f2daaecd461c068954c9fb90c1cf4fdf0b5b7d24
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-23 07:59:32 +0000

    A_Pagination_Helper_Order and example/example_request_order

---
+++ trunk/examples/pagination/example_request_order.php
@@ -0,0 +1,66 @@
+<html>
+<head>
+<title>Skeleton - Pagination example - basic</title>
+</head>
+<body>
+<?php
+include 'config.php';
+include 'Datasource.php';
+include 'A/Pagination/Request.php';
+
+// initialize an array for testing
+for ($i=0; $i<=750; ++$i) {
+	$myarray[$i]['title'] = 'This is row ' . $i;
+	$myarray[$i]['month'] = date ('F', time() + ($i * 60 * 60 * 24 * 30));
+}
+#$myarray = null;
+// create a data object that has the interface needed by the Pager object
+$datasource = new Datasource($myarray);
+
+// create a request processor to set pager from GET parameters
+$pager = new A_Pagination_Request($datasource);
+$pager->setRangeSize(3)->process();
+
+$url = new A_Pagination_Helper_Url();
+$url->set('page', $pager->getCurrentPage());
+$url->set('order_by', $pager->getOrderBy());
+
+include 'A/Pagination/Helper/Link.php';
+$link = new A_Pagination_Helper_Link($pager);
+
+include 'A/Pagination/Helper/Order.php';
+$order = new A_Pagination_Helper_Order($pager, $url, array(''=>'Row', 'title'=>'Title', 'month'=>'Month'));
+
+$rows = $pager->getItems();
+
+// display the paging links ... should this go in a template?
+$links = '';
+$links .= $link->first('First');
+$links .= $link->previous('Previous');
+$links .= $link->range();
+$links .= $link->last();
+$links .= $link->next('Next');
+
+echo "<div>$links</div>";
+
+// display the data
+echo '<table border="1">';
+echo '<tr><th>' . $order->render() . '</th></tr>';
+$n = 1;
+foreach ($rows as $value) {
+	echo '<tr>';
+	echo '<td>' . $n++ . '.</td><td>' . $value['title'] . '</td><td>' . $value['month'] . '</td>';
+	echo '</tr>';
+}
+echo '</table>';
+
+echo "<div>$links</div>";
+
+#dump($pager);
+?>
+<p/>
+<a href="../">Return to Examples</a>
+</p>
+
+</body>
+</html>
