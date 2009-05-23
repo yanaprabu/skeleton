8122515e153eefdc7c18c602f15df56a3abf08c2
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-23 01:04:46 +0000

    Changed to persist num_items in a RESTful way.

--- trunk/A/Pagination/Request.php
+++ trunk/A/Pagination/Request.php
@@ -15,12 +15,17 @@ include_once 'A/Pagination/Url.php';
 class A_Pagination_Request extends A_Pagination_Core	{
 
 	public function process()	{
-		$this->setcurrentPage($this->get('page'), $this->getFirstPage());
-		if ($pageSize = $this->get('page_size')) $this->setPageSize(intval ($pageSize));
+		if ($numItems = $this->get('num_items')) {
+			$this->setNumItems(intval($numItems));
+		}
+		if ($pageSize = $this->get('page_size')) {
+			$this->setPageSize(intval($pageSize));
+		}
 		if ($orderBy = $this->get('order_by'))	{
 			list ($field, $dir) = explode('_', $orderBy);
 			$this->setOrderBy ($field, ($dir == 'desc' ? true : false));
 		}
+		$this->setcurrentPage(intval($this->get('page')), $this->getFirstPage());	// do after getting num_items
 	}
 
 	public function get($param, $default='')	{
