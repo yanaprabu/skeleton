01f461824a41e79d10f7162173063f0bf689ce84
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-28 03:17:18 +0000

    Added client state persistence

--- trunk/A/Pagination/Helper/Link.php
+++ trunk/A/Pagination/Helper/Link.php
@@ -26,6 +26,18 @@ class A_Pagination_Helper_Link {
 	public function __construct($pager, $url=false)	{
 		$this->pager = $pager;
 		$this->url = $url ? $url : new A_Pagination_Helper_Url();
+		$page = $this->pager->getCurrentPage();
+		if ($page > 1) {
+			$this->url->set ($this->pager->getParamName('page'), $page);
+		}
+		$num_items = $this->pager->getNumItems();
+		if ($num_items) {
+			$this->url->set ($this->pager->getParamName('num_items'), $num_items);
+		}
+		$order_by = $this->pager->getOrderBy();
+		if ($order_by) {
+			$this->url->set ($this->pager->getParamName('order_by'), $order_by);
+		}
 	}
 
 	/**
@@ -92,7 +104,7 @@ class A_Pagination_Helper_Link {
 	 * @type
 	 */
 	public function page($page=false, $label=false) {
-		return $this->_link($this->url->render(false, $this->_params($this->pager->getParamName('page'), $page)), $label ? $label : $page);
+		return $this->_link($this->url->render(false, array ($this->pager->getParamName('page') => $page)), $label ? $label : $page);
 	}
 
 	/**
@@ -141,7 +153,7 @@ class A_Pagination_Helper_Link {
 	 */
 	public function order($field, $label='', $descending = false) {
 		$orderBy = $field . ($descending ? '|desc':'');
-		return $this->_link($this->url->render(false, $this->_params($this->pager->getParamName('order_by'), $orderBy)), $label ? $label : $page);
+		return $this->_link($this->url->render(false, array ($this->pager->getParamName('order_by') => $orderBy)), $label ? $label : $page);
 	}
 
 	/**
@@ -160,26 +172,4 @@ class A_Pagination_Helper_Link {
 	protected function _link($url, $label) {
 		return "<a href=\"$url\"" .($this->class ? " class=\"{$this->class}\"" : '') . ">$label</a>";
 	}
-
-	/**
-	 * @type array - persisted parameters
-	 */
-	protected function _params($field, $value) {
-		$params = array();
-		$page = $this->pager->getCurrentPage();
-		if ($page) {
-			$params[$this->pager->getParamName('page')] = $page;
-		}
-		$num_items = $this->pager->getNumItems();
-		if ($num_items) {
-			$params[$this->pager->getParamName('num_items')] = $num_items;
-		}
-		$order_by = $this->pager->getOrderBy();
-		if ($order_by) {
-			$params[$this->pager->getParamName('order_by')] = $order_by;
-		}
-		$params[$field] = $value;
-		return $params;
-	}
-
 }
