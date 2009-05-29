f56a748abd816b5477d87385e746a1b0499e4d90
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-29 00:54:36 +0000

    Cleaned up standard example

--- examples/pagination/example_request_standard.php
+++ examples/pagination/example_request_standard.php
@@ -1,45 +1,45 @@
 <html>
 <head>
-<title>Skeleton - Pagination example - basic</title>
+<title>Skeleton - Pagination example - Standard Request</title>
 </head>
 <body>
 <?php
 include 'config.php';
 include 'Datasource.php';
 include 'A/Pagination/Request.php';
+include 'A/Pagination/View/Standard.php';
 
 // initialize an array for testing
 for ($i=0; $i<=750; ++$i) {
 	$myarray[$i]['title'] = 'This is row ' . $i;
 	$myarray[$i]['month'] = date ('F', time() + ($i * 60 * 60 * 24 * 30));
 }
-#$myarray = null;
+
 // create a data object that has the interface needed by the Pager object
 $datasource = new Datasource($myarray);
 
 // create a request processor to set pager from GET parameters
 $pager = new A_Pagination_Request($datasource);
+
+// set range (number of links on either side of current page and process core based on request
 $pager->setRangeSize(3)->process();
 
-$url = new A_Pagination_Helper_Url();
-$url->set('page', $pager->getCurrentPage());
-$url->set('order_by', $pager->getOrderBy());
-
-include 'A/Pagination/View/Standard.php';
+// create a new standard view
 $view = new A_Pagination_View_Standard($pager);
 
+// retrieve items on current page
 $rows = $pager->getItems();
 
+// Set up view internally. For first/last, label is optional. If no label is passed, the number will be displayed.
 $view
+	->first('First')
 	->previous('Previous')
-	->first()
 	->range()
-	->last()
-	->next('Next');
-
-echo "<div>{$view->render()}</div>";
+	->next('Next')
+	->last('Last');
 
 // display the data
+echo "<div>{$view->render()}</div>";
 echo '<table border="1">';
 echo '<tr><th>' . $view->link()->order('', 'Row') . '</th><th>' . $view->link()->order('title', 'Title') . '</th><th>' . $view->link()->order('month', 'Month') . '</th></tr>';
 $n = 1;
@@ -49,10 +49,8 @@ foreach ($rows as $value) {
 	echo '</tr>';
 }
 echo '</table>';
-
 echo "<div>{$view->render()}</div>";
 
-#dump($pager);
 ?>
 <p/>
 <a href="../">Return to Examples</a>
