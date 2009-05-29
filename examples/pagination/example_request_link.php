bc8c914cf9ce2409cf1a57c6a99964f5e7aa14b4
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-29 01:02:42 +0000

    Cleaned up link example

--- examples/pagination/example_request_link.php
+++ examples/pagination/example_request_link.php
@@ -7,30 +7,30 @@
 include 'config.php';
 include 'Datasource.php';
 include 'A/Pagination/Request.php';
+include 'A/Pagination/Helper/Link.php';
 
 // initialize an array for testing
 for ($i=0; $i<=750; ++$i) {
 	$myarray[$i]['title'] = 'This is row ' . $i;
 	$myarray[$i]['month'] = date ('F', time() + ($i * 60 * 60 * 24 * 30));
 }
-#$myarray = null;
+
 // create a data object that has the interface needed by the Pager object
 $datasource = new Datasource($myarray);
 
 // create a request processor to set pager from GET parameters
 $pager = new A_Pagination_Request($datasource);
+
+// set range (number of links on either side of current page) and process core based on request
 $pager->setRangeSize(3)->process();
 
-$url = new A_Pagination_Helper_Url();
-$url->set('page', $pager->getCurrentPage());
-$url->set('order_by', $pager->getOrderBy());
-
-include 'A/Pagination/Helper/Link.php';
+// create a new link helper
 $link = new A_Pagination_Helper_Link($pager);
 
+// retrieve items on current page
 $rows = $pager->getItems();
 
-// display the paging links ... should this go in a template?
+// create links using helper directly
 $links = '';
 $links .= $link->first('First');
 $links .= $link->previous('Previous');
@@ -38,9 +38,8 @@ $links .= $link->range();
 $links .= $link->last();
 $links .= $link->next('Next');
 
-echo "<div>$links</div>";
-
 // display the data
+echo "<div>$links</div>";
 echo '<table border="1">';
 echo '<tr><th>' . $link->order('', 'Row') . '</th><th>' . $link->order('title', 'Title') . '</th><th>' . $link->order('month', 'Month') . '</th></tr>';
 $n = 1;
@@ -50,10 +49,8 @@ foreach ($rows as $value) {
 	echo '</tr>';
 }
 echo '</table>';
-
 echo "<div>$links</div>";
 
-#dump($pager);
 ?>
 <p/>
 <a href="../">Return to Examples</a>
