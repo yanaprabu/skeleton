fbfa96f15778d0a20a84d5c7aa119c7b969a7607
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-05-29 00:57:09 +0000

    Cleaned up standard link example

--- examples/pagination/example_request_standard_link.php
+++ examples/pagination/example_request_standard_link.php
@@ -1,36 +1,36 @@
 <html>
 <head>
-<title>Skeleton - Pagination example - basic</title>
+<title>Skeleton - Pagination example - Standard Request Link</title>
 </head>
 <body>
 <?php
 include 'config.php';
 include 'Datasource.php';
 include 'A/Pagination/Request.php';
+include 'A/Pagination/View/Standard.php';
 
 // initialize an array for testing
 for ($i=0; $i<=750; ++$i) {
 	$myarray[$i]['title'] = 'This is row ' . $i;
 	$myarray[$i]['month'] = date ('F', time() + ($i * 60 * 60 * 24 * 30));
 }
-#$myarray = null;
+
 // create a data object that has the interface needed by the Pager object
 $datasource = new Datasource($myarray);
 
 // create a request processor to set pager from GET parameters
 $pager = new A_Pagination_Request($datasource);
+
+// set range (number of links on either side of current page) and process core based on request
 $pager->setRangeSize(3)->process();
 
-$url = new A_Pagination_Helper_Url();
-$url->set('page', $pager->getCurrentPage());
-$url->set('order_by', $pager->getOrderBy());
-
-include 'A/Pagination/View/Standard.php';
+// create a new standard view
 $view = new A_Pagination_View_Standard($pager);
 
+// retrieve items on current page
 $rows = $pager->getItems();
 
-// display the paging links ... should this go in a template?
+// Set up links in $link array
 $links = '';
 $links .= $view->link()->first('First');
 $links .= $view->link()->previous('Previous');
@@ -38,6 +38,7 @@ $links .= $view->link()->range();
 $links .= $view->link()->last();
 $links .= $view->link()->next('Next');
 
+// display the paging links
 echo "<div>$links</div>";
 
 // display the data
@@ -51,9 +52,9 @@ foreach ($rows as $value) {
 }
 echo '</table>';
 
+// display the paging links
 echo "<div>$links</div>";
 
-#dump($pager);
 ?>
 <p/>
 <a href="../">Return to Examples</a>
