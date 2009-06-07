c7825cc8351e7d1c3a844b5697a518087ffbe156
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-06-07 06:46:20 +0000

    Added tests for A_Orm_DataMapper

--- A/Orm/DataMapper.php
+++ A/Orm/DataMapper.php
@@ -2,34 +2,42 @@
 
 class A_Orm_DataMapper	{
 
+	protected $mappings = array();
 	protected $class;
+	protected $table;
 
 	public function load($array)	{
-		if (class_exists ($this->class))	{
-			$object = new $this->class();
-		} else {
+		if (!class_exists ($this->class))	{
 			throw new Exception ('class ' . $this->class . ' does not exist.');
+		}
+		$object = new $this->class();
+		foreach ($this->mappings as $mapping)	{
+			$mapping->map ($object, $array);
 		}
 		return $object;
 	}
 
-	public function mapMethods($setMethod, $getMethod)	{
+	public function mapMethods($getMethod, $setMethod)	{
 		$mapping = new A_Orm_Mapping();
 		$mapping->setSetMethod ($setMethod);
 		$mapping->setGetMethod ($getMethod);
-		$this->map[] = $mapping;
+		$this->mappings[] = $mapping;
 		return $mapping;
 	}
 
 	public function mapProperty($property)	{
 		$mapping = new A_Orm_Mapping();
 		$mapping->setProperty ($property);
-		$this->map[] = $mapping;
+		$this->mappings[] = $mapping;
 		return $mapping;
 	}
 
 	public function setClass($class)	{
 		$this->class = $class;
+	}
+
+	public function setTable($table)	{
+		$this->table = $table;
 	}
 
 }
