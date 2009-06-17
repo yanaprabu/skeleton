5efb755b65d220fb555033f32e20fec7aa100f9b
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-06-17 23:28:30 +0000

    Fix if NT fix of A_Controller_Helper_Load.

--- A/Controller/Helper/Load.php
+++ A/Controller/Helper/Load.php
@@ -147,8 +147,8 @@ class A_Controller_Helper_Load {
 				}
 				// if extension found and it is in array, use it
 				$ext = isset($path_parts['extension']) && isset($this->renderClasses[$path_parts['extension']]) ? $path_parts['extension'] : $this->renderExtension; 
-		        // fix by thinsoldier for NT servers
-				$class = $path_parts['basename'];
+		        // fix by thinsoldier for NT servers that do not return filename
+				$class = isset($path_parts['filename']) ? $path_parts['filename'] : $path_parts['basename'];
 
 				include_once str_replace('_', '/', $this->renderClasses[$ext]) . '.php';
 				$obj = new $this->renderClasses[$ext]("$path$class.$ext");
