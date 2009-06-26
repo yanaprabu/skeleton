3951393b00900d20ad57b2c1d0c189e416061f1e
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-06-26 04:23:36 +0000

    Renamed SQLMapper

--- trunk/examples/orm/UserMapper.php
+++ trunk/examples/orm/UserMapper.php
@@ -2,7 +2,7 @@
 
 require_once('SQLMapper.php');
 
-class UserMapper extends SQLMapper	{
+class UserMapper extends I_Was_Told_To_Call_This_A_Table_Data_Gateway_But_The_Name_Really_Doesnt_Matter_Because_Its_Just_A_Placeholder_For_The_SQL_Functionality_Of_The_ORM	{
 
 	public function __construct($db)	{
 		parent::__construct($db, 'User','users');
