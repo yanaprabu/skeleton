8a8dcccfdb3aaaa8fd756fd156b0fbd21dd4d78e
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-07-09 07:16:10 +0000

    Moved much of the functionality that was in A_Controller_Action_Dispatch up into A_Controller_Action. Now A_Controller_Action_Dispatch just adds the pre/post filter functionality. Added underscores to the pre/post methods.

--- A/Controller/Action.php
+++ A/Controller/Action.php
@@ -10,12 +10,46 @@ class A_Controller_Action {
 	const MODULE = 'module';
 	const CONTROLLER = 'controller';
 	const ACTION = 'action';
+
+	/**
+	 * registry
+	 * @var A_Request
+	 */
 	protected $locator;
-	protected $request;
-	protected $response;
+
+	/**
+	 * The dispatched request
+	 * @var A_Request
+	 */
+	protected $request = null;
+	
+	/**
+	 * The result response
+	 * @var A_Reponse
+	 */
+	protected $response = null;
+	
+	/**
+	 * The result response
+	 * @var A_Controller_Helper_Load
+	 */
 	protected $load = null;
+
+	/**
+	 * View object for simple single view
+	 * @var A_Http_View
+	 */
+	protected $view = null;
+
+	/**
+	 * array of helpers
+	 * @var object
+	 */
 	protected $helpers = array();
 	
+	/**
+	 * constructor called by the Front Controller
+	 */
 	public function __construct($locator=null){
 	    if ($locator) {
 			$this->locator = $locator;
@@ -24,6 +58,62 @@ class A_Controller_Action {
 	    }
 	}
 	 
+	/**
+	 * Request getter
+	 * @return A_Request
+	 */
+	public function getRequest() {
+		return $this->request;
+	}
+	
+	/**
+	 * Reponse getter
+	 *  - Create a new response object if it has not been set
+	 * @return A_Reponse
+	 */
+	public function getResponse() {
+		if(!$this->response) {
+			include_once 'A/Http/Response.php';
+			$this->response = new A_Http_Response();
+		}
+		
+		return $this->response;
+	}
+
+	/**
+	 * Queries request for parameters
+	 *
+	 * @param string $param
+	 * @param mixed $default
+	 * @return mixed
+	 */
+	public function getParam($param,$default = null) {
+		return $this->request->get($param,$default);
+	}
+	
+	/**
+	 * Send redirect headers
+	 *  - Asks response object to send redirect headers
+	 *
+	 * @param string $url
+	 */
+	public function redirect($url) {
+		if (isset($this->resposne)) {
+			$this->response->setRedirect($url);
+		} else {
+			header("Content: $url");
+		}
+	}
+	
+	/**
+	 * return the result of this function to the Front Controller to forward to another controllere
+	 *
+	 * @param string $dir
+	 * @param string $class
+	 * @param string $method
+	 * @param string $args
+	 * @return array
+	 */
 	protected function forward($dir, $class, $method='', $args=null){
 		return array($dir, $class, $method, $args);
 	}
@@ -66,4 +156,14 @@ class A_Controller_Action {
 		}
 	}
  
+	/**
+	 * load view object
+	 */
+	public function view($name='', $scope='') {
+		if (!$this->view) {
+			$this->view = $this->load($scope)->view($name);
+		}
+		return $this->view;
+	}
+	
 }
