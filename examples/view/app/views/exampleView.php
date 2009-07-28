1841c4e46d18aa12b669ab6701a0b054c060e916
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-07-28 00:31:23 +0000

    Changed examples/view to use renamed setTemplate().

--- examples/view/app/views/exampleView.php
+++ examples/view/app/views/exampleView.php
@@ -4,6 +4,7 @@ include_once 'A/Http/View.php';
 class exampleView extends A_Http_View {
 	public function __construct($locator) {
 		parent::__construct($locator);
-		$this->setFilename('example');
+		$this->setRender('menu', 'menu');
+		$this->setTemplate('example');
 	}
 }
