7d7f22e85941f420f211df0eb719e744a78ba2fd
m.p.abeelen <m.p.abeelen@gmail.com>
2009-08-09 17:26:58 +0000

    small update View example

---
+++ examples/view/app/templates/differenttemplate.php
@@ -0,0 +1,21 @@
+<?php
+// partial for the header template
+echo $this->partial('layout/header');
+?>
+
+<ul id="menu">
+<?php
+// partial loop with two parameters where the 1st is the name and the 2nd is an array of values to replace name each loop
+echo $this->partialLoop('menu', 'menuitem', $this->menuitems);
+?>
+</ul>
+
+
+<p><?php echo $this->get('content'); ?></p>
+
+
+	
+<?php
+// partial for the footer template
+echo $this->partial('layout/footer');
+?>
