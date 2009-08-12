ffcdb0f943e6a4700c8b36b71a6fa02170589c02
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-08-12 08:17:31 +0000

    Added partial() and partialLoop() to A_Template_Include.

--- A/Template/Include.php
+++ A/Template/Include.php
@@ -8,6 +8,33 @@ include_once('A/Template.php');
 
 class A_Template_Include extends A_Template {
 
+	public function partial($template) {
+		return $this->render(dirname($this->filename) . "/$template.php");
+	}
+	
+	public function partialLoop($template, $name, $data=null) {
+		$template = dirname($this->filename) . "/$template.php";
+		$str = '';
+		if ($data) {
+			// $name and $data set so each element in $data set to $name
+			foreach ($data as $value) {
+				$this->data[$name] = $value;
+				$str .= $this->render($template);
+			}
+		} else {
+			// $name but not $data, so $name contains $data. set() to $keys in each element array
+			foreach ($name as $data) {
+				if (is_array($data)) {
+					foreach ($data as $key => $value) {
+						$this->data[$key] = $value;
+					}
+				}
+				$str .= $this->render($template);
+			}
+		}
+		return $str;
+	}
+	
 	public function render() {
 	    extract($this->data);
 		ob_start();
