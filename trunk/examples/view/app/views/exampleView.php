36063e74bc9dc515a8a0e6ce21867543e7a169ff
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-08-13 00:43:22 +0000

    Misc. text changes to make examples/view clearer.

--- trunk/examples/view/app/views/exampleView.php
+++ trunk/examples/view/app/views/exampleView.php
@@ -6,10 +6,10 @@ class exampleView extends A_Http_View {
 		parent::__construct($locator);
 		$this->setTemplate('example');
 		$this->set('menuitems', array(
-			array('menu_link' => '?', 'menu_title'=>'start example'),
-			array('menu_link' => '?controller=example&action=needsdifferenttemplate','menu_title'=>'this actions uses a different template'),
+			array('menu_link' => '?', 'menu_title'=>'default action using setTemplate()'),
+			array('menu_link' => '?controller=example&action=needsdifferenttemplate','menu_title'=>'this action uses setTemplate() with  a different template'),
 			array('menu_link' => '?controller=example&action=actionsetscontent','menu_title'=>'this action uses setContent()'),
-			array('menu_link' => '?controller=example&action=actionsetsrenderer','menu_title'=>'this action uses actionsetsrenderer()'))
+			array('menu_link' => '?controller=example&action=actionsetsrenderer','menu_title'=>'this action uses setsRenderer() and the A_Template_Strreplace class'))
 			);
 	}
 }
