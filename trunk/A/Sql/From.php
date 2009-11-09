b98738814535c4ea9714695e4d80dc3aa71696ba
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-11-09 08:37:09 +0000

    Added methods to A_Sql_From to deal with joins

--- trunk/A/Sql/From.php
+++ trunk/A/Sql/From.php
@@ -12,7 +12,27 @@ class A_Sql_From extends A_Sql_Table  {
 	 *
 	 * @return string
 	 */
+	
+	public function addJoin($join)	{
+		$this->joins[] = $join;
+		return $join;
+	}
+	
+	public function join()	{
+		if(func_num_args() == 1)	{
+			$join = new A_Sql_Join_String(func_get_arg(0));
+		} else	{
+			$join = new A_Sql_Join_Parameters(func_get_arg(0),func_get_arg(1),func_get_arg(2));	
+		}
+		return $this->addJoin($join);
+	}
+	
+	public function getJoins()	{
+		return $this->joins();
+	}
+	
 	public function render() {
 		return 'FROM '. parent::render();		
 	}
+	
 }
