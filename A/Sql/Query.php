3318f9185527664d78a4bd14682b9e1a79561a81
allspiritseve <allspiritseve@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-11-18 17:24:47 +0000

    Fixed A_Sql_Query to always instantiate new objects.

--- A/Sql/Query.php
+++ A/Sql/Query.php
@@ -12,50 +12,21 @@ include_once('A/Sql/Update.php');
 include_once('A/Sql/Delete.php');
 
 class A_Sql_Query	{
-
-	protected $select;
-	protected $insert;
-	protected $update;
-	protected $delete;
-
+	
 	public function select()	{
-		if(!$this->select instanceof A_Sql_Select)	{
-			$this->select = new A_Sql_Select();
-		}
-		return $this->select;
+		return new A_Sql_Select();
 	}
 	
 	public function insert($table = null, $bind = array())	{
-		if(!$this->insert instanceof A_Sql_Insert)	{
-			$this->insert = new A_Sql_Insert($table, $bind);
-		} else	{
-			if($bind)	{
-				$this->columns($bind);
-			}
-			$this->insert->table($table);
-		}
-		return $this->insert;
+		return new A_Sql_Insert($table, $bind);
 	}
 	
 	public function update($table = null, $bind = array(), $where = array())	{
-		if(!$this->update instanceof A_Sql_Update)	{
-			$this->update = new A_Sql_Update($table, $bind, $where);
-		} else	{
-			if($bind)	{
-				$this->update->columns($bind);
-			}
-			$this->update->table($table)->where($where);
-		}
-		return $this->update;
+		return new A_Sql_Update($table, $bind, $where);
 	}
 	
 	public function delete($table = null, $where = array())	{
-		if(!$this->delete instanceof A_Sql_Delete)	{
-			$this->delete = new A_Sql_Delete($table, $where);
-		} else	{
-			$this->delete->table($table)->where($where);
-		}
-		return $this->delete;
+		return new A_Sql_Delete($table, $where);
 	}
 	
 }
