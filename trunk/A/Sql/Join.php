fb315669b28b398324dc3c09c28b5a551e6c366d
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-12-04 10:50:27 +0000

    Made A_Sql_Join support only a single join.

--- trunk/A/Sql/Join.php
+++ trunk/A/Sql/Join.php
@@ -6,8 +6,10 @@
  */
 
 class A_Sql_Join {
-	protected $joins = array();
-	protected $tables = array();
+	protected $type = '';
+	protected $table1 = '';
+	protected $table2 = '';
+	protected $on = null;
 	
 	public function __construct($table1=null, $table2=null, $type='INNER') {
 		if ($table1 && $table2) {
@@ -17,21 +19,20 @@ class A_Sql_Join {
 	
 	public function join($table1, $table2, $type='INNER') {
 		$type = strtoupper($type);
-		$this->joins[] = array('type' => $type, 'table1' => $table1, 'table2' => $table2, 'on' => null);
-		$this->tables[$table1] = true; 
-		$this->tables[$table2] = true; 
+		$this->type = $type;
+		$this->table1 = $table1;
+		$this->table2 = $table2;
+		$this->on = null;
 	}
 	
 	public function on($argument1, $argument2=null, $argument3=null) {
-		$joinkey = count($this->joins)-1;
-		if (!isset($this->joins[$joinkey])) { //no join has been set yet
+		if (! $this->table1) { //no join has been set yet
 			return;
 		}
-		$join = &$this->joins[$joinkey];
-		if (!$join['on']) {
+		if (!$this->on) {
 			require_once 'A/Sql/LogicalList.php';						
-			$join['on'] = new A_Sql_LogicalList();
-			$join['on']->setEscape(false);
+			$this->on = new A_Sql_LogicalList();
+			$this->on->setEscape(false);
 		}		
 		if (is_array($argument1)) {
 			if (!count($argument1)) {  //empty array of expressions was passed
@@ -40,13 +41,13 @@ class A_Sql_Join {
 			foreach($argument1 as $column1 => $column2) {
 				// check if the is a quoted string rather than a column name
 				if (substr($column1, 0, 1) != "'") {
-					$column1 = $this->prependTableAlias($join['table1'], $column1);
+					$column1 = $this->prependTableAlias($this->table1, $column1);
 				}
 				// check if the is a quoted string rather than a column name
 				if (substr($column2, 0, 1) != "'") {
-					$column2 = $this->prependTableAlias($join['table2'], $column2);
+					$column2 = $this->prependTableAlias($this->table2, $column2);
 				}
-				$this->joins[$joinkey]['on']->addExpression($column1, $column2);
+				$this->on->addExpression($column1, $column2);
 			}
 		} else {
 			//since we allow different style of parameters we must account for different
@@ -59,20 +60,21 @@ class A_Sql_Join {
 				$argument2 = $argument3;
 			}
 			if (substr($argument1, 0, 1) != "'") {
-				$argument1 = $this->prependTableAlias($join['table1'], $argument1);
+				$argument1 = $this->prependTableAlias($this->table1, $argument1);
 			}
 			if (substr($argument2, 0, 1) != "'") {
-				$argument2 = $this->prependTableAlias($join['table2'], $argument2);
+				$argument2 = $this->prependTableAlias($this->table2, $argument2);
 			}
-			$join['on']->addExpression($logic, $argument1, $argument2);
+			$this->on->addExpression($logic, $argument1, $argument2);
 		}
 	}
 
 	public function render() {
 		$return = '';
-		foreach ($this->joins as $join) {
-			$on = $join['on'] ? ' ON '. $join['on']->render() : '';
-			$return .= " {$join['type']} JOIN {$join['table1']}$on";
+		if ($this->table1) {
+			$on = $this->on ? ' ON '. $this->on->render() : '';
+			$type = $this->type ? ' '. $this->type : '';
+			$return .= "$type JOIN {$this->table1}$on";
 		}
 		return $return;
 	}
@@ -86,7 +88,14 @@ class A_Sql_Join {
 
 
 	protected function getTables() {
-		return array_keys($this->tables);
+		$tables = array();
+		if ($table1) {
+			$tables[] = $this->table1;
+			if ($table2) {
+				$tables[] = $this->table2;
+			}
+		}
+		return $tables;
 	}
 
 
