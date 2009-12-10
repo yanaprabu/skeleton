8cf4c71432233e908daea8f8930311c54ea28b70
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-12-10 20:00:13 +0000

    Fixed problems with LIMIT in A_Db_MySQL::limit() and A_Pagination_Adapter_Db::getItems()

--- trunk/A/Db/MySQL.php
+++ trunk/A/Db/MySQL.php
@@ -87,8 +87,7 @@ class A_Db_MySQL {
 	}
 		
 	public function limit($sql, $count, $offset = null) {
-		$limit = (is_int($offset) && $offset > 0) ? ($offset . ', ' . $count) : $count; 
-		return $sql . ' LIMIT ' . $limit;
+		return $sql . ' LIMIT ' . (is_int($offset) && $offset > 0 ? "$offset," : '') . $count;
 	}
 	
 	public function lastId() {
