aaec46b1b18a745ac85eb5054d10675b5836a96c
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2009-12-10 08:01:37 +0000

    Created A/autoload.php script that registers an autoloader for the framework. Removed old A/functions/a_autoload.php

--- trunk/A/functions/a_autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function a_autoload($class) {
-	$file = str_replace(array('_','-'), array('/','_'), $class) . '.php';
-	include($file);
-}
-
-spl_autoload_register('a_autoload');
--- trunk/examples/autoload/example.php
+++ trunk/examples/autoload/example.php
@@ -1,5 +1,14 @@
+<html>
+<body>
 <?php
-include('../../A/functions/a_autoload.php');
+include '../../A/autoload.php';
 
 $bar = new Bar();
 $foobar = new Foo_Bar();
+$locator = new A_Locator();
+if (class_exists('A_Locator')) {
+	echo "A_Locator autoloaded<br/>";
+}
+?>
+</body>
+</html>
