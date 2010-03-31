1e372a3b3dd7e5da7ce53a284f5d85948ef0d700
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2010-03-31 01:43:04 +0000

    Updated example/sql

--- trunk/examples/sql/example_from.php
+++ trunk/examples/sql/example_from.php
@@ -3,44 +3,42 @@ include 'config.php';
 
 $from = new A_Sql_From();
 echo $from
-	->table('tablefoo')
-	->join('foo0', 'bar0', 'LEFT')
+	->table('table1')
+	->join('table2')
 	->on('column5', 'column6')
+	->render();
+echo "\n<br/>\n";
+dump($from);
+
+$from = new A_Sql_From();
+echo $from
+	->table('table1')
+	->join('table2', 'RIGHT')
+	->on('column5', 'column6')
+	->render();
+echo "\n<br/>\n";
+dump($from);
+
+$from = new A_Sql_From();
+echo $from
+	->table('table1')
+	->join('table2', 'table1', 'LEFT')
+	->on('column5', 'column6')
+	->render();
+echo "\n<br/>\n";
+dump($from);
+
+$from = new A_Sql_From();
+echo $from
+	->table('foo0')
+	->join('foo0', 'bar0', 'LEFT')
+	->on('column1', 'column2')
+	->join('bar0', 'baz0', 'OUTER')
+	->on('column3', 'column4')
 #	->leftjoin('foo', 'bar')->on('foo1.column1', 'column2')->on('OR', 'column3', 'column4')
 #	->innerjoin('foo2', 'bar2')->on('column5', 'column6')
 #	->innerjoin('foo3', 'bar3')->on(array('column7' => 'column8', 'column9' => 'column10'))
 	->render();
 echo "\n<br/>\n";
-
 dump($from);
 
-/*
-include 'A/Sql/Select.php';
-
-$select = new A_Sql_Select();
-echo $select
-	->columns()
-	->from('tablefoo')
-	->join('foo0', 'bar0', 'LEFT')
-	->leftjoin('foo', 'bar')->on('foo1.column1', 'column2')->on('OR', 'column3', 'column4')
-	->innerjoin('foo2', 'bar2')->on('column5', 'column6')
-	->innerjoin('foo3', 'bar3')->on(array('column7' => 'column8', 'column9' => 'column10'))
-	->where('foo', 'bar');
-echo "\n<br/>\n";
-
-$select = new A_Sql_Select();
-echo $select
-	->columns()
-	->from('tablefoo')
-	->join('foo0', 'bar0', 'LEFT')->on('column5', "column6")
-	->where('foo', 'bar');
-echo "\n<br/>\n";
-
-$select = new A_Sql_Select();
-echo $select
-	->columns()
-	->from('tablefoo')
-	->join('foo0', 'bar0', 'LEFT')->on('column5', "'some value'")
-	->where('foo', 'bar');
-echo "\n<br/>\n";
-*/
