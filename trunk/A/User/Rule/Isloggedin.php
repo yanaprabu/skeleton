3d80e02e0437e6ed1269c96819622119bb93822d
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2010-04-12 06:50:21 +0000

    Changes to A_User_Access and A_User_Rule_Isloggedin.

--- trunk/A/User/Rule/Isloggedin.php
+++ trunk/A/User/Rule/Isloggedin.php
@@ -7,12 +7,14 @@
 
 class A_User_Rule_Isloggedin extends A_Rule_Abstract {
 	
-	public function __construct ($errorMsg) {
-		$this->errorMsg = $errorMsg;
-	}
+#	public function __construct ($errorMsg) {
+#		$this->errorMsg = $errorMsg;
+#	}
 
 	public function validate() { 
-		$user = $this->getValue();
+#		$this->params['errorMsg'] = $this->params['field'];
+		$user = $this->container;
+dump($user, 'A_User_Rule_Isloggedin');
 		if ($user) {
 			return $user->isLoggedIn();
 		}
