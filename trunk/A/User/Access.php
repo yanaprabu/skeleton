a8a7b06cb4c071c793cd545e0e457d9b7ad3844d
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2010-04-12 06:21:08 +0000

    Change A_User_Access to act as special rule set.

--- trunk/A/User/Access.php
+++ trunk/A/User/Access.php
@@ -5,17 +5,27 @@
  * @package A_User 
  */
 
-class A_User_Access extends A_Rule_Set {
+class A_User_Access {
 	protected $user;
+	protected $rules = array();
 	
 	public function __construct($user) {
 		$this->user = $user;
 	}
 
+	public function addRule($rule) {
+		$this->rules[] = $rule;
+	}
+
 	public function run($locator) {
-		if (!$this->isValid($this->user)) { 
-			$request = $locator->get('Request');
-			$request->set('action', $this->errorMsg[0]);	// get first error as action
+		foreach ($this->rules as $rule) {
+			if (!$rule->isValid($this->user)) { 
+				$request = $locator->get('Request');
+				// A_User_Rule_* use the Rule's errorMsg to hold the forward
+				if ($request && isset($this->errorMsg[0])) {
+					$request->set('controller', $this->errorMsg[0]);
+				}
+			}
 		}
 	}
 }
