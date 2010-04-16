b245b3f89f93a750825f85dbfa03a4f888175597
m.p.abeelen@gmail.com <m.p.abeelen@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2010-04-16 07:17:55 +0000

    layout of register function in usermodel

--- trunk/examples/blog/app/models/usersModel.php
+++ trunk/examples/blog/app/models/usersModel.php
@@ -83,5 +83,84 @@ class usersModel extends A_Model {
 	function loginErrorMsg() {
 		return $this->errmsg;
 	}
+
+	function register($request){ 
+		
+		/*
+		Registration pages
+		    * - Show Registration form
+		    * - Registration form submitted; user already has another account with the same email address
+		    * - Registration form submitted; username not available
+		    * - Registration form submitted; account created; activation email sent
+		    * - Registration form submitted; username/email combination already exists, but with different password
+		    * - Registration form submitted; username/email combination already exists; password is correct
+		    * - Registration form submitted; account already exists but is not yet activated
+		*/
+
+		$data = array();
+		//Check if any field is empty
+		if (empty($data['username']) || empty($data['email']) || empty($data['password']) || empty($data['passwordagain'])) {
+			// Return error missing field
+			
+		}
+		// Check if the passwords match
+     	if (isset($data['password']) && isset($data['passwordagain']) && $data['password'] != $data['passwordagain']){
+			// Return error passwords do not match
+			
+		}
+
+		// Check if the username is available
+		if($this->usernameAvailable($data['username'])){ 
+			if($this->emailExists($data['email'])){ 
+				// The email adress is already in the db
+				// User doesn't know he already has an account
+				// or he tries to register again
+				// Show message + registration form + link to sign in form + link to send new password
+				
+			} else {
+				// Create account
+				
+				// Send activation email
+				
+				// Show message succesful registration
+				
+			}
+			
+		// Username is not available / already in database
+		} else {
+			// Check if thise username belongs to the posted email
+			if($this->usernameHasEmail($data['username'], $data['email'])){
+				// Check if account has been activated?
+				if($this->accountActivated($data['username'], $data['email'])){
+					// The account has been activated already. In that case check if password is correct
+					if($this->passwordCorrect($data['username'], $data['password'])){
+						// Login the user and redirect to success page
+						$this->login($data['username'], $data['password']);
+						
+					} else {
+						// User has an activated account but forgot his password
+						// show message + signin form + forgot password link
+						
+					}
+				} else {
+					// account is not yet activated
+					// Show message user has to activate account + show link to resend activation email
+					
+				}
+			} else {
+				// Explanation: another user already taken that username: return error status
+				// Show message username already taken + registration form
+
+			}
+			
+		}
+		
+	}
+	
+	protected function usernameAvailable($username){}
+	protected function emailExists($email){}
+	protected function usernameHasEmail($username, $email){}
+	protected function accountActivated($username, $email){}
+	protected function passwordCorrect($username, $password){}
 	
 }
