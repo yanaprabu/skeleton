e23fccabbdc76e4979f532451feb2b7d33a04172
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2010-04-27 08:22:10 +0000

    Added paginated listing to admin/posts.

--- trunk/examples/blog/app/admin/controllers/posts.php
+++ trunk/examples/blog/app/admin/controllers/posts.php
@@ -13,8 +13,53 @@ class posts extends A_Controller_Action {
 	function index($locator) { 
 		$template = $this->_load()->template();
 		
+		$this->listing($locator);
+	}
+
+	function listing($locator) {
+		$usersession = $locator->get('UserSession');
+		$db = $locator->get('Db');
+		
+		$template = $this->_load()->template();
+		
+		// create a data object that has the interface needed by the Pager object
+		$datasource = new A_Pagination_Adapter_Db($db, "SELECT * FROM posts WHERE users_id=" . $usersession->get('id'));
+		 
+		// create a request processor to set pager from GET parameters
+		$pager = new A_Pagination_Request($datasource);
+		 
+		// initialize using values from $_GET
+		$pager->process($locator->get('Request'));
+		 
+		// create a "standard" view object to create pagination links
+		$view = new A_Pagination_View_Standard($pager);
+		 
+		$rows = $pager->getItems();
+
+		$template->set('rows', $rows);
+		$template->set('links', $view->render());		// display the pagination links
+		
 		$this->response->set('maincontent', $template->render());
 
 	}
 
+	function edit($locator) { 
+		$template = $this->_load()->template();
+		
+		$this->response->set('maincontent', $template->render());
+
+	}
+
+/*
+id
+post_date
+permalink
+title
+excerpt
+content
+comments_allowed
+post_type
+users_id
+active
+*/
 }
--- trunk/examples/blog/app/admin/templates/posts.php
+++ trunk/examples/blog/app/admin/templates/posts.php
@@ -1,2 +1,14 @@
 <h1>Manage Posts</h1>
 <p>This is a list of blog posts.</p>
+<?php
+	// display the data
+	echo '<table border="0">';
+	foreach ($rows as $row) {
+	    echo '<tr>';
+	    echo '<td>' . $row['id'] . '.&nbsp;</td><td>' . $row['title'] . '</td>';
+	    echo '</tr>';
+	}
+	echo '</table>';
+	
+	echo '<br/><div>Page: ' . $links . '</div>';
+?>		 
