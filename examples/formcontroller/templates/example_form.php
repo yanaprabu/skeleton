debcb79277c89461df5c29a63b0df2f1bfc62370
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2010-08-18 00:28:20 +0000

    Fixes to examples/formcontroller and examples/hangman to work with renaming A_Controller_Input/FormParameter to A_Controller_Input/Form_Field.

--- examples/formcontroller/templates/example_form.php
+++ examples/formcontroller/templates/example_form.php
@@ -15,22 +15,22 @@
     <tr>
       <td>Field 1 (Numbers only in range 1-10)</td>
       <td><?php $p1 = $this->getParameter('field1'); echo $p1->render(); ?></td>
-      <td><span class="error"><?php echo implode(', ', $p1->error_msg); ?></span></td>
+      <td><span class="error"><?php echo $p1->getErrorMsg(', '); ?></span></td>
     </tr>
     <tr>
       <td>Field 2 (Must match Field 1)</td>
       <td><?php $p2 = $this->getParameter('field2'); echo $p2->render(); ?></td>
-      <td><span class="error"><?php echo implode(', ', $p2->error_msg); ?></span></td>
+      <td><span class="error"><?php echo $p2->getErrorMsg(', '); ?></span></td>
     </tr>
     <tr>
       <td>Field 3 (Letters only min length 5)</td>
       <td><input type="text" name="field3" value="<?php $p3 = $this->getParameter('field3'); echo $p3->value; ?>"></td>
-      <td><span class="error"><?php echo implode(', ', $p3->error_msg); ?></span></td>
+      <td><span class="error"><?php echo $p3->getErrorMsg(', '); ?></span></td>
     </tr>
     <tr>
       <td>Field 4 (Convert letters to uppercase)</td>
       <td><input type="text" name="field4" value="<?php $p4 = $this->getParameter('field4'); echo $p4->value; ?>"></td>
-      <td><span class="error"><?php echo implode(', ', $p4->error_msg); ?></span></td>
+      <td><span class="error"><?php echo $p4->getErrorMsg(', '); ?></span></td>
     </tr>    
     <tr>
       <td>&nbsp;</td>
