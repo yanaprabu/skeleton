11182d0907574842856ac0d392649b8ad8643652
jonahbron.d@gmail.com <jonahbron.d@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2010-09-13 23:21:54 +0000

    Added Doc headers and property/function comments.  Any comments, arguments, or functions that were not understood were explained with "???".  These need to be corrected in future.

--- trunk/A/Pager.php
+++ trunk/A/Pager.php
@@ -1,64 +1,164 @@
 <?php
 /**
- * Pagination class 
- * 
- * @package A_Pager 
+ * Pagination class
+ *
+ * @package  A
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD
+ * @link	 http://skeletonframework.com/
  */
 
+/**
+ * A_Pager
+ */
 class A_Pager {
-	public $datasource = null;					// Pageable datasource object
-	public $page_size = 10;					// number of rows of data per page
-	public $range_size = 10;					// number of links in pager
+	/**
+	 * @var mixed $datasource Pageable datasource object
+	 */
+	public $datasource = null;
+	/**
+	 * @var integer $page_size Number of rows of data per page
+	 */
+	public $page_size = 10;
+	/**
+	 * @var integer $range_size Number of links in pager
+	 */
+	public $range_size = 10;
+	/**
+	 * @var integer $current_page Number of current page
+	 */
 	public $current_page = 0;
+	/**
+	 * @var integer $first_page ???
+	 */
 	public $first_page = 1;
+	/**
+	 * @var integer $last_page ???
+	 */
 	public $last_page = -1;
+	/**
+	 * @var integer $first_row ???
+	 */
 	public $first_row = 1;
+	/**
+	 * @var integer $list_row ???
+	 */
 	public $last_row = 0;
+	/**
+	 * @var integer $start_now ???
+	 */
 	public $start_row = 0;
+	/**
+	 * @var integer $end_row ???
+	 */
 	public $end_row = 0;
-	
-	public $page_param = 'page';				// parameter names for URLs
+	/**
+	 * @var string $page_param Parameter names for URLs
+	 */
+	public $page_param = 'page';
+	/**
+	 * @var string $last_row_param ???
+	 */
 	public $last_row_param = 'last_row';
+	/**
+	 * @var string $page_size_param ???
+	 */
 	public $page_size_param = 'page_size';
+	/**
+	 * @var string $order_by_param ???
+	 */
 	public $order_by_param = 'order_by';
-	public $order_by_fields = array();			// field names in database e.g. myfield or mytable.myfield
-	public $order_by_default_field = '';		// initial sort order
-	public $order_by_current_field = '';	// currently sorting on this field
-	public $order_by_current_desc = 0;			// 0=ascending or 1=descending for current field
-		
+	/**
+	 * @var array $order_by_fields field names in database e.g. myfield or mytable.myfield
+	 */
+	public $order_by_fields = array();
+	/**
+	 * @var string $order_by_default_field Initial sort order
+	 */
+	public $order_by_default_field = '';
+	/**
+	 * @var string $order_by_current_field Currently sorting on this field
+	 */
+	public $order_by_current_field = '';
+	/**
+	 * @var integer $order_by_current_desc 0=ascending or 1=descending for current field
+	 */
+	public $order_by_current_desc = 0;
+	
+	/**
+	 * __construct
+	 *
+	 * @param mixed $datasource ???
+	 */
 	public function __construct($datasource) {
 		$this->datasource = $datasource;
 	}
-
+	
+	/**
+	 * setPageSize
+	 *
+	 * @param integer $n Number of results to show on each page
+	 * @return A_Pager This object instance
+	 */
 	public function setPageSize($n) {
 		if ($n > 0) {
 			$this->page_size = $n;
 		}
 		return $this;
 	}
-
+	
+	/**
+	 * setRangeSize
+	 *
+	 * @param integer $n ???
+	 * @return A_Pager This object instance
+	 */
 	public function setRangeSize($n) {
 		if ($n > 2) {
 			$this->range_size = $n;
 		}
 		return $this;
 	}
-
+	
+	/**
+	 * setPageParameter
+	 *
+	 * @param mixed $name ???
+	 * @return A_Pager This object instance
+	 */
 	public function setPageParameter($name) {
 		$this->page_param = $name;
 		return $this;
 	}
 
+	/**
+	 * setPageSizeParameter
+	 *
+	 * @param mixed $name ???
+	 * @return A_Pager This object instance
+	 */
 	public function setPageSizeParameter($name) {
 		$this->page_size_param = $name;
 		return $this;
 	}
 
+	/**
+	 * setLastRowParameter
+	 *
+	 * @param mixed $name ???
+	 * @return A_Pager This object instance
+	 */
 	public function setLastRowParameter($name) {
 		$this->last_row_param = $name;
 		return $this;
 	}
 
+	/**
+	 * setOrderByCurrent
+	 *
+	 * @param mixed $field ???
+	 * @param mixed $direction ???
+	 * @return A_Pager This object instance
+	 */
 	public function setOrderByCurrent($field, $direction) {
 		$this->order_by_current_field = $field;
 		$pager->order_by_current_desc = $direction;
@@ -85,6 +185,13 @@ class A_Pager {
 		return $this;
 	}
 
+	/**
+	 * setOrderByFields
+	 *
+	 * @param mixed $fields ???
+	 * @param mixed $default_field ??? (optional)
+	 * @return A_Pager This object instance
+	 */
 	public function setOrderByFields($fields, $default_field=false) {
 		$this->order_by_fields = $fields;
 		if ($default_field !== false) {
@@ -96,11 +203,23 @@ class A_Pager {
 		return $this;
 	}
 
+	/**
+	 * setOrderByParameter
+	 *
+	 * @param mixed $name ???
+	 * @return A_Pager This object instance
+	 */
 	public function setOrderByParameter($name) {
 		$this->order_by_param = $name;
 		return $this;
 	}
 
+	/**
+	 * getOrderByParameter
+	 *
+	 * @param mixed $field ???
+	 * @return array ???
+	 */
 	public function getOrderByParameter($field) {
 		$key = array_search($field, $this->order_by_fields);
 		if ($key !== false) {
@@ -111,6 +230,12 @@ class A_Pager {
 		return $params;
 	}
 
+	/**
+	 * setCurrentPage
+	 *
+	 * @param mixed $n Page to show
+	 * @return A_Pager This object instance
+	 */
 	public function setCurrentPage($n) {
 		if ($this->last_row < 1) {			// do not access datasource if last_row has been set
 			$this->last_row = $this->datasource->getNumRows();
@@ -138,14 +263,30 @@ class A_Pager {
 		return $this;
 	}
 
+	/**
+	 * getPageSize
+	 *
+	 * @return mixed The current page size
+	 */
 	public function getPageSize() {
 		return $this->page_size;
 	}
 
+	/**
+	 * getCurrentPage
+	 *
+	 * @return mixed The current page
+	 */
 	public function getCurrentPage() {
 		return $this->current_page;
 	}
 
+	/**
+	 * getPrevPage
+	 *
+	 * @param integer $length ??? (optional)
+	 * @return mixed The index of the previous page
+	 */
 	public function getPrevPage($length=1)	{
 		if ($length < 1) {
 			$length = 1;
@@ -158,6 +299,12 @@ class A_Pager {
 		}		
 	}
 	
+	/**
+	 * getNextPage
+	 *
+	 * @param integer $length ??? (optional)
+	 * @return mixed The index of the next page
+	 */
 	public function getNextPage($length=1)	{
 		if ($length < 1) {
 			$length = 1;
@@ -170,30 +317,65 @@ class A_Pager {
 		}
 	}
 
+	/**
+	 * getLastPage
+	 *
+	 * @return mixed The index of the last page
+	 */
 	public function getLastPage() {
 		return $this->last_page;
 	}
 
+	/**
+	 * getFirstPage
+	 *
+	 * @return mixed The index of the first page
+	 */
 	public function getFirstPage() {
 		return $this->first_page;
 	}
 
+	/**
+	 * getFirstRow
+	 *
+	 * @return mixed ???
+	 */
 	public function getFirstRow() {
 		return $this->first_row;
 	}
 	
+	/**
+	 * getLastRow
+	 *
+	 * @return mixed ???
+	 */
 	public function getLastRow() {
 		return $this->last_row;
 	}
 	
+	/**
+	 * getStartRow
+	 *
+	 * @return mixed ???
+	 */
 	public function getStartRow() {
 		return $this->start_row;
 	}
 
+	/**
+	 * getEndRow
+	 *
+	 * @return mixed ???
+	 */
 	public function getEndRow() {
 		return $this->end_row;
 	}
 
+	/**
+	 * getValues
+	 *
+	 * @return mixed ???
+	 */
 	public function getValues() {
 		return array(
 			'page_size' => $this->page_size,
@@ -205,6 +387,13 @@ class A_Pager {
 			);
 	}
 
+	/**
+	 * getRange
+	 *
+	 * @param integer $range_start Beginning of range to get (optional)
+	 * @param integer $range_end End of range to get (optional)
+	 * @return array Range of results from $range_start to $range_end
+	 */
 	public function getRange($range_start=0, $range_end=0) {
 		if ($range_start == 0) {
 			// get number of links each side of current page
@@ -227,6 +416,13 @@ class A_Pager {
 		return range($range_start, $range_end);
 	}
 
+	/**
+	 * getRows
+	 *
+	 * @param integer $start_row ??? (optional)
+	 * @param integer $end_row ??? (optional)
+	 * @return mixed ???
+	 */
 	public function getRows($start_row=0, $end_row=0) {
 		if (($start_row == 0) && ($end_row == 0)) {
 			$start_row = $this->start_row;
@@ -235,6 +431,11 @@ class A_Pager {
 		return $this->datasource->getRows($start_row, $end_row);
 	}
 
+	/**
+	 * hasPages
+	 *
+	 * @return boolean True if there are more than one pages
+	 */
 	public function hasPages() {
 		return $this->last_page > 1;
 	}
--- trunk/A/Session.php
+++ trunk/A/Session.php
@@ -1,10 +1,17 @@
 <?php
 /**
- * Encapsulate session data 
+ * Encapsulate session data
  * 
- * @package A_Session 
+ * @package  A
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD
+ * @link	 http://skeletonframework.com/
  */
 
+/**
+ * A_Session
+ *
+ * This class provides various utility functions pertaining to the user session
+ */
 class A_Session {
 	protected $_data = array();
 	protected $_a_namespace = 'A_Session';
@@ -12,12 +19,24 @@ class A_Session {
 	protected $_regenerate;
 	protected $_isstarted = false;
 	protected $_p3p = '';
-		
+	
+	/**
+	 * __construct
+	 *
+	 * @param mixed $namespace ??? (optional)
+	 * @param mixed $regenerate ??? (optional)
+	 */
 	public function __construct($namespace=null, $regenerate=false) {
 		$this->initNamespace($namespace);
 		$this->_regenerate = $regenerate;
 	}
 	
+	/**
+	 * initNamespace
+	 *
+	 * @param mixed $namespace ??? (optional)
+	 * @return null
+	 */
 	public function initNamespace($namespace=null) {
 		if ($namespace) {
 			$this->_namespace = $namespace;
@@ -36,6 +55,11 @@ class A_Session {
 		}
 	}
 	
+	/**
+	 * setHandler
+	 *
+	 * @param mixed $handler ???
+	 */
 	public function setHandler($handler) 
 	{
 		session_set_save_handler(array(&$handler, 'open'),
@@ -54,10 +78,20 @@ class A_Session {
 		return $this;
 	}
 	
+	/**
+	 * setP3P
+	 *
+	 * @param string $policy ??? (optional)
+	 */
 	public function setP3P($policy='P3P: CP="CAO PSA OUR"') {
 		$this->_p3p = $policy;
 	}
-
+	
+	/**
+	 * start
+	 *
+	 * @return null
+	 */
 	public function start() {
 		if (session_id() == '') {
 			$msie = strstr($_SERVER['HTTP_USER_AGENT'], 'MSIE');
@@ -75,11 +109,22 @@ class A_Session {
 		}
 	}
 	
+	/**
+	 * get
+	 *
+	 * @param mixed $name ???
+	 * @param mixed $default ??? (optional)
+	 */
 	public function get($name, $default=null) {
 		$this->start();
 		return isset($this->_data[$name]) ? $this->_data[$name] : $default;
 	}
 	
+	/**
+	 * getRef
+	 *
+	 * @param mixed $name ??? (optional)
+	 */
 	public function & getRef($name=null) {
 		$this->start();
 		if ($name !== null) {
@@ -92,6 +137,14 @@ class A_Session {
 		}
 	}
 	
+	/**
+	 * set
+	 *
+	 * @param mixed $name ???
+	 * @param mixed $value ???
+	 * @param integer $count ??? (optional)
+	 * @return A_Session This object instance
+	 */
 	public function set($name, $value, $count=0) {
 		if ($name) {
 			$this->start();
@@ -107,24 +160,54 @@ class A_Session {
 		return $this;
 	}
 	
+	/**
+	 * has
+	 *
+	 * @param mixed $name ???
+	 * @return boolean True if session contains that data.  False otherwise
+	 */
 	public function has($name) {
 		$this->start();
 		return isset($this->_data[$name]);
 	}
 	
+	/**
+	 * __get
+	 *
+	 * @param mixed $name ???
+	 * @return mixed The object in the session with the key $name
+	 */
 	public function __get($name) {
 		return $this->get($name);
 	}
-
+	
+	/**
+	 * __set
+	 *
+	 * @param mixed $name ???
+	 * @param mixed $value ???
+	 * @return boolean True if successful
+	 */
 	public function __set($name, $value) {
 		return $this->set($name, $value);
 	}
-
+	
+	/**
+	 * expire
+	 *
+	 * @param mixed $name The key to set expiration for
+	 * @param mixed $count The expiration to set (optional)
+	 */
 	public function expire($name, $count=0) {
 		$this->start();
 		$_SESSION[$this->_a_namespace]['expire'][$name] = $count;
 	}
 	
+	/**
+	 * doExpiration
+	 *
+	 * @return null
+	 */
 	protected function doExpiration() {
 		if (isset($_SESSION[$this->_a_namespace]['expire'])) {
 			foreach ($_SESSION[$this->_a_namespace]['expire'] as $name => $value) {
@@ -138,10 +221,20 @@ class A_Session {
 		}
 	}
 	
+	/**
+	 * close
+	 *
+	 * @return null
+	 */
 	public function close() {
 		session_write_close();
 	}
 	
+	/**
+	 * destroy
+	 *
+	 * @return null
+	 */
 	public function destroy() {
 		if ($this->_namespace) {
 			$_SESSION[$this->_namespace] = array();
