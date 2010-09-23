3e330494aa3542100d05d00d0e4271c833ecf96c
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2010-09-23 08:21:33 +0000

    Fixed tests/menu.php so class names are properly displayed.

--- trunk/tests/menu.php
+++ trunk/tests/menu.php
@@ -11,10 +11,11 @@ function show_test_in_dir($base_dir, $dir) {
 	$test_ext = 'Test.php';
 	$length_ext = strlen($test_ext);
 	echo "<ul>\n";
+	$baseclassname = 'A_' . str_replace('/', '_', $dir);
 	foreach(glob("$base_dir$dir*") as $testfile) {
 		$filename = basename($testfile);
 		if (substr($filename, -$length_ext) == $test_ext) {									// show test scripts
-			$classname = 'A_' . substr($filename, 0, strlen($filename)-$length_ext);
+			$classname = $baseclassname . substr($filename, 0, strlen($filename)-$length_ext);
 			echo "<li><a href=\"all_tests.php?test=$dir$filename\">$classname</a></li>\n";
 		} elseif (is_dir($testfile) && ! in_array($filename, array('.', '..'))) {			// show only dirs but not ./..
 			echo "<li><a href=\"all_tests.php?test=$dir$filename\">$filename</a>\n";
