84267b1072bd753fa9af8a9548ff3578d102c8e7
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2010-10-02 08:38:07 +0000

    Fixed A_DateTime_Range so contains() works. Added intersects() method. Added test support for both methods.

--- trunk/tests/unit/DateTime/RangeTest.php
+++ trunk/tests/unit/DateTime/RangeTest.php
@@ -44,6 +44,81 @@ class RangeTest extends UnitTestCase	{
 		$this->range->getEnd ('Y-m-d');
 	}
 	
+	function testContains()	{
+		$datetime1 = new A_DateTime();
+		$datetime1->parseDate('2001/01/01');
+		$datetime2 = new A_DateTime();
+		$datetime2->parseDate('2001/02/01');
+		$datetime3 = new A_DateTime();
+		$datetime3->parseDate('2001/03/01');
+		$datetime4 = new A_DateTime();
+		$datetime4->parseDate('2001/04/01');
+		
+		$range = new A_DateTime_Range($datetime1, $datetime3);
+		$this->assertTrue($range->contains($datetime2));
+
+		$this->assertFalse($range->contains($datetime4));
+
+		$this->assertFalse($range->contains($datetime1));
+		$this->assertTrue($range->contains($datetime1, true));		// inclusive check of end dates
+
+		$this->assertFalse($range->contains($datetime3));
+		$this->assertTrue($range->contains($datetime3, true));		// inclusive check of end dates
+	}
+
+	function testIntersects()	{
+		$datetime1 = new A_DateTime();
+		$datetime1->parseDate('2001/01/01');
+		$datetime2 = new A_DateTime();
+		$datetime2->parseDate('2001/02/01');
+		$datetime3 = new A_DateTime();
+		$datetime3->parseDate('2001/03/01');
+		$datetime4 = new A_DateTime();
+		$datetime4->parseDate('2001/04/01');
+		$datetime5 = new A_DateTime();
+		$datetime5->parseDate('2001/05/01');
+		
+		// not overlapping
+		$range1 = new A_DateTime_Range($datetime1, $datetime2);
+		$range2 = new A_DateTime_Range($datetime3, $datetime4);
+		$this->assertFalse($range1->intersects($range2));
+		
+		// range1 end date in range2
+		$range1 = new A_DateTime_Range($datetime1, $datetime3);
+		$range2 = new A_DateTime_Range($datetime2, $datetime4);
+		$this->assertTrue($range1->intersects($range2));
+		
+		// range1 start date in range2
+		$range1 = new A_DateTime_Range($datetime3, $datetime5);
+		$range2 = new A_DateTime_Range($datetime1, $datetime4);
+		$this->assertTrue($range1->intersects($range2));
+		
+		// range1 the same as range2
+		$range1 = new A_DateTime_Range($datetime2, $datetime4);
+		$range2 = new A_DateTime_Range($datetime2, $datetime4);
+		$this->assertTrue($range1->intersects($range2));
+		
+		// range1 end date the same as range2 start date
+		$range1 = new A_DateTime_Range($datetime1, $datetime3);
+		$range2 = new A_DateTime_Range($datetime3, $datetime5);
+		$this->assertTrue($range1->intersects($range2));
+		
+		// range1 start date the same as range2 end date
+		$range1 = new A_DateTime_Range($datetime3, $datetime5);
+		$range2 = new A_DateTime_Range($datetime1, $datetime3);
+		$this->assertTrue($range1->intersects($range2));
+		
+		// range1 inside range2
+		$range1 = new A_DateTime_Range($datetime2, $datetime4);
+		$range2 = new A_DateTime_Range($datetime1, $datetime5);
+		$this->assertTrue($range1->intersects($range2));
+		
+		// range2 inside range1
+		$range1 = new A_DateTime_Range($datetime1, $datetime5);
+		$range2 = new A_DateTime_Range($datetime2, $datetime4);
+		$this->assertTrue($range1->intersects($range2));
+	}
+
 	function testToArray()	{
 		$date4 = new MockA_DateTime();
 		$date4->setReturnValue ('getTimestamp', strtotime ('1/1/10'));
