9be04ca7478dfea7302c155dafa0aeeaccbb07fb
jonahbron.d <jonahbron.d@gmail.com>
2010-11-24 01:14:19 +0000

    Created beginnings of WebSocket server.  Made Server, Request, and Client.  Probably doesn't work.

---
+++ A/WebSocket/Server.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * A_WebSocket_Server
+ *
+ * This handles connecting with WebSocket clients.  It also receives
+ * JSON data from clients, maps to controller/action, and dispatches.
+ *
+ * @package A_WebSocket
+ */
+class A_WebSocket_Server
+{
+
+	private $master;
+	
+	private $sockets = array();
+	
+	private $clients = array();
+	
+	private $locator;
+	
+	private $path;
+	/**
+	 * Constructor
+	 */
+	function __construct($locator, $path)
+	{
+		$this->locator = $locator;
+		$this->path = $path;
+	}
+
+	/**
+	 * Main server loop
+	 */
+	function run()
+	{
+		$this->prepareMaster();
+		
+		while (true) {
+			$changed_sockets = $this->sockets;
+			socket_select($changed_sockets, $write = NULL, $exceptions = NULL, NULL);
+				
+			foreach ($changed_sockets as $socket) {
+				if ($socket == $this->master) {
+					if (($resource = socket_accept($this->master)) < 0) {
+						// Socket error
+						continue;
+					} else {
+						$client = new A_WebSocket_Client($resource, $this);
+						$this->clients[$resource] = $client;
+						$this->sockets[] = $resource;
+					}
+				} else {
+					$client = $this->clients[$socket];
+					$bytes = socket_recv($socket, $data, 4096, 0);
+					if ($bytes === 0) {
+						unset($this->clients[$socket]);
+						$index = array_search($socket, $this->sockets);
+						unset($this->sockets[$index]);
+						unset($client);
+					} else {
+						if ($client->isConnected) {
+							$this->parseData($data);
+						} else {
+							$client->connect($data);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	protected function prepareMaster()
+	{
+		$this->master = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+		if ($master < 0) {
+			die('Could not create socket: ' . socket_strerror($this->master));
+		}
+
+		socket_set_option($this->master, SOL_SOCKET, SO_REUSEADDR, 1);
+
+		$res = socket_bind($this->master, $host, $port);
+		if ($res < 0) {
+			die('Could not bind socket: ' . socket_strerror($res));
+		}
+
+		$res = socket_listen($this->master, 5);
+		if ($res < 0) {
+			die('Could not listen to socket: ' . socket_strerror($res));
+		}
+
+		$this->sockets[] = $this->master;
+	}
+	
+	protected function parseData($data)
+	{
+		$firstChar = substr($data, 0, 1);
+		$endIndex = strpos($data, chr(255));
+		
+		while ($firstChar == chr(0) && $endIndex !== false) {
+			// get block and remove from data
+			$block = substr($data, 1, $endIndex - 1);
+			$data = substr($data, $endIndex);
+			
+			// do something with $block
+			$this->handleData($block);
+			
+			// get ready for next loop
+			$firstChar = substr($data, 0, 1);
+			$index = strpos($data, chr(255));
+		}
+	}
+	
+	protected function handleData($block)
+	{
+		$request = new A_WebSocket_Request($block);
+		$this->locator->set('Request', $request);
+		
+		$front = new A_Controller_Front($path, null, null);
+		$front->run($this->locator);
+	}
+}
