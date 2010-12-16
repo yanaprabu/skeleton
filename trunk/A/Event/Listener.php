014e84a6ce14c0bd489f7b563b57a24d9455626d
jonahbron.d@gmail.com <jonahbron.d@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2010-12-16 23:47:09 +0000

    Created stubs for Event library

---
+++ trunk/A/Event/Listener.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Event Listener object
+ * 
+ * Interface for listener objects being passed to Manager
+ * 
+ * @author Jonah <jonah[at]nucleussystems[dot]com>
+ */
+interface A_Event_Listener
+{
+	/**
+	 * Called when event is fired in Manager
+	 * 
+	 * @param object $eventObject
+	 */
+	public function onEvent($eventObject = null);
+}
---
+++ trunk/A/Event/Manager.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Event Manager
+ * 
+ * Handles creating, storing, and firing of events.
+ * 
+ * @author Jonah <jonah[at]nucleussystems[dot]com>
+ */
+class A_Event_Manager
+{
+
+	private $_events;
+	
+	public function __construct()
+	{
+		
+	}
+	
+	/**
+	 * Add event listener.  After being called, that eventName can be triggered
+	 * with fireEvent.
+	 * 
+	 * @param string $eventName
+	 * @param A_Event_Listener $eventListener
+	 */
+	public function addEventListener(string $eventName, A_Event_Listener $eventListener)
+	{
+		
+	}
+	
+	/**
+	 * Add multi-event listener.  This allows one object to listen to multiple
+	 * events.
+	 * 
+	 * @param string $eventName
+	 * @param A_Event_MultiListener $eventMultiListener
+	 */
+	public function addEventMultiListener(string $eventName, A_Event_MultiListener $eventMultiListener)
+	{
+		
+	}
+	
+	/**
+	 * Removes all listeners for the given event
+	 * 
+	 * @param string $eventName
+	 */
+	public function killEvent(string $eventName)
+	{
+		
+	}
+	
+	/**
+	 * Removes event listener.  If no more listeners are left on that event,
+	 * the event itself is removed.
+	 * 
+	 * @param A_Event_Listener $eventListener
+	 */
+	public function removeEventListener(A_Event_Listener $eventListener)
+	{
+		
+	}
+	
+	/**
+	 * Fires the event of the given name.  The object (optional) is passed to
+	 * the event handler.
+	 * 
+	 * @param string $eventName
+	 * @param object $eventObject
+	 */
+	public function fireEvent(string $eventName, object $eventObject = null)
+	{
+		
+	}
+}
