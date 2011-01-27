fce7a706b95571e24df97aec720cb4f2540c2f8a
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-01-27 21:31:46 +0000

    A_Logger - added logging levels, made output templated, added clear() method, added tests.

--- A/Logger.php
+++ A/Logger.php
@@ -14,10 +14,16 @@
  */
 class A_Logger {
 	protected $buffer = '';
+	protected $template = "{datetime} - {message}\n";
 	protected $writers = array();
+	protected $level = 0;		// logging level default to always write
 	protected $errorMsg = '';
 	
-	public function __construct($writers=array()) {
+	/**
+	 * @param $writer - filename (will create A_Logger_File($writer)) or array of writer objects
+	 * @param $level  - level to log messages
+	 */
+	public function __construct($writers=array(), $level=0) {
 		if ($writers) {
 			if (is_array($writers)) {
 				foreach($writers as $writer) {
@@ -27,36 +33,96 @@ class A_Logger {
 				$this->addWriter($writers);
 			}
 		}
+		$this->level = $level;
 	}
 	
-	public function getErrorMsg() {
-		return $this->errorMsg;
-	}
-	
+	/**
+	 * @param $writer - filename (will create A_Logger_File($writer)) or array of writer objects
+	 */
 	public function addWriter($writer) {
 		if (is_string($writer)) {
 			$this->writers[] = new A_Logger_File($writer);
 		} elseif (is_object($writer)) {
 			$this->writers[] = $writer;
 		}
+		return $this;
 	}
 	
-	public function log($message) {
-		$this->buffer .= date('Y-m-d H:i:s') . "\n$message\n";
+	/**
+	 * @param $template - string containing the {datetime} and {message} tags for replacement
+	 */
+	public function setTemplate($template) {
+		$this->template = $template;
+		return $this;
 	}
 	
-	public function write($message='') {
+	/**
+	 * @param $level  - level to write messages
+	 */
+	public function setLevel($level) {
+		$this->level = $level;
+		return $this;
+	}
+	
+	/**
+	 * Returns whether to log at given level
+	 * 
+	 * @param $level  - level to log messages
+	 */
+	public function isLoggable($level) {
+		return $level <= $this->level;
+	}
+	
+	/**
+	 * @param $message - string to write to log
+	 * @param $level   - level to log messages
+	 */
+	public function log($message, $level=0) {
+		$this->levels[$this->nMessages] = $level;
+		$this->messages[$this->nMessages] = str_replace(array('{datetime}', '{message}') , array(date('Y-m-d H:i:s'), $message), $this->template);
+		++$this->nMessages;
+		return $this;
+	}
+	
+	/**
+	 * Remove previous messages from all logs
+	 */
+	public function clear() {
+		foreach ($this->writers as $writer) {
+			$writer->clear();
+		}
+	}
+	
+	/**
+	 * @param $message - optional message to log
+	 */
+	public function write($level=null) {
 		if ($this->writers) {
-			if ($message) {
-				$this->buffer .= "$message\n";
+			if ($level !== null) {
+				$this->level = $level;
+			}
+			$buffer = '';
+			foreach ($this->messages as $n => $msg) {
+				if ($this->isLoggable($this->levels[$n])) {
+					$buffer .= $msg;
+				}
 			}
 			foreach ($this->writers as $writer) {	
-				$writer->write($this->buffer);
+				$writer->write($buffer);
 				$this->errorMsg .= $writer->getErrorMsg();
 			}
 		} else {
 			$this->errorMsg .= "No log writer. ";
 		}
+		return $this;
 	}
+
+	/**
+	 * Return current error message
+	 */
+	public function getErrorMsg() {
+		return $this->errorMsg;
+	}
+	
 }
 
