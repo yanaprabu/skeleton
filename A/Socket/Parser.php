ce7abb0ca564828589e1b09fd49b94099963a95e
jonahbron.d <jonahbron.d@gmail.com>
2011-01-27 01:00:41 +0000

    Removed WebSocket specific parsing code from Server.  Created new class for extracting messages from data, called A_Socket_Parser.
    @todo: find a way to tell A_Socket_Server which Message/Parser/Client to use.

---
+++ A/Socket/Parser.php
@@ -0,0 +1,18 @@
+<?php
+
+class A_Socket_Parser
+{
+
+	private $data;
+	
+	public function __construct($data)
+	{
+		$data->block = $data;
+	}
+	
+	public function parseMessages()
+	{
+		return array($data);
+	}
+	
+}
