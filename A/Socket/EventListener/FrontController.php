c5607173013844b047af8c3edffb643ef64c36b8
jonahbron.d <jonahbron.d@gmail.com>
2011-02-11 21:17:13 +0000

    A_Socket: Worked on improving chat demo.  Server end pretty much finished, but untested.

--- A/Socket/EventListener/FrontController.php
+++ A/Socket/EventListener/FrontController.php
@@ -3,28 +3,37 @@
 class A_Socket_EventListener_FrontController extends A_Socket_EventListener_Abstract
 {
 
-	public function  __construct($locator)
+	private $connectRequest;
+	private $disconnectRequest;
+	
+	public function  __construct($locator, $connectRequest, $disconnectRequest)
 	{
 		parent::__construct($locator);
+		$this->connectRequest = $connectRequest;
+		$this->disconnectRequest = $disconnectRequest;
 	}
 	
 	public function onConnect($data)
 	{
-		$data->setSession(new A_Collection());
+		$this->runController($this->connectRequest);
 	}
 	
 	public function onDisconnect($data)
 	{
-		
+		$this->runController($this->disconnectRequest);
 	}
 	
 	public function onMessage($data)
+	{
+		$this->runController(new A_Socket_Request($data));
+	}
+
+	public function runController($request)
 	{
 		$Locator = $this->_locator;
 
 		$Config = $Locator->get('Config');
 		
-		$Request = new A_Socket_Request($data);
 		$Locator->set('Request', $Request);
 		
 		$front = new A_Controller_Front($Config->get('APP'), $Config->get('DEFAULT_ACTION'), $Config->get('ERROR_ACTION'));
---
+++ examples/websocket/app/controllers/connect.php
@@ -0,0 +1,36 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of connect
+ *
+ * @author jonah
+ */
+class connect extends A_Controller_Action
+{
+	public function __construct($locator=null)
+	{
+		parent::__construct($locator);
+	}
+
+	public function user($locator = null)
+	{
+		$message = $locator->get('Request')->getData();
+
+		$client = new client();
+
+		$message->setSession(new A_Collection);
+
+		$message->getSession()->set('client', $client);
+
+		$message->reply(array(
+				'command' => 'new:user',
+				'data' => $client->serialize()
+			),
+			A_Socket_Message_Abstract::OTHERS
+		);
+	}
+}
