cc4961d4d43cad8bf4eff78cf63cf3056b4d2f41
jonahbron.d@gmail.com <jonahbron.d@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-02-16 03:23:04 +0000

    A_Socket: Finalized connect and disconnect event message system.  PHPDocumented Server.  Simplified FrontController.  Updated example.

--- trunk/A/Socket/Server.php
+++ trunk/A/Socket/Server.php
@@ -11,61 +11,115 @@
 class A_Socket_Server
 {
 
+	// client class interface to check
 	const BASE_CLIENT = 'A_Socket_Client_Abstract';
+	// message class interface to check
 	const BASE_MESSAGE = 'A_Socket_Message_Abstract';
 
+	// connect event identifier
 	const EVENT_CONNECT = 'a.socket.onconnect';
+	// disconnect event identifier
 	const EVENT_DISCONNECT = 'a.socket.ondisconnect';
+	// message event identifier
 	const EVENT_MESSAGE = 'a.socket.onmessage';
-	
-	private $_socket;
-	
-	private $_sockets = array();
-	
-	private $_clients = array();
-	
-	private $_host;
-	
-	private $_port;
-	
-	private $_eventManager;
 
-	private $_parser;
+	/**
+	 * Holds the main socket listening for connections
+	 * @var resource
+	 */
+	protected $_socket;
 
-	private $_client_class;
-
-	private $_message_class;
+	/**
+	 * Holds all connected sockets
+	 * @var array
+	 */
+	protected $_sockets = array();
 	
 	/**
-	 * Constructor
+	 * Holds all connected client objects
+	 * @var array
 	 */
-	public function __construct(
-			A_Socket_EventListener_Abstract $eventListener,
-			A_Socket_Parser $parser,
-			A_Socket_Message_Abstract $connectMessage,
-			A_Socket_Message_Abstract $disconnectMessage)
+	protected $_clients = array();
+
+	/**
+	 * The host to listen on
+	 * @var string
+	 */
+	protected $_host;
+
+	/**
+	 * The port to listen on
+	 * @var integer
+	 */
+	protected $_port;
+
+	/**
+	 * Event manager.  Used to fire events on connect, disconnect, and message
+	 * @var A_Event
+	 */
+	protected $_eventManager;
+
+	/**
+	 * Parser object to extract messages from read stream
+	 * @var A_Socket_Parser
+	 */
+	protected $_parser;
+
+	/**
+	 * The class name to use when instantiating a new client
+	 * @var string
+	 */
+	protected $_client_class;
+
+	/**
+	 * The class name to use when instantiating a new message
+	 * @var string
+	 */
+	protected $_message_class;
+
+	/**
+	 * The message to send when a connection is made
+	 * @var string
+	 */
+	protected $_connectMessage;
+
+	/**
+	 * The message to send when a client disconnects
+	 * @var string
+	 */
+	protected $_disconnectMessage;
+	
+	/**
+	 * Constructor.
+	 *
+	 * @param A_Socket_EventListener_Abstract $eventListener event listener object to handle events
+	 * @param A_Socket_Parser $parser parser object to extract messages from read stream
+	 */
+	public function __construct(A_Socket_EventListener_Abstract $eventListener, A_Socket_Parser $parser)
 	{
 		$this->_eventManager = new A_Event_Manager();
 		$this->_eventManager->addEventListener($eventListener);
 		$this->_parser = $parser;
-		$this->_connectMessage = $connectMessage;
-		$this->_disconnectMessage = $disconnectMessage;
 	}
 
 	/**
 	 * Main server loop
+	 *
+	 * @param array $config Configuration array
 	 */
 	public function run($config = array())
 	{
 		$this->_host = $config['host'];
 		$this->_port = $config['port'];
+		$this->_connectMessage = $config['message-connect'];
+		$this->_disconnectMessage = $config['message-disconnect'];
 
-		$this->_client_class = $config['client-class'];
+		$this->_client_class = $config['class-client'];
 		if (!is_subclass_of($this->_client_class, self::BASE_CLIENT)) {
 			throw new Exception('A_Socket_Server: the client class is invalid.');
 		}
 
-		$this->_message_class = $config['message-class'];
+		$this->_message_class = $config['class-message'];
 		if (!is_subclass_of($this->_message_class, self::BASE_MESSAGE)) {
 			throw new Exception('A_Socket_Server: the message class is invalid.');
 		}
@@ -79,6 +133,7 @@ class A_Socket_Server
 				
 			foreach ($updated_sockets as $socket) {
 				if ($socket == $this->_socket) {
+					// A new connection
 					$resource = socket_accept($socket);
 					if ($resource !== false) {
 						$client = new $this->_client_class($resource);
@@ -88,23 +143,28 @@ class A_Socket_Server
 						// socket error
 					}
 				} else {
+					// a new message
 					$client = $this->_clients[$socket];
 					$bytes = socket_recv($socket, $data, 4096, 0);
 					if ($bytes !== 0) {
+						// message is valid
 						if ($client->isConnected()) {
-							$this->parseData($data, $client);
+							$this->handleData($data, $client);
 						} else {
 							if ($client->connect($data)) {
 								$this->fireEvent(
 									self::EVENT_CONNECT,
-									$client
+									$client,
+									$this->_connectMessage
 								);
 							}
 						}
 					} else {
+						// client disconnected
 						$this->fireEvent(
 							self::EVENT_DISCONNECT,
-							$client
+							$client,
+							$this->_disconnectMessage
 						);
 						unset($this->_clients[$socket]);
 						$index = array_search($socket, $this->_sockets);
@@ -116,6 +176,9 @@ class A_Socket_Server
 		}
 	}
 
+	/**
+	 * Setup main socket to listen for new connections
+	 */
 	protected function initializeSocket()
 	{
 		$this->_socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
@@ -137,8 +200,14 @@ class A_Socket_Server
 
 		$this->_sockets[] = $this->_socket;
 	}
-	
-	protected function parseData($data, $client)
+
+	/**
+	 * Take data, parse messages, and fire message event
+	 * 
+	 * @param string $data Message received
+	 * @param object $client Client that sent the message
+	 */
+	protected function handleData($data, $client)
 	{
 		$blocks = $this->_parser->parseMessages($data);
 		
@@ -151,23 +220,19 @@ class A_Socket_Server
 			);
 		}
 	}
-	
+
+	/**
+	 * Convenience function for firing events
+	 * 
+	 * @param string $event Event to fire
+	 * @param object $client Client that initiated the event
+	 * @param string $message Message received from client
+	 */
 	protected function fireEvent($event, $client, $message = null)
 	{
-		switch ($event) {
-			case self::EVENT_CONNECT:
-				$message = $this->_connectMessage;
-				$message->setClients($client, $this->_clients);
-				break;
-			case self::EVENT_DISCONNECT:
-				$message = $this->_disconnectMessage;
-				$message->setClients($client, $this->_clients);
-				break;
-			case self::EVENT_MESSAGE:
-				$message = new $this->_message_class($message, $client, $this->_clients);
-				break;
-		}
-		
-		$this->_eventManager->fireEvent($event, $message);
+		$this->_eventManager->fireEvent(
+			$event,
+			new $this->_message_class($message, $client, $this->_clients)
+		);
 	}
 }
