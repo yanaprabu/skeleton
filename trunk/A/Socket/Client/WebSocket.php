56d057af1c7d6d64f2b0754a69a4470f08d300e4
jonahbron.d@gmail.com <jonahbron.d@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-02-20 00:55:48 +0000

    A_Socket: removed Parser interface/class, and moved it's responsibility into Client where it belongs.

--- trunk/A/Socket/Client/WebSocket.php
+++ trunk/A/Socket/Client/WebSocket.php
@@ -31,6 +31,35 @@ class A_Socket_Client_WebSocket extends A_Socket_Client_Abstract
 	}
 
 	/**
+	 * Extract messages from data stream
+	 * 
+	 * @param string $data
+	 * @return array
+	 */
+	public function receive($data)
+	{
+		$blocks = array();
+
+		$firstChar = substr($data, 0, 1);
+		$endIndex = strpos($data, chr(255));
+
+		while ($firstChar == chr(0) && $endIndex !== false) {
+			// get block and remove from data
+			$block = substr($data, 1, $endIndex - 1);
+			$data = substr($data, $endIndex);
+
+			// store the block
+			$blocks[] = $block;
+
+			// get ready for next loop
+			$firstChar = substr($data, 0, 1);
+			$index = strpos($data, chr(255));
+		}
+
+		return $blocks;
+	}
+
+	/**
 	 * Perform WebSocket rev 76 handshake with client
 	 * 
 	 * @param string $data
