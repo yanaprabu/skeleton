ab9102d1ac97a839e5796d9c6687f6fc61c95cf6
jonahbron.d <jonahbron.d@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-05-01 22:09:09 +0000

    Created Equals rule to check fields for specific value.  Strict or dynamic typed comparison optional.

---
+++ trunk/A/Rule/Equals.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Rule to make sure a field matches a given value
+ *
+ * The first parameter is the value to compare to.  The second value defines whether or not the comparison ought to be
+ * strict.  The third parameter is the name of the field.  The fourth parameter sets the error message.
+ * 
+ * @package A_Rule_Set
+ */
+
+class A_Rule_Equals extends A_Rule_Base {
+	const ERROR = 'A_Rule_Equals';
+	protected $params = array(
+							'value' => '',
+							'strict' => '',
+							'field' => '', 
+							'errorMsg' => '', 
+							'optional' => false
+							);
+							
+    protected function validate() {
+      return $this->params['strict'] ? $this->getValue() === $this->params['value'] : $this->getValue() == $this->params['value'];
+    }
+}
