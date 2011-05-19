582b20d3473cccfcb8fd6908d444d0bfb54e8956
jonahbron.d <jonahbron.d@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-05-19 00:29:03 +0000

    Added test for form field render

--- trunk/tests/unit/Model/Form/FieldTest.php
+++ trunk/tests/unit/Model/Form/FieldTest.php
@@ -17,4 +17,30 @@ class Model_Form_FieldTest extends UnitTestCase {
 		$this->assertFalse(!$result);
 	}
 	
+	function testRender() {
+		$field = new A_Model_Form_Field('foo');
+		$field->setValue('bar');
+		
+		$renderer = new MockRenderer();
+		$field->setRenderer($renderer);
+		$field->render();
+		
+		$data = $renderer->getData();
+		$this->assertEqual('foo', $data['name']);
+		$this->assertEqual('bar', $data['value']);
+	}
+	
+}
+
+class MockRenderer {
+	private $data;
+	public function import($data) {
+		$this->data = $data;
+	}
+	public function getData() {
+		return $this->data;
+	}
+	public function render() {
+		return null;
+	}
 }
