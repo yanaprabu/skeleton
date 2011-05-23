5c71d01b624ff69c43641b032bfc55607c529d2c
jonahbron.d <jonahbron.d@gmail.com>
2011-05-23 22:34:14 +0000

    Cleaned up syntax in A_Sql

--- A/Sql.php
+++ A/Sql.php
@@ -12,7 +12,8 @@
  *
  * Wrap select, insert, update, delete SQL generators in a single inteface
  */
-class A_Sql {
+class A_Sql
+{
 	protected $connection;
 	protected $select = null;
 	protected $insert = null;
@@ -24,7 +25,8 @@ class A_Sql {
 	 *
 	 * @param mixed $connection MySQL connection to use
 	 */
-	public function __construct($connection=null) {
+	public function __construct($connection=null)
+	{
 		$this->connection = $connection;
 	}
 	
@@ -34,36 +36,34 @@ class A_Sql {
 	 * @param mixed $name ???
 	 * @param mixed $args ???
 	 */	
-	public function __call($name, $args) {
+	public function __call($name, $args)
+	{
 		switch ($name) {
 			case 'select':
 				if (!$this->select) {
-					#include_once 'A/Sql/Select.php';
 					$this->select = new A_Sql_Select($this->connection);
 				}
 				return $this->select->columns($args);
 			break;
 			case 'insert':
 				if (!$this->insert) {
-					#include_once 'A/Sql/Insert.php';
 					$this->insert = new A_Sql_Insert($this->connection);
 				}
 				return $this->insert->table($args);
 			break;
 			case 'update':
 				if (!$this->update) {
-					#include_once 'A/Sql/Update.php';
 					$this->update = new A_Sql_Update($this->connection);
 				}
 				return $this->update->table($args);
 			break;
 			case 'delete':
 				if (!$this->delete) {
-					#include_once 'A/Sql/Delete.php';
 					$this->delete = new A_Sql_Delete($this->connection);
 				}
 				return $this->delete->table($args);
 			break;
 		}
 	}
+
 }
