9b7ee7579f25a7646ff0d800ff0c0aa6bf809977
jonahbron.d <jonahbron.d@gmail.com>
2011-05-23 21:33:44 +0000

    Cleaned up syntax in A_Application

--- A/Application.php
+++ A/Application.php
@@ -13,7 +13,8 @@
  *
  *  Wrap bootstrap in a class that has configuration settings
  */
-class A_Application {
+class A_Application
+{
 	protected $exception;
 	protected $includePath = '';
 	protected $configPath = array('config.ini', 'skeleton');	
@@ -30,87 +31,93 @@ class A_Application {
 		'Front'    => array('A_Controller_Front', false),
 	);
 	
-	public function __construct($exception = null) {
+	public function __construct($exception = null)
+	{
 		$this->exception = $exception;
 	}
 	
-   public function run() {
-   	if (!$this->includePath) $this->setPath($_SERVER['DOCUMENT_ROOT']);
+	public function run()
+	{
+		if (!$this->includePath) $this->setPath($_SERVER['DOCUMENT_ROOT']);
 		set_include_path(get_include_path() . PATH_SEPARATOR . $this->includePath);
 		array_walk($this->loadComponents, array($this, 'initialize'));
 		
-   		$this->component('Pathinfo')->run($this->component('Request'));
-      	$this->component('Front')->run($this->component('Locator'));
-      	if (!$this->component('Response')->hasRenderer()) {
+		$this->component('Pathinfo')->run($this->component('Request'));
+		$this->component('Front')->run($this->component('Locator'));
+		if (!$this->component('Response')->hasRenderer()) {
 			$this->component('Response')->setRenderer($this->components('Template'));
-      	}
-      	return $this->component('Response')->render();           
-   }
-   
-   public function component($component) {
+		}
+		return $this->component('Response')->render();           
+	}
+	
+	public function component($component)
+	{
 		if (!isset($this->components[$component])) {
 			if ($this->component('Config')->useExceptions) {
-	         	#include_once 'A/Exception.php';
 	         	throw A_Exception::getInstance($this->exception, 'Component "'. $component .' does not exist in stack"');				
 			}
 			return false;
 		}
 		return $this->components[$component];
-   }
+	}
 	
-	public function initConfig($component) {
+	public function initConfig($component)
+	{
 		$config = new $component($this->configPath[0], $this->configPath[1]);
 		return $config->loadFile();
 	}
 
-	public function initMapper($component) {
-		#include_once 'A/DL.php';
+	public function initMapper($component)
+	{
 		$config = $this->component('Config');
 		$defaultAction = new A_DL('', $config->defaultController, $config->defaultAction);
 		return new $component($config->applicationPath, $defaultAction);
 	}
 	
-	public function initFront($component) {
-		#include_once 'A/DL.php';
+	public function initFront($component)
+	{
 		$mapper = $this->component('Mapper');
 		$config = $this->component('Config');
 		$defaultAction = new A_DL('', $config->errorController, $config->errorAction);
 		return new $component($mapper, $defaultAction);
 	}
 
-	public function initSession($component) {
+	public function initSession($component)
+	{
 		$session = new $component('A');
 		$config = $this->component('Config');
 	  	if ($config->sessionHandler == 'database') {
-			##include_once 'A/Session/Handler/Database.php';
 			#$session->setHandler(new A_Session_Handler_Database());	  			
 	  	} else {
-  			##include_once 'A/Session/Handler/Filesystem.php';
   			#$session->setHandler(new A_Session_Handler_Filesystem($config->sessionPath));	  		
 	  	}
 	  	return $session;
 	}
 	
-	public function __call($method, $args) {
-		//only want to intercept component initializations
+	public function __call($method, $args)
+	{
+		// intercept component initialization
 		if (substr($method, 0, 4) == 'init') {
 			return new $args[0];
 		}	
 		return false;
 	}
 	
-	public function set($component, $object, $register = true) {
+	public function set($component, $object, $register = true)
+	{
 		if (is_string($object)) $this->load($component);
 		$this->loadComponents[$component] = array($object, (bool)$register);
 		return $this;
 	}
 
-	public function setPath($path) {
+	public function setPath($path)
+	{
 		$this->includePath = $path;
 		return $this;
 	}
 	
-	protected function initialize($component, $key) {
+	protected function initialize($component, $key)
+	{
 		$key = ucfirst($key);
 		if (!is_object($component[0])) {
 			$this->load($component[0]);
@@ -124,10 +131,10 @@ class A_Application {
 			$this->component('Locator')->set($key, $this->components[$key]);
 		}
 		return $this;
-	}		
+	}
 	
 	protected function load($class) {
 		return include_once str_replace('_', DIRECTORY_SEPARATOR, $class).'.php';
-	}   
+	}
 } 
 
