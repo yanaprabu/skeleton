6bff21679bb35c0a7ab101fd7f4b4c4ba1726533
jonahbron.d <jonahbron.d@gmail.com>
2011-05-23 22:15:32 +0000

    Cleaned up syntax in A_Json

--- A/Json.php
+++ A/Json.php
@@ -12,11 +12,13 @@
  *
  * Json encoding and decoding
  */
-class A_Json {
+class A_Json
+{
 	protected $encoded = null;  // holds passed or last encoded JSON strings
 	protected $decoded = null;  // holds passed or last decoded PHP object or array
 
-	public function __construct($mixed = null) {
+	public function __construct($mixed = null)
+	{
 		if (is_string($mixed) && (substr($mixed, 0, 1) == '{')) {
 			$this->encoded = $mixed;	// var is JSON string
 		} else {
@@ -24,17 +26,20 @@ class A_Json {
 		}
 	}
 
-	public function encode($mixed) {
+	public function encode($mixed)
+	{
 		$this->encoded = json_encode(isset($mixed) ? $mixed : $this->decoded);
 		return $this->encoded;
 	}
 
-	public function decode($mixed=null, $array=false) {
+	public function decode($mixed=null, $array=false)
+	{
 		$this->decoded = json_decode(isset($mixed) ? $mixed : $this->encoded, $array);
 		return $this->decoded;
 	}
 
-	public function decodeInto($into, $mixed=null, $intersect=true) {
+	public function decodeInto($into, $mixed=null, $intersect=true)
+	{
 		$array = json_decode(isset($mixed) ? $mixed : $this->encoded, true);
 		if (is_array($into)) {
 			if ($intersect) {
