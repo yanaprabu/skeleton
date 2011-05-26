6c18de472fdbc3daf878ab5141bef81bbdf4b723
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-05-26 23:35:20 +0000

    A_Email and A_Email_Phpmail - added connect() and disconnect() methods.

--- A/Email/Phpmail.php
+++ A/Email/Phpmail.php
@@ -1,41 +1,34 @@
 <?php
 /**
- * Phpmail.php
- * 
- * @package  A
+ * A_Email_Phpmail
+ *
+ * Connection to default PHP mail() function
+ *
+ * @package  A_Email
  * @license  http://www.opensource.org/licenses/bsd-license.php BSD
  * @link	 http://skeletonframework.com/
  */
 
-/**
- * A_Email_Phpmail
- * 
- * Provides email functionality through PHP's built-in mail() function.  To be used in composition with A_Email.
- */
 class A_Email_Phpmail
 {
 	protected $error = 0;
 	
-	/**
-	 * Sends an email with the given settings
-	 * 
-	 * @param string $to
-	 * @param string $subject
-	 * @param string $message
-	 * @param string $headers
-	 * @return true upon success
-	 */
+	public function connect()
+	{
+		return true;
+	}
+
+	public function disconnect()
+	{
+		return true;
+	}
+
 	public function send($to='', $subject='', $message='', $headers='')
 	{
 		$this->error = mail($to, $subject, $message, $headers);
 		return $this->error;
 	}
-	
-	/**
-	 * Produces a textual error message
-	 * 
-	 * @return error message
-	 */
+
 	public function getErrorMsg()
 	{
 		return $this->error ? "Send error {$this->error} using mail. " : '';
