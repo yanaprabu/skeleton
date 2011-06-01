fc550a97e5baffe4bc9655ef5ddc6e3b332fe803
jonahbron.d <jonahbron.d@gmail.com>
2011-06-01 17:56:02 +0000

    Removed superfluous includes from A_Filter_**.  Changed Smartquotes filter slightly.

--- A/Filter/Regexp.php
+++ A/Filter/Regexp.php
@@ -1,5 +1,4 @@
 <?php
-#include_once 'A/Filter/Abstract.php';
 /**
  * Regexp.php
  *
--- A/Filter/Smartquotes.php
+++ A/Filter/Smartquotes.php
@@ -1,5 +1,4 @@
 <?php
-#include_once 'A/Filter/Abstract.php';
 /**
  * Smartquotes.php
  *
@@ -16,20 +15,14 @@
 class A_Filter_Smartquotes extends A_Filter_Base {
 	
 	public function filter () {
-		/*
-		 * by Chris Shiflett (http://shiflett.org/)
-		 */
-		$search = array(chr(145), 
-	                    chr(146), 
-	                    chr(147), 
-	                    chr(148), 
-	                    chr(151)); 
-	    $replace = array("'", 
-	                     "'", 
-	                     '"', 
-	                     '"', 
-	                     '-'); 
-	    return str_replace($search, $replace, $this->getValue()); 
+		$smartQuotes = array(
+			chr(145) => "'",
+			chr(146) => "'",
+			chr(147) => '"',
+			chr(148) => '"',
+			chr(151) => '-'
+		);
+	    return str_replace(array_keys($search), array_values($search), $this->getValue()); 
 	}
 
 }
