6edb53c468c26767908ff91d4c717514d1e63c72
jonahbron.d@gmail.com <jonahbron.d@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-06-01 19:15:07 +0000

    Fixed A_Http package

--- trunk/A/Http/Download.php
+++ trunk/A/Http/Download.php
@@ -2,7 +2,7 @@
 /**
  * Download.php
  *
- * @package  A
+ * @package  A_Http
  * @license  http://www.opensource.org/licenses/bsd-license.php BSD
  * @link	 http://skeletonframework.com/
  */
--- trunk/A/Http/Request.php
+++ trunk/A/Http/Request.php
@@ -2,7 +2,7 @@
 /**
  * Request.php
  *
- * @package  A
+ * @package  A_Http
  * @license  http://www.opensource.org/licenses/bsd-license.php BSD
  * @link	 http://skeletonframework.com/
  */
