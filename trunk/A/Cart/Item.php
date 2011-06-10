8f2f3267e36216019059f987920eee9d1473dcc7
jonahbron.d@gmail.com <jonahbron.d@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-06-10 17:32:23 +0000

    Broke A_Cart_Itemdata out into it's own file.

--- trunk/A/Cart/Item.php
+++ trunk/A/Cart/Item.php
@@ -233,47 +233,3 @@ class A_Cart_Item
 	}
 
 }
-
-class A_Cart_Itemdata
-{
-		protected $name = '';	protected $value = '';	protected $options = null;
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param string $name
-	 * @param mixed $value
-	 * @param mixed $options
-	 */
-	public function __construct($name, $value, $options=null)
-	{
-		$this->name = $name;
-		$this->value = $value;
-		$this->options = $options;
-	}
-	
-	/**
-	 * @return string
-	 */
-	public function getName()
-	{
-		return($this->name);
-	}
-	
-	/**
-	 * @return mixed
-	 */
-	public function getValue()
-	{
-		return($this->value);
-	}
-	
-	/**
-	 * @return mixed
-	 */
-	public function getOptions()
-	{
-		return($this->options);
-	}
-
-}
---
+++ trunk/A/Cart/Itemdata.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Itemdata.php
+ *
+ * @package  A_Cart
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD
+ * @link	 http://skeletonframework.com/
+ */
+
+/**
+ * A_Cart_Itemdata
+ */
+class A_Cart_Itemdata
+{
+	
+	protected $name = '';
+	protected $value = '';
+	protected $options = null;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param string $name
+	 * @param mixed $value
+	 * @param mixed $options
+	 */
+	public function __construct($name, $value, $options=null)
+	{
+		$this->name = $name;
+		$this->value = $value;
+		$this->options = $options;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return($this->name);
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getValue()
+	{
+		return($this->value);
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getOptions()
+	{
+		return($this->options);
+	}
+
+}
