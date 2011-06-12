ac8fb91d0da1aafce0cbd5ca2b6e701bf075e376
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-06-12 02:23:21 +0000

    A_Cart_Request and examples/cart - Fixed problem with newitems.

--- examples/cart/app/controllers/cart.php
+++ examples/cart/app/controllers/cart.php
@@ -24,32 +24,34 @@ class cart extends A_Controller_Action
 		// get any items added so we can fetch associated product records
 		$newitems = $cartrequest->getNewItems();
 		
-		// get SKUs for query
-		$skus = array();
-		foreach ($newitems as $item) {
-			$skus[] = $item->getProductID();
+		if ($newitems) {
+			// get SKUs for query
+			$skus = array();
+			foreach ($newitems as $item) {
+				$skus[] = $item->getProductID();
+			}
+			// get all new product records in one query
+			$rows = $product->findProductsSkus($skus);
+			
+			// assign product data to cart items for display
+			foreach ($newitems as $item) {
+				$sku = $item->getProductID();
+				foreach ($rows as $row) {
+					if ($row['sku'] == $sku) {
+						$item->setId($row['id']);
+						$item->setUnitPrice($row['price']);
+						$item->setData('name', $row['name']);
+						$item->setData('color', $row['color']);
+						$item->setData('size', $row['size']);
+						break;
+					}
+				}			
+			}
+			
+			// add new items now that they have had product data assigned
+			$cartrequest->addNewItems();
 		}
-		// get all new product records in one query
-		$rows = $product->findProductsSkus($skus);
 		
-		// assign product data to cart items for display
-		foreach ($newitems as $item) {
-			$sku = $item->getProductID();
-			foreach ($rows as $row) {
-				if ($row['sku'] == $sku) {
-					$item->setId($row['id']);
-					$item->setUnitPrice($row['price']);
-					$item->setData('name', $row['name']);
-					$item->setData('color', $row['color']);
-					$item->setData('size', $row['size']);
-					break;
-				}
-			}			
-		}
-		
-		// add new items now that they have had product data assigned
-		$cartrequest->addNewItems();
-
 		$response->setPartial('content', 'cart', array('product'=>$product, 'cart'=>$cart, 'cartrequest'=>$cartrequest));
 		
 	}
