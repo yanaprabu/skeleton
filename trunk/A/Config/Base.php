8cd929ff60d976b2980f4bfa6a7ebe82f62c3903
jonahbron.d@gmail.com <jonahbron.d@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-06-14 23:03:20 +0000

    Cleaned up syntax in A/Config/

--- trunk/A/Config/Base.php
+++ trunk/A/Config/Base.php
@@ -12,42 +12,36 @@
  * 
  * Abstract base class for configuration
  */
-abstract class A_Config_Base extends A_Collection {
+abstract class A_Config_Base extends A_Collection
+{
+	
 	protected $_filename;
 	protected $_section;
 	protected $_exception;
 	protected $_error = 0;
 	protected $_errorMsg = '';
 	
-	/**
-	 *
-	 */
-	public function __construct($filename='', $section='', $exception=null) {
+	public function __construct($filename='', $section='', $exception=null)
+	{
 		$this->_filename = $filename;
 		$this->_section = $section;
 		$this->_exception = $exception;
 	}
 	
-	/**
-	 *
-	 */
-	public function setCollectionClass($collection_class) {
+	public function setCollectionClass($collection_class)
+	{
 		$this->_collectionClass = $collection_class;
 		return $this;
 	}
-
-	/**
-	 *
-	 */
-	public function setException($exception=null) {
+	
+	public function setException($exception=null)
+	{
 		$this->_exception = $exception;
 		return $this;
 	}
-
-	/**
-	 *
-	 */
-	public function loadFile($filename='', $section='') {
+	
+	public function loadFile($filename='', $section='')
+	{
 		if ($filename) {
 			$this->_filename = $filename;
 		}
@@ -58,20 +52,18 @@ abstract class A_Config_Base extends A_Collection {
 		$data = $this->_loadFile();
 		restore_error_handler();
 	  
-		//if there was a problem loading the file
+			//if there was a problem loading the file
 		if (($this->_error || !count($data))
-		//if the requested section does not exist
-		|| ($this->_section && !isset($data[$this->_section]))) {
+			//if the requested section does not exist
+			|| ($this->_section && !isset($data[$this->_section]))) {
 			return false;
 		}
 		$this->_data = ($this->_section ? $data[$this->_section] : $data);
 		return $this;
 	}
 	
-	/**
-	 *
-	 */
-	public function errorHandler($errno, $errstr, $errfile, $errline) {
+	public function errorHandler($errno, $errstr, $errfile, $errline)
+	{
 		$this->_error = $errno;
 		$this->_errorMsg = $errstr;
 		if ($this->_exception) {
@@ -79,27 +71,25 @@ abstract class A_Config_Base extends A_Collection {
 		}
 	}
 	
-	/**
-	 *
-	 */
-	public function isError() {
+	public function isError()
+	{
 		return $this->_error;
 	}
 	
-	/**
-	 *
-	 */
-	public function getErrorMsg() {
+	public function getErrorMsg()
+	{
 		return $this->_errorMsg;
 	}
+	
 	/**
 	 * Pass configuration data registered by class name to an object's config() method
 	 */
-	
-	protected function configure($obj) {
+	protected function configure($obj)
+	{
 		$class = get_class($obj);
 		if (($class !== false) && $this->has($class) && method_exists($obj, 'config')) {
 			$obj->config($this->get($class));
 		}
 	}
+
 }
--- trunk/A/Config/Xml.php
+++ trunk/A/Config/Xml.php
@@ -12,9 +12,12 @@
  * 
  * Support for reading XML configuration files
  */
+class A_Config_Xml extends A_Config_Base
+{
 
-class A_Config_Xml extends A_Config_Base {
-   protected function _loadFile() {
-      return simplexml_load_file($this->_filename);
-   }
+	protected function _loadFile()
+	{
+		return simplexml_load_file($this->_filename);
+	}
+
 }
