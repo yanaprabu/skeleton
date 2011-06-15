67ccce51ba6c1e13a439f1f295a398c018b45a9b
jonahbron.d@gmail.com <jonahbron.d@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-06-15 00:48:32 +0000

    Cleaned up syntax in A/Controller/Action and App.

--- trunk/A/Controller/App.php
+++ trunk/A/Controller/App.php
@@ -12,41 +12,48 @@
  * 
  * Application controller class for state/transition based action selection
  */
-class A_Controller_App extends A_Controller_Input {
+class A_Controller_App extends A_Controller_Input
+{
+
 	protected $state_name = '';
 	protected $state_name_init = '';
 	protected $no_init_errors = true;
 	protected $states = array();
 	protected $transition = array();
-
-	public function __construct($locator=null, $state_name='') {
+	
+	public function __construct($locator=null, $state_name='')
+	{
 	    parent::__construct($locator);
 		$this->setInitState($state_name);
 	}
-
-	public function setInitState($state_name) {
+	
+	public function setInitState($state_name)
+	{
 		$this->state_name = $state_name;
 		$this->state_name_init = $state_name;
 		return $this;
 	}
-
-	public function addState($state) {
+	
+	public function addState($state)
+	{
 		if (($state instanceof A_Controller_App_State) && $state->name) {
 			$this->states[$state->name] = $state;
 		}
 		return $this;
 	}
 	
-	public function addTransition($transition) {
+	public function addTransition($transition)
+	{
 		if ($transition->fromState && $transition->toState) {
 			$this->transitions[$transition->fromState][] = $transition;
 		}
 		return $this;
 	}
 	
-	public function findState($request) {
+	public function findState($request)
+	{
 		// if no initial state then set to first state
-		if (! $this->state_name && $this->states) {
+		if (!$this->state_name && $this->states) {
 			reset($this->states);
 			$state = current($this->states);
 			$this->state_name = $state->name;
@@ -54,7 +61,7 @@ class A_Controller_App extends A_Controller_Input {
 		do {
 			$run = false;
 			$n = count($this->transitions[$this->state_name]);
-			for ($i=0; $i<$n; ++$i) {
+			for ($i = 0; $i < $n; $i++) {
 				if ($this->transitions[$this->state_name][$i]->rule->isValid($request) && $this->transitions[$this->state_name][$i]->condition) {
 					$this->state_name = $this->transitions[$this->state_name][$i]->getToState();
 					if (isset($this->transitions[$this->state_name])) {
@@ -64,24 +71,25 @@ class A_Controller_App extends A_Controller_Input {
 				}
 			}
 		} while ($run);
-
+		
 		if (isset($this->states[$this->state_name])) {
 			return $this->states[$this->state_name];
 		}
 		$handler = null;
 		return $handler;
 	}
-
-    public function run($locator) {
+	
+    public function run($locator)
+    {
 		$state = $this->findState($locator->get('Request'));
-
+		
 		// clear any error messages on init
 		if ($this->no_init_errors && ($this->state_name_init == $state->name)) {
 			foreach (array_keys($this->params) as $field) {
 				$this->params[$field]->setError(null);
 			}
 		}
-
+		
 		// run code for this state
 		if (isset($state->handler)) {
 			if (is_object($state->handler)) {
@@ -99,15 +107,18 @@ class A_Controller_App extends A_Controller_Input {
  *
  * @package A_Controller
  */
-class A_Controller_App_State {
+class A_Controller_App_State
+{
+
 	public $name;
 	public $handler;
-
-	public function __construct($name, $handler) {
+	
+	public function __construct($name, $handler)
+	{
 		$this->name = $name;
 		$this->handler = $handler;
 	}
-	
+
 }
 
 /**
@@ -115,20 +126,24 @@ class A_Controller_App_State {
  *
  * @package A_Controller
  */
-class A_Controller_App_Transition {
+class A_Controller_App_Transition
+{
+
 	public $fromState;
 	public $toState;
 	public $rule;
 	public $condition;
-
-	public function __construct($fromState, $toState, $rule, $condition=true) {
+	
+	public function __construct($fromState, $toState, $rule, $condition=true)
+	{
 		$this->fromState = $fromState;
 		$this->toState = $toState;
 		$this->rule = $rule;
 		$this->condition = $condition;
 	}
 	
-	public function getToState() {
+	public function getToState()
+	{
 		return $this->toState;
 	}
 	
