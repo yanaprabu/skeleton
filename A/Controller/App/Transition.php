d6c349fa187c29c11c7dc1a54c70f460a72f5052
jonahbron.d <jonahbron.d@gmail.com>
2011-06-15 00:51:03 +0000

    Broke A_Controller_App_State and A_Controller_App_Transition out into their own files.

---
+++ A/Controller/App/Transition.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Transition.php
+ *
+ * @package  A_Controller
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD
+ * @link	 http://skeletonframework.com/
+ */
+
+/**
+ * A_Controller_App_Transition
+ * 
+ * Application controller class to hold transition information.
+ */
+class A_Controller_App_Transition
+{
+
+	public $fromState;
+	public $toState;
+	public $rule;
+	public $condition;
+	
+	public function __construct($fromState, $toState, $rule, $condition=true)
+	{
+		$this->fromState = $fromState;
+		$this->toState = $toState;
+		$this->rule = $rule;
+		$this->condition = $condition;
+	}
+	
+	public function getToState()
+	{
+		return $this->toState;
+	}
+	
+}
