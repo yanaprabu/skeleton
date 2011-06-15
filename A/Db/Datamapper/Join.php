a26d42f01db89fbc37b8cab8308ff435dbb5b385
jonahbron.d <jonahbron.d@gmail.com>
2011-06-15 19:36:02 +0000

    Broke A_Db_Datamapper_Join and A_Db_Datamapper_Mapping out into their own files.

---
+++ A/Db/Datamapper/Join.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Join.php
+ *
+ * @package  A_Db
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD
+ * @link	 http://skeletonframework.com/
+ */
+
+/**
+ * A_Db_Datamapper_Join
+ */
+class A_Db_Datamapper_Join
+{
+
+	public $table1; 
+	public $field1;
+	public $table2; 
+	public $field2;
+	public $type;
+	
+	function __construct($table1, $field1, $table2, $field2, $type='')
+	{
+		$this->table1 = $table1; 
+		$this->field1 = $field1;
+		$this->table2 = $table2; 
+		$this->field2 = $field2;
+		$this->type = $type;
+	}
+	
+	function render()
+	{
+		return " {$this->table1} {$this->type} JOIN {$this->table2} ON {$this->table1}.{$this->field1}={$this->table2}.{$this->field2} ";
+	}
+
+}
---
+++ A/Db/Datamapper/Mapping.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Mapping.php
+ *
+ * @package  A_Db
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD
+ * @link	 http://skeletonframework.com/
+ */
+
+/**
+ * A_Db_Datamapper_Mapping
+ */
+class A_Db_Datamapper_Mapping
+{
+
+	public $table_name = '';
+	public $property_name = '';
+	public $field_name = '';
+	public $type = '';
+	public $size = 0;
+	public $is_key = false;
+	public $filters = array();
+	
+	public function __construct($property_name, $field_name, $type, $size, $is_key=false, $table_name='', $filters=array())
+	{
+		$this->property_name = $property_name;
+		$this->field_name = $field_name;
+		$this->type = $type;
+		$this->size = $size;
+		$this->is_key = $is_key;
+		$this->table_name = $table_name;
+		$this->filters = $filters;
+	}
+	
+	public function setObject($object, $row)
+	{
+	     if ($this->property_name && $this->field_name) {
+	     	$property = $this->property_name;
+	     	if (isset($row[$this->field_name])) {
+	     		 $object->$property = $row[$this->field_name];
+	     	}
+	     }
+		return $this;
+	}
+	
+	public function setRow($row, $object)
+	{
+	     if ($this->property_name && $this->field_name) {
+	     	$property = $this->property_name;
+	     	if (isset($object->$property)) {
+	     		$row[$this->field_name] = $object->$property;
+	     	}
+	     }
+		return $this;
+	}
+	
+	public function getTableFieldName()
+	{
+	     if ($this->table_name) {
+	     	return $this->table_name . '.' . $this->field_name;
+	     } else {
+	     	return $this->field_name;
+	     }
+	}
+	
+	public function isKey()
+	{
+	     return $this->is_key;
+	}
+
+}
