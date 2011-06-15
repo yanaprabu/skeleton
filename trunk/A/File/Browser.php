c2b49229eb061fccb79067439efa220c200f91f4
jonahbron.d@gmail.com <jonahbron.d@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-06-15 22:31:54 +0000

    Cleaned up syntax and PHPDoc comments in A/File

--- trunk/A/File/Browser.php
+++ trunk/A/File/Browser.php
@@ -12,7 +12,9 @@
  * 
  * Get list of directories and files in a directory.
  */
-class A_File_Browser {
+class A_File_Browser
+{
+
 	const SORT_NONE = 0;
 	const SORT_CASE_SENSITIVE = 1;
 	const SORT_CASE_INSENSITIVE = 2;
@@ -39,8 +41,9 @@ class A_File_Browser {
 	protected $path_separator = ';';
 	protected $extra_params = array();
 	protected $errorMsg = '';
-
-	public function __construct($request, $base_path='', $default_area='', $areas=array()) {
+	
+	public function __construct($request, $base_path='', $default_area='', $areas=array())
+	{
 		$this->request = $request;
 		$this->base_path = $this->_addTrailingSlash($base_path);
 		$this->areas = $areas;
@@ -48,22 +51,25 @@ class A_File_Browser {
 		$this->sort = self::SORT_CASE_INSENSITIVE;
 	}
 	
-	protected function _formatParam($param) {
+	protected function _formatParam($param)
+	{
 		$value = trim(preg_replace(array("/[\{$this->path_separator}]/", '/[^a-zA-Z0-9\-\/\.\ \%]/'), array('/', ''), urldecode($this->request->get($param))), '/');
 		if ($value != '') {
 			$value .= '/';
 		}
 		return $value;
 	}
-
-	protected function _addTrailingSlash($value) {
+	
+	protected function _addTrailingSlash($value)
+	{
 		if (substr($value, -1, 1) != '/') {
 			$value .= '/';
 		}
 		return $value;
 	}
-
-	public function processRequest() {
+	
+	public function processRequest()
+	{
 		$this->rel_path = $this->_formatParam($this->param_path);
 		$area = $this->_formatParam($this->param_area);
 		if ($area && in_array($area, $this->areas)) {
@@ -73,7 +79,8 @@ class A_File_Browser {
 		$this->rel_path = $this->_addTrailingSlash($this->rel_path);
 	}
 	
-	public function readDir () {
+	public function readDir()
+	{
 		$this->processRequest();
 		
 		$path = $this->base_path . $this->rel_area . $this->rel_path;
@@ -83,7 +90,7 @@ class A_File_Browser {
 			$this->errorMsg = "$path does not exist<br/>";
 			return -1;
 		}
-
+		
 		$result = array();
 		$this->entry_n = 0;
 		$this->dir_n = 0;
@@ -92,11 +99,9 @@ class A_File_Browser {
 		$this->dir_max = 0;
 		$this->file_max = 0;
 		
-#		$param = "{$this->param_area}={$this->rel_area}&{$this->param_path}=";
 		$param = array($this->param_area=>$this->rel_area, $this->param_path=>'');
 		
 		while ($directory->valid()) {
-		            
 			$filename = $directory->getFileName();
 			if ($directory->isDir()) {
 				if ($filename == '..') {
@@ -128,7 +133,7 @@ class A_File_Browser {
 			usort($this->entries, array($this, '_entrySort'));
 		}
 		
-		for ($i=0; $i<$this->entry_max; ++$i) {
+		for ($i = 0; $i < $this->entry_max; $i++) {
 			if ($this->entries[$i]['type'] == 'dir') {
 				$this->dirs[$this->dir_max++] = $i;
 			} else {
@@ -138,8 +143,9 @@ class A_File_Browser {
 		
 		return $this->entry_max;
 	}
-
-	public function _entrySort($e1, $e2) {
+	
+	public function _entrySort($e1, $e2)
+	{
 		if ($this->sort == self::SORT_CASE_SENSITIVE) {
 			$a = $e1['filename'];
 			$b = $e2['filename'];
@@ -152,67 +158,78 @@ class A_File_Browser {
 		}
 		return ($a < $b) ? -1 : 1;
 	}
-
-	public function getBasePath() {
+	
+	public function getBasePath()
+	{
 		return $this->base_path;
 	}
-
-	public function getPath() {
+	
+	public function getPath()
+	{
 		return $this->base_path . $this->rel_area . $this->rel_path;
 	}
-
-	public function getAreaPath() {
+	
+	public function getAreaPath()
+	{
 		return $this->rel_area;
 	}
-
-	public function getRelativePath() {
+	
+	public function getRelativePath()
+	{
 		return $this->rel_path;
 	}
-
-	public function rewindFiles() {
+	
+	public function rewindFiles()
+	{
 		$this->file_n = 0;
 	}
 	
-	public function nextFile() {
+	public function nextFile()
+	{
 		if ($this->file_n < $this->file_max) {
 			$this->file_current_n = $this->file_n;
 			return $this->entries[$this->files[$this->file_n++]];
 		}
 	}
-
-	public function rewindDirs() {
+	
+	public function rewindDirs()
+	{
 		$this->dir_n = 0;
 	}
 	
-	public function nextDir() {
+	public function nextDir()
+	{
 		if ($this->dir_n < $this->dir_max) {
 			$this->dir_current_n = $this->dir_n;
 			return $this->entries[$this->dirs[$this->dir_n++]];
 		}
 	}
-
-	public function getFiles() {
+	
+	public function getFiles()
+	{
 		$files = array();
 		foreach ($this->files as $n) {
 			$files[] = $this->entries[$n];
 		}
 		return $files;
 	}
-
-	public function getDirs() {
+	
+	public function getDirs()
+	{
 		$dirs = array();
 		foreach ($this->dirs as $n) {
 			$dirs[] = $this->entries[$n];
 		}
 		return $dirs;
 	}
-
-	public function buildParam($params, $sep1='&', $sep2='=') {
+	
+	public function buildParam($params, $sep1='&', $sep2='=')
+	{
 		return $this->buildParameters($params, $sep1, $sep2);
 	}
-
-	public function buildParameters($params, $sep1='&', $sep2='=') {
-#dump($params, 'buildParam');
+	
+	public function buildParameters($params, $sep1='&', $sep2='=')
+	{
 		foreach ($this->extra_params as $name => $value) {
 			if (($name != $this->param_area) && ($name != $this->param_path)) {
 				$params[$name] = $value;
@@ -227,16 +244,18 @@ class A_File_Browser {
 		}
 		return $str;
 	}
-
-	public function getParam() {
+	
+	public function getParam()
+	{
 		return $this->getParameters();
 	}
-
-	public function getParameters() {
+	
+	public function getParameters()
+	{
 		$params = array(
 			$this->param_area => $this->rel_area,
 			$this->param_path => $this->rel_path,
-			);
+		);
 		foreach ($this->extra_params as $name => $value) {
 			if (($name != $this->param_area) && ($name != $this->param_path)) {
 				$params[$name] = $value;
@@ -244,13 +263,15 @@ class A_File_Browser {
 		}
 		return $params;
 	}
-
-	public function setExtraParameters($params=array()) {
+	
+	public function setExtraParameters($params=array())
+	{
 		$this->extra_params = $params;
 		return $this;
 	}
-
-	public function setSort($sort) {
+	
+	public function setSort($sort)
+	{
 		$this->sort = $sort;
 		return $this;
 	}
