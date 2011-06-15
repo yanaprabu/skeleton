d1c2ba2dd71997cf96257e56368177b73212bd31
jonahbron.d@gmail.com <jonahbron.d@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-06-15 02:34:19 +0000

    Cleaned up syntax in A/Controller/List

--- trunk/A/Controller/List.php
+++ trunk/A/Controller/List.php
@@ -12,29 +12,29 @@
  * 
  * was intended for pagination support -- not used
  */
-class A_Controller_List extends A_Controller_Input {
+class A_Controller_List extends A_Controller_Input
+{
 
-	public function __construct($db) {
+	public function __construct($db)
+	{
 		$this->pager = new A_Pager($db);
-	
+		
 		$this->addParameter($param1 = new A_Controller_Input_Field($this->pager->parampagen));
 		$parampagen->addFilter(new FilterRegexp('/[^0-9]/', ''));
-	
+		
 		$this->addParameter($paramrecordcount = new A_Controller_Input_Field($this->pager->paramrecordcount));
 		$paramrecordcount->addFilter(new FilterRegexp('/[^0-9]/', ''));
-	
+		
 		$this->addParameter($paramorderby = new A_Controller_Input_Field($this->pager->paramorderby));
 		$paramorderby->addFilter(new FilterRegexp('/[^0-9]/', ''));
-	
 	}
 	
-	public function run($locator) {
-	
+	public function run($locator)
+	{
 		$request = $locator->get('Request');
 		$this->processRequest($request);
-	
+		
 		if ($reqest->is_post) {
-		} else {
 		}
 		parent::run($locator);
 	}
