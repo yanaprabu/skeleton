bc6e738ba78ca4cfc73148e1dcb923db9cc4e294
jonahbron.d <jonahbron.d@gmail.com>
2011-06-17 00:10:45 +0000

    Improved A_Renderable and A_Renderer interfaces

--- A/Renderer.php
+++ A/Renderer.php
@@ -1,6 +1,43 @@
 <?php
+/**
+ * Renderer.php
+ *
+ * @package  A
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD
+ * @link	 http://skeletonframework.com/
+ */
 
-interface A_Renderer {
-	public function render();
-	public function __toString();
+/**
+ * A_Renderer
+ *
+ * Interface representing an object capable of rendering data.
+ */
+interface A_Renderer extends A_Renderable
+{
+
+	/**
+	 * Set internal data to be rendered
+	 * 
+	 * @param string $key Key of the data
+	 * @param mixed $value Value to be stored under $key
+	 * @return $this Should return self for fluency
+	 */
+	public function set($key, $value);
+	
+	/**
+	 * Get internal data at index
+	 * 
+	 * @param string $key
+	 * @return mixed Value stored under $key
+	 */
+	public function get($key);
+	
+	/**
+	 * Merge an array of data with the data already stored internally
+	 * 
+	 * @param array $data
+	 * @return $this Should return self for fluency
+	 */
+	public function import($data);
+
 }
