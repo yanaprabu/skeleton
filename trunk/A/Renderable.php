3669c22554de171a3d021548c8c9c6019846caaa
jonahbron.d@gmail.com <jonahbron.d@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-06-17 00:10:45 +0000

    Improved A_Renderable and A_Renderer interfaces

--- trunk/A/Renderable.php
+++ trunk/A/Renderable.php
@@ -1,7 +1,49 @@
 <?php
+/**
+ * Renderable.php
+ *
+ * @package  A
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD
+ * @link	 http://skeletonframework.com/
+ */
 
-interface A_Renderable extends A_Renderer {
-	public function set($key, $value);
-	public function get($key);
-	public function import($data);
+/**
+ * A_Renderable
+ *
+ * Interface representing a renderable object.
+ */
+interface A_Renderable
+{
+
+	/**
+	 * Render this object.  Call when program is ready to convert object to a string.  Usually calls the render() method of the A_Renderer kept internally.
+	 * 
+	 * @return string Rendered result
+	 * @see A_Renderer
+	 */
+	public function render();
+	
+	/**
+	 * Magic method so that object is automatically rendered when used in a string context.  Should call render().
+	 * 
+	 * @return string Rendered result
+	 * @see render()
+	 */
+	public function __toString();
+	
+	/**
+	 * Sets the internal A_Renderer object to use.
+	 * 
+	 * @param A_Renderer $renderer
+	 * @return $this Should return self for fluency.
+	 */
+	public function setRenderer(A_Renderer $renderer);
+	
+	/**
+	 * Indicate whether or not this Renderable has been given a Renderer.
+	 * 
+	 * @return bool True if has a renderer, false otherwise
+	 */
+	public function hasRenderer();
+
 }
