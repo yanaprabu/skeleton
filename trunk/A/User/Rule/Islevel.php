3c1a84a1c90f2f77beaff973354ee3021673f077
jonahbron.d@gmail.com <jonahbron.d@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-06-24 21:27:55 +0000

    Cleaned up syntax and PHPDoc comments in A/User/.

--- trunk/A/User/Rule/Islevel.php
+++ trunk/A/User/Rule/Islevel.php
@@ -12,21 +12,26 @@
  * 
  * Check if user's access level is >= required access level.
  */
-class A_User_Rule_Islevel extends A_User_Rule_Base {
+class A_User_Rule_Islevel extends A_User_Rule_Base
+{
+
 	protected $level;
 	
-	public function __construct ($level, $forward=array(), $field='access') {
+	public function __construct ($level, $forward=array(), $field='access')
+	{
 		$this->level = $level;
 		$this->forward = $forward;
 		$this->field = $field;
 	}
-
-	public function setLevel($level) {
+	
+	public function setLevel($level)
+	{
 		$this->level = $level;
 		return $this;
 	}
 	
-	public function isValid($user=null) { 
+	public function isValid($user=null)
+	{ 
 		$user = $this->getUser($user);
 		if ($user && $user->isLoggedIn()) {
 			$userlevel = $user->get($this->field);
--- trunk/A/User/Session.php
+++ trunk/A/User/Session.php
@@ -12,58 +12,68 @@
  * 
  * Access to user data from session.
  */
-class A_User_Session {
+class A_User_Session
+{
+
 	protected $_data;
 	protected $_session;
 	protected $_namespace;
 	
-	
-	public function __construct($session, $namespace='A_User_Session') {
+	public function __construct($session, $namespace='A_User_Session')
+	{
 		$this->_session = $session;
 		$this->_namespace = $namespace;
 	}
-
-	public function setSession($session) {
+	
+	public function setSession($session)
+	{
 		$this->_session = $session;
 		return $this;
 	}
-
-	public function getSession() {
+	
+	public function getSession()
+	{
 		return $this->_session;
 	}
-
-	public function setNamespace($namespace) {
+	
+	public function setNamespace($namespace)
+	{
 		$this->_namespace = $namespace;
 		return $this;
 	}
-
-	public function getNamespace() {
+	
+	public function getNamespace()
+	{
 		return $this->_namespace;
 	}
-
-	public function start() {
+	
+	public function start()
+	{
 		$this->_session->start();
 		if (!isset($this->_data)) {
 			$this->_data =& $this->_session->getRef($this->_namespace);
 		}
 	}
-
-	public function isLoggedIn() {
+	
+	public function isLoggedIn()
+	{
 		$this->start();
-		if ($this->_data && isset($this->_data['auth']) ) {
+		if ($this->_data && isset($this->_data['auth'])) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 	
-	public function logout() {
+	public function logout()
+	{
 		if ($this->_data) {
 			$this->_session->set($this->_namespace, null);	// unset all data
 		}
 	}
 	
-	public function login($data=array()) {
+	public function login($data=array())
+	{
 		if ($this->_namespace) {
 			$this->start();
 			$this->_data['auth'] = true;
@@ -72,31 +82,45 @@ class A_User_Session {
 	}
 	
 	/**
-	 * depricated name for isLoggedIn()
+	 * Alias for isLoggedIn()
+	 * 
+	 * @deprecated
+	 * @see isLoggedIn()
 	 */
-	public function isSignedIn() {
+	public function isSignedIn()
+	{
 		return $this->isLoggedIn();
 	}
 	
 	/**
-	 * depricated name for logout()
+	 * Alias for logout()
+	 * 
+	 * @deprecated
+	 * @see logout()
 	 */
-	public function signout() {
+	public function signout()
+	{
 		$this->logout();
 	}
 	
 	/**
-	 * depricated name for login()
+	 * Alias for login()
+	 * 
+	 * @deprecated
+	 * @see login()
 	 */
-	public function signin($data=array()) {
+	public function signin($data=array())
+	{
 		$this->login($data);
 	}
 	
-	public function get($key='') {
+	public function get($key='')
+	{
 		$this->start();
-		if ($this->_namespace && isset($this->_data['data'] ) ) {
+		if ($this->_namespace && isset($this->_data['data']))
+		{
 			if ($key) {
-				if (isset($this->_data['data'][$key]) ) {
+				if (isset($this->_data['data'][$key])) {
 					return $this->_data['data'][$key];
 				}
 			} else {
@@ -105,7 +129,8 @@ class A_User_Session {
 		}
 	}
 	
-	public function set($key, $value) {
+	public function set($key, $value)
+	{
 		if ($key && $this->_namespace) {
 			$this->start();
 			if ($value !== null) {
@@ -117,30 +142,34 @@ class A_User_Session {
 		return $this;
 	}
 	
-	public function __get($name) {
+	public function __get($name)
+	{
 		return $this->get($name);
 	}
-
-	public function __set($name, $value) {
+	
+	public function __set($name, $value)
+	{
 		return $this->set($name, $value);
 	}
-
-	public function __call($name, $args) {
+	
+	public function __call($name, $args)
+	{
 		$prefix = substr($name, 0, 3);
 		$key = strtolower(substr($name, 3));
 		$this->start();
 		switch ($prefix) {
-		case 'get':
-			return isset($this->_data[$key]) ? $this->_data[$key] : null;
-			break;
-		case 'set':
-			$this->_data[$key] = isset($args[0]) ? $args[0] : null;
-			return $this;
-			break;
+			case 'get':
+				return isset($this->_data[$key]) ? $this->_data[$key] : null;
+				break;
+			case 'set':
+				$this->_data[$key] = isset($args[0]) ? $args[0] : null;
+				return $this;
+				break;
 		}
 	}
-
-	public function merge($data) {
+	
+	public function merge($data)
+	{
 		if (is_array($data) && $this->_namespace) {
 			$this->start();
 			if (isset($this->_data['data']) && is_array($this->_data['data'])) {
@@ -152,7 +181,8 @@ class A_User_Session {
 		return $this;
 	}
 	
-	public function close() {
+	public function close()
+	{
 		$this->_session->close();
 		//session_write_close();
 	}
