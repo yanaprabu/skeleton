9e8670aa14c1f532043776bc3d7a1400e85f1230
christopherxthompson <christopherxthompson@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-06-25 06:40:46 +0000

    A_Http - A_Http_Response - fix bug, A_Http_Cookie - added class

---
+++ trunk/A/Http/Cookie.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Url.php
+ *
+ * @package  A_Http
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD
+ * @link	 http://skeletonframework.com/
+ */
+
+/**
+ * A_Http_Cookie
+ *
+ * This class provides various methods with which to create, manipulate, and read URLs.
+ */
+class A_Http_Cookie
+{
+	protected $expire = 0;
+	protected $path = '';
+	protected $domain = '';
+	protected $secure = false;
+	protected $httponly = false;
+	
+	public function get($name)
+	{
+		return isset($_COOKIE[$name]) ? $_COOKIE[$name] : null;
+	}
+
+	public function set($name, $value, $expire=null, $path=null, $domain=null, $secure=null, $httponly=null)
+	{
+		// if not set then use defaults
+		if ($expire === null) {
+			$expire = $this->expire;
+		}
+		if ($path === null) {
+			$path = $this->path;
+		}
+		if ($domain === null) {
+			$domain = $this->domain;
+		}
+		if ($secure === null) {
+			$secure = $this->secure;
+		}
+		if ($httponly === null) {
+			$httponly = $this->httponly;
+		}
+		// save cookie data
+		$this->cookies[$name] = array(
+			'name' => $name,
+			'value' => $value,
+			'expire' => $expire,
+			'path' => $path,
+			'domain' => $domain,
+			'secure' => $secure,
+			'httponly' => $httponly,
+			);
+		return $this;
+	}
+	
+	public function setExpire($expire)
+	{
+		$this->expire = $expire;
+		return $this;
+	}
+	
+	public function setPath($path)
+	{
+		$this->path = $path;
+		return $this;
+	}
+	
+	public function setDomain($domain)
+	{
+		$this->domain = $domain;
+		return $this;
+	}
+	
+	public function setSecure($secure)
+	{
+		$this->secure = $secure;
+		return $this;
+	}
+	
+	public function setHttponly($httponly)
+	{
+		$this->httponly = $httponly;
+		return $this;
+	}
+	
+	public function render()
+	{
+		foreach ($this->cookies as $c) {
+			setcookie($c['name'], $c['value'], $c['expire'], $c['path'], $c['domain'], $c['secure'], $c['httponly']);
+        }
+	}
+
+}
