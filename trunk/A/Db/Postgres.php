f99f2282a3b24b6b0a839842f560a66a9cfe44a3
jonahbron.d@gmail.com <jonahbron.d@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-06-26 18:27:18 +0000

    [Db] Implemented lastId() in the Postgres Adapter.

--- trunk/A/Db/Postgres.php
+++ trunk/A/Db/Postgres.php
@@ -14,6 +14,7 @@
  */
 class A_Db_Postgres extends A_Db_Adapter
 {
+
 	protected $sequenceext = '_seq';
 	protected $sequencestart = 1;
 	protected $_recordset_class = 'A_Db_Recordset_Postgres';
@@ -158,7 +159,8 @@ class A_Db_Postgres extends A_Db_Adapter
 	
 	protected function _lastId()
 	{
-		return 0;
+		$row = pg_fetch_row(pg_query($this->_connection, 'SELECT lastval();'));
+		return $row[0];
 	}
 
 }
