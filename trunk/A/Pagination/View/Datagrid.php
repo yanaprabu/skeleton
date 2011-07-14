662b80311e1d2d9acc2b234a1b816efc0b1b9e8b
jonahbron.d <jonahbron.d@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-07-14 03:25:04 +0000

    [Pagination] Renamed A_Pagination_View_DataGrid to A_Pagination_View_Datagrid.

--- trunk/A/Pagination/View/Datagrid.php
+++ trunk/A/Pagination/View/Datagrid.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * DataGrid.php
+ * Datagrid.php
  *
  * @package  A_Pagination
  * @license  http://www.opensource.org/licenses/bsd-license.php BSD
@@ -9,11 +9,11 @@
  */
 
 /**
- * A_Pagination_View_DataGrid
+ * A_Pagination_View_Datagrid
  * 
  * Generate complete data grid
  */
-class A_Pagination_View_DataGrid
+class A_Pagination_View_Datagrid
 {
 
 }
