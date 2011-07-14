44b555a163396937728cb4d2da54fd3c4d806e91
jonahbron.d <jonahbron.d@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-07-14 16:04:31 +0000

    [Socket] Renamed A_Socket_EventListener_* classes to A_Socket_Eventlistener_*.  Complete.

---
+++ trunk/A/Socket/Eventlistener/Cachedcontroller.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Cachedcontroller.php
+ *
+ * @package  A_Socket_Eventlistener
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD
+ * @link	 http://skeletonframework.com/
+ * @author   Jonah Dahlquist <jonah@nucleussystems.com>
+ */
+
+/**
+ * A_Socket_Eventlistener_Cachedcontroller
+ * 
+ * Stores controllers in memory ahead of time to reduce routing time.
+ */
+class A_Socket_Eventlistener_Cachedcontroller
+{
+
+	public function  __construct($locator)
+	{
+		parent::__construct($locator);
+	}
+	
+	public function onConnect($data)
+	{
+		$data->setSession(new A_Collection());
+	}
+	
+	public function onDisconnect($data)
+	{
+
+	}
+	
+	public function onMessage($data)
+	{
+		$route = $data->getRoute();
+		
+		// check validity of route and cached controller/action
+		if (is_string($route) && isset($this->_cache[$route]) && is_callable(array())) {
+			
+		}
+	}
+	
+	/**
+	 *
+	 * @param array $cache Cache of controller/action routes mapped to keywords
+	 */
+	public function setCache($cache)
+	{}
+
+}
