ae0cfb898555cf417e2c794e804ca510ccf9b405
jonahbron.d <jonahbron.d@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-07-14 00:59:47 +0000

    [Datetime] Rename complete

---
+++ trunk/A/Datetime/Duration.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * Duration.php
+ *
+ * @package  A_Datetime
+ * @license  http://www.opensource.org/licenses/bsd-license.php BSD
+ * @link	 http://skeletonframework.com/
+ * @author   Eran Galt, Cory Kaufman, Christopher Thompson
+ */
+
+/**
+ * A_Datetime_Duration
+ * 
+ * Date & Time Duration functionality 
+ */
+class A_Datetime_Duration
+{
+
+	protected $partNames = array('years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds', 'positive');
+	protected $years = 0;
+	protected $months = 0;
+	protected $weeks = 0;
+	protected $days = 0;
+	protected $hours = 0;
+	protected $minutes = 0;
+	protected $seconds = 0;
+	protected $positive = true;
+	
+	/**
+	 * Configuration at instatiation with Duration string, array or indivitual values
+	 * 
+	 * @param int $years
+	 * @param int $months
+	 * @param int $weeks
+	 * @param int $days
+	 * @param int $hours
+	 * @param int $minutes
+	 * @param int $seconds
+	 */
+	public function __construct ($years=0, $months=0, $weeks=0, $days=0, $hours=0, $minutes=0, $seconds=0)
+	{
+		if(is_array($years)) {
+			$this->config ($years);
+		} elseif (is_string($years)) {
+			$this->parseDuration($years);
+		} else {
+			$this->config(array(
+				'years' => $years,
+				'months' => $months,
+				'weeks' => $weeks,
+				'days' => $days,
+				'hours' => $hours,
+				'minutes' => $minutes,
+				'seconds' => $seconds
+			));
+		}
+	}
+	
+	/**
+	 * parse a Duration string in the format "1 years 1 months 3 weeks 4 days 5 hours 6 minutes 7 seconds"
+	 * 
+	 * @param string $string
+	 * @return array
+	 */
+	public function parseDuration ($string)
+	{
+		$parts = array();
+		$stringParts = explode(',', $string);
+		foreach($stringParts as $part) {
+			$part = trim($part);
+			$breakPos = strpos($part, ' ');
+			if($breakPos !== false) {
+				$num = (int) substr($part, 0, $breakPos);
+				$partName = substr($part, $breakPos + 1);
+			} else {
+				$num = 1;
+				$partName = $part;
+			}
+			if($partName[strlen($partName) - 1] != 's') {
+				$partName .= 's';
+			}
+			if(in_array($partName,$this->partNames)) {
+				$parts[$partName] = $num;
+			}
+		}
+		$this->config($parts);
+		return $parts;
+	}
+	
+	/**
+	 * Configure class with assoc array. See partNames property in the format array('years'=>1, 'months'=>2, 'weeks'=>3, 'days'=>4, 'hours'=>5, minutes'=>6, 'seconds'=>7, 'positive'=>true).  Values omitted from the array will be set to zero
+	 * 
+	 * @param array $parts
+	 */
+	public function config ($parts)
+	{
+		foreach($this->partNames as $part) {
+			$this->$part = isset($parts[$part]) ? $parts[$part] : 0;
+		}
+	}
+	
+	/**
+	 * Set Duration to be used as a positive value when used with a date.
+	 */
+	public function setPositive()
+	{
+		$this->positive = true;
+	}
+	
+	/**
+	 * Set Duration to be used as a negative value when used with a date 
+	 */
+	public function setNegative()
+	{
+		$this->positive = false;
+	}
+	
+	/**
+	 * Return Duration string in strtotime() style in the format "1 years 1 months 3 weeks 4 days 5 hours 6 minutes 7 seconds" 
+	 */
+	public function toString()
+	{
+		$string = array();
+		foreach($this->partNames as $part) {
+			$value = $this->$part;
+			$string[] = $this->positive ? $value : ((0 - $value) . ' ' . $part);
+		}
+		return implode(', ', $string);  
+	}
+	
+	/**
+	 * Return Duration values in assoc array
+	 * 
+	 * @return array
+	 */
+	public function toArray()	{
+		return array (
+			'years' => $this->years,
+			'months' => $this->months,
+			'weeks' => $this->weeks,
+			'days' => $this->days,
+			'hours' => $this->hours,
+			'minutes' => $this->minutes,
+			'seconds' => $this->seconds
+		);
+	}
+			
+	/**
+	 * Return value of Duration when used in string context per toString() method
+	 * 
+	 * @return string
+	 */
+	public function __toString() {
+		return $this->toString();
+	}
+
+}
---
+++ trunk/tests/unit/Datetime/DurationTest.php
@@ -0,0 +1,76 @@
+<?php
+
+class DurationTest extends UnitTestCase	{
+
+	function setUp()	{
+		$this->duration = new A_Datetime_Duration();
+		$this->expectedArray = array(
+			'years' => 2,
+			'months' => 3,
+			'weeks' => 0,
+			'days' => 5,
+			'hours' => 1,
+			'minutes' => 10,
+			'seconds' => 3
+		);
+		$this->expectedArray2 = array(
+			'years' => 0,
+			'months' => 0,
+			'weeks' => 3,
+			'days' => 0,
+			'hours' => 0,
+			'minutes' => 0,
+			'seconds' => 0
+		);
+		
+	}
+
+	function testConstructFromIntegers()	{
+		$duration = new A_Datetime_Duration (2,3,0,5,1,10,3);
+		$this->assertEqual ($duration->toArray(), $this->expectedArray);
+	}
+	
+	function testConstructconfig()	{
+		$duration = new A_Datetime_Duration (array (
+			'years' => 2,
+			'months' => 3,
+			'days' => 5,
+			'hours' => 1,
+			'minutes' => 10,
+			'seconds' => 3			
+		));
+		$this->assertEqual ($duration->toArray(), $this->expectedArray);
+	}
+	
+	function testConstructparseDuration()	{
+		$duration = new A_Datetime_Duration ('2 years, 3 months, 5 days, 1 hour, 10 minutes, 3 seconds');
+		$this->assertEqual ($duration->toArray(), $this->expectedArray);
+	}
+	
+	function testParseDuration()	{
+		$this -> duration -> parseDuration ('2 years, 3 months, 5 days, 1 hour, 10 minutes, 3 seconds');
+		$this -> assertEqual ($this->duration->toArray(), $this->expectedArray);
+	}
+
+	function testConfig()	{
+		$this->duration->config (array (
+			'years' => 2,
+			'months' => 3,
+			'days' => 5,
+			'hours' => 1,
+			'minutes' => 10,
+			'seconds' => 3			
+		));
+		$this->assertEqual ($this->duration->toArray(), $this->expectedArray);		
+	}
+	
+	function testParseDurationOverwritesExistingValues()	{
+		$duration = new A_Datetime_Duration ('4 years, 4 months, 4 weeks, 4 days, 4 hours, 4 minutes, 4 seconds');
+		$duration->parseDuration ('2 years, 3 months, 5 days, 1 hour, 10 minutes, 3 seconds');
+		$this->assertEqual ($duration->toArray(), $this->expectedArray);
+		$duration2 = new A_Datetime_Duration ('4 years, 4 months, 4 weeks, 4 days, 4 hours, 4 minutes, 4 seconds');
+		$duration2->parseDuration ('3 weeks');
+		$this->assertEqual ($duration2->toArray(), $this->expectedArray2);
+	}
+	
+}
