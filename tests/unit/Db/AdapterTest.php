7cb2cceebde0b1f0c83c295e346b9c9bf9588a6c
jonahbron.d <jonahbron.d@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2011-08-12 00:57:23 +0000

    [Db] added getConnection() method to A_Db_Adapter, and corresponding unit test.

--- tests/unit/Db/AdapterTest.php
+++ tests/unit/Db/AdapterTest.php
@@ -1,52 +1,68 @@
 <?php
 
-class DbAdapterClass extends A_Db_Adapter {
+class DbAdapterClass extends A_Db_Adapter
+{
+
 	public $test_config = array();
 	
-	public function _connect() {
+	public function _connect()
+	{
 		$this->test_config = $this->_config;
+		$this->_connection = 'xyz';
 	}
+	
+	protected function _close()
+	{}
+	
+	protected function _query($sql)
+	{}
+	
+	protected function _lastId()
+	{}
 
-	protected function _close() {		
-	}
+	protected function _selectDb($database)
+	{}
 
-	protected function _query($sql) {		
-	}
+	public function limit($sql, $count, $offset='')
+	{}
 
-	protected function _lastId() {		
-	}
-
-	protected function _selectDb($database) {		
-	}
-
-	public function limit($sql, $count, $offset='') {		
-	}
 }
 
-class Db_AdapterTest extends UnitTestCase {
+class Db_AdapterTest extends UnitTestCase
+{
+
 	protected $config;
 	
-	function setUp() {
+	function setUp()
+	{
 		$this->config = array(
+			
+			// not meant to be a real connection, no connection actually made
 			'SINGLE' => array(
-			    'phptype' => 'mysql',
-			    'hostspec' => 'localhost',
-			    'database' => 'single',
-			    'username' => 'single',
-			    'password' => 'single',
-				),
-			);
+				'phptype' => 'mysql',
+				'hostspec' => 'localhost',
+				'database' => 'single',
+				'username' => 'single',
+				'password' => 'single',
+			),
+		);
 	}
 	
-	function TearDown() {
-	}
-	
-	function testDb_AdapterConstruct() {
+	function testDb_AdapterConstruct()
+	{
 		$Db_Adapter = new DbAdapterClass($this->config['SINGLE']);
 		
 		$Db_Adapter->connect();
 		$this->assertEqual($Db_Adapter->test_config['database'], 'single');
-#echo "ERROR for connection name '{$Db_Adapter->connection_set['database']}': " . $Db_Adapter->_getErrorMsg() . "<br/>\n";
+	}
+	
+	public function testGetConnection()
+	{
+		$Db_Adapter = new DbAdapterClass($this->config['SINGLE']);
+		$Db_Adapter->connect();
+		
+		$this->assertEqual($Db_Adapter->getConnection(), 'xyz');
 	}
 
 }
+
