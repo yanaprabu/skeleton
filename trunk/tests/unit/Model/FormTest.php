369b1d453d446dca2d8b6b4f1fc40ece968c2d12
christopherxthompson@gmail.com <christopherxthompson@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2012-11-30 05:32:38 +0000

    A_Model_Form - moved code from isValid() that checked if submitted into isSubmitted(). Now isValid() calls isSubmitted(). isSubmitted() can now take a request object and be called before isValid(). Updated tests.

--- trunk/tests/unit/Model/FormTest.php
+++ trunk/tests/unit/Model/FormTest.php
@@ -1,4 +1,10 @@
 <?php
+class Model_FormTest_Request extends A_Http_Request
+{
+	public function setMethod($method) {
+		$this->method = $method;
+	}
+}
 
 class Model_FormTest extends UnitTestCase {
 	
@@ -8,12 +14,61 @@ class Model_FormTest extends UnitTestCase {
 	function TearDown() {
 	}
 	
-	function testModel_FormNotNull() {
-  		$model = new A_Model_Form();
+	function testModel_FormSubmitted() {
+		$request = new Model_FormTest_Request();
+		$model = new A_Model_Form();
 		
-		$result = true;
-  		$this->assertTrue($result);
-		$this->assertFalse(!$result);
+		// check not post or 
+		$this->assertFalse($model->isSubmitted($request));
+		
+		// test submit param mode, post/get not required
+		$request->set('submit', 'foo');
+		$model->setMethod('');
+		$model->setSubmitParameterName('submit');
+		$this->assertTrue($model->isSubmitted($request));
+
+		// test post mode
+		$request->setMethod('POST');
+
+		// post but no submit param name
+		$model->setSubmitParameterName('');
+		$this->assertTrue($model->isSubmitted($request));
+
+		// post and submit param name
+		$model->setSubmitParameterName('submit');
+		$this->assertTrue($model->isSubmitted($request));
+	}
+	
+	function testModel_FormValid() {
+		$request = new Model_FormTest_Request();
+		$model = new A_Model_Form();
+		
+		// check not post
+		$this->assertFalse($model->isValid($request));
+		
+		// test post mode no fields or rules
+		$request->setMethod('POST');
+		$this->assertTrue($model->isValid($request));
+
+		// test post mode with field and param value but no rules
+		$request->set('foo', 'bar');
+		$field = $model->newField('foo');
+		$this->assertTrue($model->isValid($request));
+		$this->assertEqual($model->get('foo'), 'bar');
+
+		// add rule
+		$field->addRule(new A_Rule_Notnull('foo', 'error'));
+		
+		// param not set
+		$request->set('foo', null);
+		$this->assertFalse($model->isValid($request));
+		$this->assertTrue($model->isError());
+		$this->assertEqual($model->getErrorMsg(), array('foo'=>array('error')));
+		$this->assertEqual($model->getErrorMsg(' '), 'error');
+
+		// param not set
+		$request->set('foo', 'bar');
+		$this->assertTrue($model->isValid($request));
 	}
 	
 }
