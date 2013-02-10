b4ad13fed55bbc0a0cb7b053df43b21b987c3364
christopherxthompson@gmail.com <christopherxthompson@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2013-02-10 07:09:27 +0000

    A_Pagination_Helper_Url - simplified default, setBase() and parameter base URLs.Updated test.

--- trunk/tests/unit/Pagination/Helper/UrlTest.php
+++ trunk/tests/unit/Pagination/Helper/UrlTest.php
@@ -3,34 +3,38 @@
 class Pagination_Helper_UrlTest extends UnitTestCase	{
 
 	public function setUp()	{
-		$this->url = new A_Pagination_Helper_Url ('forums.devnetwork.net', 'http');
+		$this->url = new A_Pagination_Helper_Url ('forums.devnetwork.net/page.php', 'http');
 	}
 
 	public function testSingleSetParameter()	{
 		$this->url->set('order', 'title');
-		$this->assertEqual ($this->url->render ('page.php'), 'http://forums.devnetwork.net/page.php?order=title');
+		$this->assertEqual ($this->url->render (), 'http://forums.devnetwork.net/page.php?order=title');
 	}
 
 	public function testDoubleSetParameter()	{
 		$this->url->set ('order', 'title');
 		$this->url->set ('pageSize', '5');
-		$this->assertEqual ($this->url->render ('page.php'), 'http://forums.devnetwork.net/page.php?order=title&pageSize=5');
+		$this->assertEqual ($this->url->render (), 'http://forums.devnetwork.net/page.php?order=title&pageSize=5');
 	}
 
 	public function testDoubleSetParameterWithBuildParameter()	{
 		$this->url->set ('order', 'title');
 		$this->url->set ('pageSize', '5');
-		$this->assertEqual ($this->url->render ('page.php', array ('page' => 2)), 'http://forums.devnetwork.net/page.php?order=title&pageSize=5&page=2');
+		$this->assertEqual ($this->url->render ('', array ('page' => 2)), 'http://forums.devnetwork.net/page.php?order=title&pageSize=5&page=2');
 	}
 
 	public function testSetBase()	{
 		$this->url->setBase ('www.devnetwork.net');
-		$this->assertEqual ($this->url->render ('page.php'), 'http://www.devnetwork.net/page.php');
+		$this->assertEqual ($this->url->render (), 'http://www.devnetwork.net');
+	}
+
+	public function testSetBaseParam()	{
+		$this->assertEqual ($this->url->render ('page.php'), 'page.php');
 	}
 
 	public function testSetProtocol()	{
 		$this->url->setProtocol ('https');
-		$this->assertEqual ($this->url->render ('page.php'), 'https://forums.devnetwork.net/page.php');
+		$this->assertEqual ($this->url->render (), 'https://forums.devnetwork.net/page.php');
 	}
 
 }
