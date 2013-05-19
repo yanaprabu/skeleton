8917acc877133d0fd729921ce1ded9878db7a6b4
m.p.abeelen@gmail.com <m.p.abeelen@gmail.com@6c5459e6-3e4a-0410-a0df-4314d3cb0b36>
2013-05-19 20:32:38 +0000

    Few changes to blog example single post template

--- trunk/examples/blog/app/blog/templates/singlePost.php
+++ trunk/examples/blog/app/blog/templates/singlePost.php
@@ -4,10 +4,10 @@
 	
 	<?php foreach($content as $article){
 		echo '<div class="post">';
-		echo '<h2 class="post_title"><a href="blog/'. $article['permalink'] . '">' . $article['title'] . '</a></h2>';
-		echo '<p class="post_meta">On ' .  $article['post_date'] . ' by ' . $article['username'] . '</p>';
-		echo '<p>' .  $article['excerpt'] . '</p>';
-		echo '<p>' .  $article['content'] . '</p>';
+		echo '<h2 class="post_title"><a href="blog/'. $this->escape($article['permalink']) . '">' . $this->escape($article['title']) . '</a></h2>';
+		echo '<p class="post_meta">On ' .  $this->escape($article['post_date']) . ' by ' . $this->escape($article['username']) . '</p>';
+		echo '<p>' .  $this->escape($article['excerpt']) . '</p>';
+		echo '<p>' .  $this->escape($article['content']) . '</p>';
 		echo '	</div>';
 	} 
 	?>
@@ -22,30 +22,30 @@
 		<input type="hidden" name="posts_id" value="<?php echo (int) $content[0]['id']; ?>" />
 		<div>
 			<label>Name</label>
-			<input type="text" name="author" value="<?php echo isset($comment['author']) ? htmlentities($comment['author'],ENT_QUOTES,"UTF-8") : ''; ?>" >
+			<input type="text" name="author" value="<?php echo isset($comment['author']) ? $this->escape($comment['author']) : ''; ?>" >
 			<?php
-			echo isset($commenterror['author']) ? '<p class="error">' . htmlentities($commenterror['author'],ENT_QUOTES,"UTF-8") . '</p>' : '';
+			echo isset($commenterror['author']) ? '<p class="error">' . $this->escape($commenterror['author']) . '</p>' : '';
 			?>
 		</div>
 		<div>
 			<label>Email</label>
-			<input type="text" name="author_email" value="<?php echo isset($comment['author_email']) ? htmlentities($comment['author_email'],ENT_QUOTES,"UTF-8") : ''; ?>" >
+			<input type="text" name="author_email" value="<?php echo isset($comment['author_email']) ? $this->escape($comment['author_email']) : ''; ?>" >
 			<?php
-			echo isset($commenterror['author_email']) ? '<p class="error">' . htmlentities($commenterror['author_email'][0],ENT_QUOTES,"UTF-8") . '</p>' : '';
+			echo isset($commenterror['author_email']) ? '<p class="error">' . $this->escape($commenterror['author_email'][0]) . '</p>' : '';
 			?>
 		</div>
 		<div>
 			<label>Url</label>
-			<input type="text" name="author_url" value="<?php echo isset($comment['author_url']) ? htmlentities($comment['author_url'],ENT_QUOTES,"UTF-8") : ''; ?>" >
+			<input type="text" name="author_url" value="<?php echo isset($comment['author_url']) ? $this->escape($comment['author_url']) : ''; ?>" >
 			<?php
-			echo isset($commenterror['author_url']) ? '<p class="error">' . htmlentities($commenterror['author_url'][0],ENT_QUOTES,"UTF-8") . '</p>' : '';
+			echo isset($commenterror['author_url']) ? '<p class="error">' . $this->escape($commenterror['author_url'][0]) . '</p>' : '';
 			?>
 		</div>
 		<div>
 			<label>Name</label>
-			<textarea name="comment" rows="5" cols="20"><?php echo isset($comment['comment']) ? htmlentities($comment['comment'],ENT_QUOTES,"UTF-8") : ''; ?></textarea>
+			<textarea name="comment" rows="5" cols="20"><?php echo isset($comment['comment']) ? $this->escape($comment['comment']) : ''; ?></textarea>
 			<?php
-			echo isset($commenterror['comment']) ? '<p class="error">' . htmlentities($commenterror['comment'][0],ENT_QUOTES,"UTF-8") . '</p>' : '';
+			echo isset($commenterror['comment']) ? '<p class="error">' . $this->escape($commenterror['comment'][0]) . '</p>' : '';
 			?>
 		</div>
 		<div>
@@ -63,16 +63,16 @@
 		echo '<li>No comments now</li>';
 	} 
 	foreach($comments as $comment){
-		echo '<li class="alt item" id="comment-' . $comment['comment_id'] . '">';
+		echo '<li class="alt item" id="comment-' . (int) $comment['comment_id'] . '">';
 		echo '	<h4 class="vcard">';
-		echo '		<a href="#comment-' . $comment['comment_id'] . '">#</a> ';
-		echo '		<strong><cite>' . $comment['author'] . '</cite> says on ';
-		echo '		<span class="comment-date"> ' . $comment['comment_date']. '</span></strong>';
+		echo '		<a href="#comment-' . (int) $comment['comment_id'] . '">#</a> ';
+		echo '		<strong><cite>' . $this->escape($comment['author']) . '</cite> says on ';
+		echo '		<span class="comment-date"> ' . $this->escape($comment['comment_date']) . '</span></strong>';
 		echo '		<span class="gravatar">';
 		echo '			<img src="img/user.png" width="48" height="48" alt="" />';
 		echo '		</span>';
 		echo '	</h4>';
-		echo '	<blockquote><p>' . $comment['comment']. '</p></blockquote>';
+		echo '	<blockquote><p>' . $this->escape($comment['comment']) . '</p></blockquote>';
 		echo '</li>';
 	}
 	?>
