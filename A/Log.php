c4a541b28df3d9eb89b8a4d651263ad0f49ef624
christopherxthompson <christopherxthompson@gmail.com>
2014-03-26 23:41:14 +0000

    A_Log -Add support for static and instantiated usage

--- A/Log.php
+++ A/Log.php
@@ -61,7 +61,7 @@ class A_Log
 	/**
 	 * initialize property array to be assigned to self::staticData or $this->data
 	 */
-	protected function _initData()
+	protected static function _initData()
 	{
 		return array(
 			'messages' => array(),
@@ -79,7 +79,7 @@ class A_Log
 	/**
 	 * @return array containing either self::staticData or $this->data depending on if called statically or as object
 	 */
-	public function & _getData()
+	public static function & _getData()
 	{
 		if (isset($this)) {
 			return $this->data;
@@ -95,7 +95,7 @@ class A_Log
 	 * @param $writer - either string filename (will create A_Log_File($writer)) or array of writer objects
 	 * @return $this
 	 */
-	public function addWriter($writer=null)
+	public static function _addWriter($writer=null)
 	{
 		$data =& self::_getData();
 		if (is_string($writer)) {
@@ -110,7 +110,7 @@ class A_Log
 	 * @param $level  - maximum level at or below which messages will be written to log
 	 * @return $this
 	 */
-	public function setLevel($level)
+	public static function _setLevel($level)
 	{
 		$data =& self::_getData();
 		$data['level'] = $level;
@@ -121,7 +121,7 @@ class A_Log
 	 * @param $autoWrite  - set whether unwritten log messages arewritten on destruct
 	 * @return $this
 	 */
-	public function setAutoWrite($autoWrite)
+	public static function _setAutoWrite($autoWrite)
 	{
 		$data =& self::_getData();
 		$data['autoWrite'] = $autoWrite;
@@ -134,69 +134,17 @@ class A_Log
 	 * @param $level  - level to log messages
 	 * @return boolean if $level <= level property
 	 */
-	public function isLoggable($level)
+	public static function _isLoggable($level)
 	{
 		$data =& self::_getData();
 		return $level <= $data['level'];
 	}
 	
 	/**
-	 * @param string $message String to write to log
-	 * @param int $level Level to log messages
-	 * @param string $label optional label for message
-	 * @return $this
-	 */
-	public function log($level, $message='', $context=array())
-	{
-		$data =& self::_getData();
-
-		// deal with ($message) or ($message, $context) parameters
-		if (is_string($level)) {
-			if (is_array($message)) {
-				$context = $message;
-			}
-			$message = $level;
-			$level = $data['level'];
-		}
-		
-		$data['levels'][$data['nMessages']] = $level;
-		if (isset($context['exception'])) {
-			// deal with exceptions here
-		}
-		if (is_string($message)) {
-			if ($context) {
-				foreach ($context as $key => $value) {
-					$message = str_replace("\{$key\}", $value);
-				}
-			}
-		} else {
-			$message = self::_format($message);
-		}
-		$data['messages'][$data['nMessages']] = array(date($data['dateFormat']), $message);
-		++$data['nMessages'];
-		$data['written'] = false;
-		return isset($this) ? $this : null;
-	}
-	
-	/**
 	 * @param mixed $message variable to format
 	 * @return $message
 	 */
-	public function __call($name, $args)
-	{
-		if (in_array($name, $this->levelNames)) {
-			$level = array_search($name, $this->levelNames);
-			$message = isset($args[0]) ? $args[0] : '';
-			$context = isset($args[1]) ? $args[1] : array();
-			$this->log($level, $message, $context);
-		}
-	}
-	
-	/**
-	 * @param mixed $message variable to format
-	 * @return $message
-	 */
-	public function _format($message)
+	public static function _format($message)
 	{
 		switch (gettype($message)) {
 		case 'boolean':
@@ -216,9 +164,48 @@ class A_Log
 	}
 	
 	/**
+	 * @param string $message String to write to log
+	 * @param int $level Level to log messages
+	 * @param string $label optional label for message
+	 * @return $this
+	 */
+	public static function _log($level, $message='', $context=array())
+	{
+		$data =& self::_getData();
+
+		// deal with ($message) or ($message, $context) parameters
+		if (is_string($level)) {
+			if (is_array($message)) {
+				$context = $message;
+			}
+			$message = $level;
+			$level = $data['level'];
+		}
+		
+		$data['levels'][$data['nMessages']] = $level;
+		if (isset($context['exception'])) {
+			// deal with exceptions here
+		}
+		if (is_string($message)) {
+			if ($context) {
+				foreach ($context as $key => $value) {
+					$message = str_replace('{' . $key . '}', $value, $message);
+echo "key=$key, value=$value, message=$message<br>";
+				}
+			}
+		} else {
+			$message = self::_format($message);
+		}
+		$data['messages'][$data['nMessages']] = array(date($data['dateFormat']), $message);
+		++$data['nMessages'];
+		$data['written'] = false;
+		return isset($this) ? $this : null;
+	}
+	
+	/**
 	 * Remove previous messages from all logs
 	 */
-	public function clear()
+	public static function _clear()
 	{
 		$data =& self::_getData();
 		foreach ($data['writers'] as $writer) {
@@ -230,7 +217,7 @@ class A_Log
 	 * @param $message - optional message to log
 	 * @return $this
 	 */
-	public function write($level=null)
+	public static function _write($level=null)
 	{
 		$data =& self::_getData();
 		if ($level === null) {
@@ -264,10 +251,72 @@ class A_Log
 	 * 
 	 * @return string
 	 */
-	public function getErrorMsg()
+	public static function _getErrorMsg()
 	{
 		$data =& self::_getData();
 		return $data['errorMsg'];
+	}
+	
+	/**
+	 * @param mixed $message variable to format
+	 * @return $message
+	 */
+	public static function _call($name, $args)
+	{
+		switch ($name) {
+		case 'log':
+			$level = isset($args[0]) ? $args[0] : 0;
+			$message = isset($args[1]) ? $args[1] : '';
+			$context = isset($args[2]) ? $args[2] : array();
+			return self::_log($level, $message, $context);
+			break;
+		case 'addWriter':
+			return self::_addWriter(isset($args[0]) ? $args[0] : null);
+			break;
+		case 'setLevel':
+			return self::_setLevel(isset($args[0]) ? $args[0] : 0);
+			break;
+		case 'setAutoWrite':
+			return self::_setAutoWrite(isset($args[0]) ? $args[0] : 0);
+			break;
+		case 'isLoggable':
+			return self::_isLoggable(isset($args[0]) ? $args[0] : 0);
+			break;
+		case 'clear':
+			return self::_clear();
+			break;
+		case 'write':
+			self::_write(isset($args[0]) ? $args[0] : 0);
+			break;
+		case 'getErrorMsg':
+			return self::_getErrorMsg();
+			break;
+		default:
+			if (in_array($name, self::levelNames)) {
+				$level = array_search($name, self::levelNames);
+				$message = isset($args[0]) ? $args[0] : '';
+				$context = isset($args[1]) ? $args[1] : array();
+				self::_log($level, $message, $context);
+			}
+		}
+	}
+	
+	/**
+	 * @param mixed $message variable to format
+	 * @return $message
+	 */
+	public function __call($name, $args)
+	{
+		self::_call($name, $args);
+	}
+	
+	/**
+	 * @param mixed $message variable to format
+	 * @return $message
+	 */
+	public function __callStatic($name, $args)
+	{
+		self::_call($name, $args);
 	}
 	
 	/**
@@ -277,7 +326,7 @@ class A_Log
 	{
 		$data =& self::_getData();
 		if ($data['autoWrite'] && !$data['written']) {
-			self::write();
+			self::_write();
 		}
 	}
 
